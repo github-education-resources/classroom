d3dc70469a414bc5c114d42124072c97eb21e040
Mark Tareshawty <mark@marktareshawty.com>
2015-05-13 14:33:54 -0400

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/login',  to: 'sessions#new',     as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
 
+  get   '/auth/failure',            to: 'sessions#failure'
   match '/auth/:provider/callback', to: 'sessions#create',  via: [:get, :post]
-  match '/auth/failure',            to: 'sessions#failure', via: [:get, :post]
 end
