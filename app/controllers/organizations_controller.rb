138ec0ca6fa487076b3c1ea3fa22f9d436aabee4
Mark Tareshawty <mark@marktareshawty.com>
2015-05-14 01:12:56 -0400

    update to org routes and controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,13 +1,53 @@
 class OrganizationsController < ApplicationController
+  before_action :ensure_logged_in
+  before_action :authorize_user!,  only: :show
+  before_action :set_organization, only: :show
+
   def new
+    @organization = Organization.new
+    @users_github_organizations = current_user.github_client
+      .users_organizations.collect { |org| [org.login, org.id] }
   end
 
   def create
+    github_organization_id = params[:org_id].to_i
+
+    if Organization.where(github_id: github_organization_id).present?
+      redirect_to new_organization_path, notice: 'Classroom has already been added'
+    elsif !current_user.github_client.is_organization_owner?(github_organization_id)
+      redirect_to new_organization_path, notice: 'You are not an owner of this organization'
+    else
+      login = current_user.github_client.organization(github_organization_id).login
+
+      @organization = Organization.new(login: login, github_id: github_organization_id)
+      @organization.users << current_user
+
+      if @organization.save
+        redirect_to dashboard_path, notice: "#{login} was successfully added"
+      else
+        redirect_to new_organization_path, error: "Could not create classroom"
+      end
+    end
   end
 
   def show
   end
 
-  def destroy
+  private
+
+  def authorize_user!
+    begin
+      has_user_id = Organization.find(params[:id]).user_ids.include?(current_user.id)
+    rescue ActiveRecord::RecordNotFound
+      has_user_id = false
+    end
+
+    unless has_user_id
+      redirect_to '/404.html'
+    end
+  end
+
+  def set_organization
+    @organization = Organization.find(params[:id])
   end
 end
