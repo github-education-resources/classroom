b881283301040bcaf73e5d3b0dab6d7486eabab7
Mark Tareshawty <mark@marktareshawty.com>
2015-06-01 11:28:09 -0400

    update #find_or_create_team to be class method

--- app/models/git_hub_team.rb
+++ app/models/git_hub_team.rb
@@ -1,18 +1,16 @@
 class GitHubTeam
   attr_reader :id, :name
 
-  def initialize(github_client, org_id, id=nil, name=nil)
-    @github_client = github_client
-    @org_id        = org_id
-    @id            = id
-    @name          = name
+  def initialize(id, name)
+    @id   = id
+    @name = name
   end
 
-  def find_or_create_team(team_id, team_name)
-    if team = @github_client.team(team_id)
-      GitHubTeam.new(@github_client, @org_id, team.id, team.name)
-    elsif team = @github_client.create_team(@org_id, { name: team_name, permission: 'push' })
-      GitHubTeam.new(@github_client, @org_id, team.id, team.name)
+  def self.find_or_create_team(github_client, org_id, team_id, team_name)
+    if team = github_client.team(team_id)
+      GitHubTeam.new(team.id, team.name)
+    elsif team = github_client.create_team(org_id, { name: team_name, permission: 'push' })
+      GitHubTeam.new(team.id, team.name)
     else
       NullGitHubTeam.new
     end
