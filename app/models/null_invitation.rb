d9c17afd0e9ab6dab5ab9d41027d59b1f125e18d
Mark Tareshawty <mark@marktareshawty.com>
2015-06-01 11:30:50 -0400

    added null invitation view and fixed typo Organization -> Invitation

--- app/models/null_invitation.rb
+++ app/models/null_invitation.rb
@@ -1,19 +1,11 @@
-class NullInvitation
-  attr_accessor :id, :key, :team_id, :title, :organization_id, :user_id
-
-  def user
-    User.none
-  end
-
-  def organization
-    Organization.none
-  end
-
-  def to_param
-    @key
-  end
-
+class NullInvitation < Invitation
   def to_partial_path
     'invitations/null'
+  end
+
+  protected
+
+  def assign_key
+    nil
   end
 end
