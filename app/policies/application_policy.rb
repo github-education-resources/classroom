cc464a32fb565f73bfd63e6b747ba4c66efe8ab1
Mark Tareshawty <mark@marktareshawty.com>
2015-06-20 19:04:03 -0400

    fixed application_policy missing

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,40 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def github_organization_admin?(user, organization_github_id)
+    user.github_client.organization_admin?(organization_github_id)
+  end
+end
--- app/policies/assignment_policy.rb
+++
@@ -1,5 +0,0 @@
-class AssignmentPolicy < ApplicationPolicy
-  def create?
-    github_organization_admin?(@user, @organization.github_id)
-  end
-end
