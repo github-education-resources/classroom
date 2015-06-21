72f3441b472d8cffbc97cd0826348f99f6d14c2e
Mark Tareshawty <mark@marktareshawty.com>
2015-06-20 20:01:33 -0400

    spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include Pundit
 
+  include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   helper_method :current_user, :logged_in?
