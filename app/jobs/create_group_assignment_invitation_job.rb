01208fe02d711a2837153cd29193c1738daa0c1e
Mark Tareshawty <mark@marktareshawty.com>
2015-06-23 16:42:06 -0400

    consistency

--- app/jobs/create_group_assignment_invitation_job.rb
+++ app/jobs/create_group_assignment_invitation_job.rb
@@ -2,7 +2,7 @@ class CreateGroupAssignmentInvitationJob < ActiveJob::Base
   queue_as :default
 
   def perform(group_assignment)
-    @invitation = group_assignment.build_group_assignment_invitation
-    @invitation.save!
+    invitation = group_assignment.build_group_assignment_invitation
+    invitation.save!
   end
 end
