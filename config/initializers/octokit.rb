c161429595387771fb1f4bdf3b90fb434c3c023f
Mark Tareshawty <mark@marktareshawty.com>
2015-06-24 17:24:21 -0500

    added logger and turned off cache control for request

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,4 +1,5 @@
 stack = Faraday::RackBuilder.new do |builder|
+  builder.response :logger
   builder.use Faraday::HttpCache, store: Rails.cache
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
