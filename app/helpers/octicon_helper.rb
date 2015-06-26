f0b24429916e8b2fcc47d6c2ce0ed40c9d74ed08
Mark Tareshawty <mark@marktareshawty.com>
2015-06-26 16:47:08 -0500

    WIP

--- app/helpers/octicon_helper.rb
+++ app/helpers/octicon_helper.rb
@@ -1,8 +1,18 @@
 module OcticonHelper
+  # Public: Get a mega-oction
+  #
+  # code - The String which contains the octicon type
+  #
+  # Returns a String which is a <span> and the octicon
   def mega_octicon(code)
     content_tag :span, '', class: "mega-octicon octicon-#{code.to_s.dasherize}"
   end
 
+  # Public: Get a oction
+  #
+  # code - The String which contains the octicon type
+  #
+  # Returns a String which is a <span> and the octicon
   def octicon(code)
     content_tag :span, '', class: "octicon octicon-#{code.to_s.dasherize}"
   end
