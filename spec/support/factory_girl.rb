0a460815996cefcad22c6ab8a1fbe845bce844e0
Mark Tareshawty <mark@marktareshawty.com>
2015-07-04 12:33:09 -0400

    factory girl...

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,13 +1,3 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      # Test factories in spec/factories are working.
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
-  end
 end
