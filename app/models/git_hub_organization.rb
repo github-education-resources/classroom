86036de1569e75b80f29a8fde5e26dc6de4b93ab
Mark Tareshawty <mark@marktareshawty.com>
2015-07-10 12:38:10 -0400

    GitHubOrganization `#create_team` description
    Add a description when creating a team

--- app/models/git_hub_organization.rb
+++ app/models/git_hub_organization.rb
@@ -24,7 +24,10 @@ class GitHubOrganization
   #
   def create_team(team_name)
     github_team = with_error_handling do
-      @client.create_team(@id, name: team_name, permission: 'push')
+      @client.create_team(@id,
+                          description: "#{team_name} created by GitHub Classroom",
+                          name: team_name,
+                          permission: 'push')
     end
 
     GitHubTeam.new(@client, github_team.id)
