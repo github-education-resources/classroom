5a0c95fc305fde9d28386a74fe022bd97517e7dd
Mark Tareshawty <mark@marktareshawty.com>
2015-07-10 12:12:07 -0400

    add shoulda-matchers for testing

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :test do
   gem 'database_cleaner'
   gem 'factory_girl_rails'
   gem 'faker'
+  gem 'shoulda-matchers', '3.0.0.rc1'
   gem 'vcr'
   gem 'webmock'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,4 +49,11 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  Shoulda::Matchers.configure do |c|
+    c.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
