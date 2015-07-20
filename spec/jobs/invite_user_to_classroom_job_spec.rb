6959fb49f34fac3f68de556a27ffa8ff09c5fd18
Mark Tareshawty <mark@marktareshawty.com>
2015-07-20 10:22:48 -0400

    rename

--- spec/jobs/invite_user_to_classroom_job_spec.rb
+++ spec/jobs/invite_user_to_classroom_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe InviteNewUserJob, type: :job do
+RSpec.describe InviteUserToClassroomJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
