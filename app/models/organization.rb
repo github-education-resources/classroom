4db2597d4c8744d92f8d9c5bba93cb0e9e0127e3
Mark Tareshawty <mark@marktareshawty.com>
2015-08-07 22:08:03 -0400

    Stop users from removing each other
    For an organization, there are times when removing a user should not be
    possible.
    
    * When there is only one active user left
    * When a user has created an assignment
    
    Using the before_remove callback we can check for this and stop the
    transaction if one of these things happen.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,12 +9,10 @@ class Organization < ActiveRecord::Base
   has_many :group_assignments, dependent: :destroy
   has_many :repo_accesses,     dependent: :destroy
 
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, presence: true, before_remove: [:verify_not_assignment_creator, :verify_not_last_user]
 
   validates :github_id, presence: true, uniqueness: true
   validates :title,     presence: true, uniqueness: { case_sensitive: false }
-
-  after_save :validate_minimum_number_of_users
 
   # Public
   #
@@ -32,10 +30,20 @@ class Organization < ActiveRecord::Base
 
   # Internal
   #
-  def validate_minimum_number_of_users
-    return if users.count > 0
-    error_message = 'must have at least one user'
-    errors.add(:users, error_message)
-    fail ActiveRecord::RecordInvalid.new(self), error_message
+  def verify_not_last_user(_user)
+    return if users.where(state: 'active').count > 1
+    errors.add(:base, 'This organization must have at least one active user')
+    fail ActiveRecord::RecordInvalid.new(self), 'unable to remove user'
+  end
+
+  # Internal
+  #
+  def verify_not_assignment_creator(user)
+    users_assignments = assignments.where(creator: user) + group_assignments.where(creator: user)
+
+    return unless users_assignments.present?
+
+    errors.add(:user, 'is the creator of one or more assignments, and cannot be removed at this time')
+    fail ActiveRecord::RecordInvalid.new(self), 'unable to remove user'
   end
 end
