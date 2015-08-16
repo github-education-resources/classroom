49a40143132462229f68566bba06b54ac88c1274
Mark Tareshawty <mark@marktareshawty.com>
2015-08-16 18:52:11 -0400

    use Rails secrets for bitly api key

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -2,5 +2,5 @@ Bitly.use_api_version_3
 
 Bitly.configure do |config|
   config.api_version = 3
-  config.access_token = ENV['BITLY_API_KEY']
+  config.access_token = Rails.application.secrets.bitly_api_key
 end
