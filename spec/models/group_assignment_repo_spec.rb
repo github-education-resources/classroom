299d5fac983fc0295831b3f500aeeb3f08b98484
Mark Tareshawty <mark@marktareshawty.com>
2015-08-22 15:52:34 -0400

    added tests for #creator

--- spec/models/group_assignment_repo_spec.rb
+++ spec/models/group_assignment_repo_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe GroupAssignmentRepo, type: :model do
     # it { is_expected.to validate_uniqueness_of(:group).scoped_to(:group_assignment) }
   end
 
-  describe 'callbacks', :vcr do
+  context 'with created objects', :vcr do
     let(:organization) { GitHubFactory.create_owner_classroom_org                                      }
     let(:repo_access)  { RepoAccess.create(user: organization.users.first, organization: organization) }
 
@@ -46,36 +46,39 @@ RSpec.describe GroupAssignmentRepo, type: :model do
       @group_assignment_repo.destroy if @group_assignment_repo
     end
 
-    describe 'before_validation' do
-      describe '#create_github_repository' do
-        it 'creates the repository on GitHub' do
-          assert_requested :post, github_url("/organizations/#{organization.github_id}/repos")
+    describe 'callbacks', :vcr do
+      describe 'before_validation' do
+        describe '#create_github_repository' do
+          it 'creates the repository on GitHub' do
+            assert_requested :post, github_url("/organizations/#{organization.github_id}/repos")
+          end
+        end
+      end
+
+      describe 'before_create' do
+        describe '#add_team_to_github_repository' do
+          it 'adds the team to the repository' do
+            github_repo = GitHubRepository.new(organization.github_client, @group_assignment_repo.github_repo_id)
+            assert_requested :put, github_url("/teams/#{group.github_team_id}/repos/#{github_repo.full_name}")
+          end
+        end
+      end
+
+      describe 'before_destroy' do
+        describe '#destroy_github_repository' do
+          it 'deletes the repository from GitHub' do
+            repo_id = @group_assignment_repo.github_repo_id
+            @group_assignment_repo.destroy
+
+            assert_requested :delete, github_url("/repositories/#{repo_id}")
+          end
         end
       end
     end
 
-    describe 'before_create' do
-      describe '#add_team_to_github_repository' do
-        it 'adds the team to the repository' do
-          github_repo = GitHubRepository.new(organization.github_client, @group_assignment_repo.github_repo_id)
-          assert_requested :put, github_url("/teams/#{group.github_team_id}/repos/#{github_repo.full_name}")
-        end
-      end
-
-      # describe '#push_starter_code' do
-      #   it 'pushes the starter code to the GitHub Repository' do
-      #   end
-      # end
-    end
-
-    describe 'before_destroy' do
-      describe '#destroy_github_repository' do
-        it 'deletes the repository from GitHub' do
-          repo_id = @group_assignment_repo.github_repo_id
-          @group_assignment_repo.destroy
-
-          assert_requested :delete, github_url("/repositories/#{repo_id}")
-        end
+    describe '#creator' do
+      it 'returns the group assignments creator' do
+        expect(@group_assignment_repo.creator).to eql(group_assignment.creator)
       end
     end
   end
