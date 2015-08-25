6330576c7772fd17136e5914047a169e84c4f2aa
Mark Tareshawty <mark@marktareshawty.com>
2015-08-24 23:08:41 -0400

    Set and authorize organization
    If the user goes to an organizations page, and they have not already
    been added to the org, Classroom for GitHub will see if they are an
    admin.
    
    If they are then it will add them to the org. Otherwise it will return
    a 404.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
-  before_action :authorize_organization_addition, only:   [:create]
-  before_action :set_organization,                except: [:new, :create]
-  before_action :set_users_github_organizations,  only:   [:new, :create]
+  before_action :authorize_organization_addition,                  only:   [:create]
+  before_action :set_organization, :authorize_organization_access, except: [:new, :create]
+  before_action :set_users_github_organizations,                   only:   [:new, :create]
 
   decorates_assigned :organization
 
@@ -51,26 +51,22 @@ class OrganizationsController < ApplicationController
   end
 
   def invite
-    @organization_owners = set_github_organization_owners
-  end
-
-  def invite_users
-    params[:github_owners].each do |login, id|
-      email = params[:github_owner_emails][login]
-      InviteUserToClassroomJob.perform_later(id, email, current_user, @organization)
-    end
-
-    respond_to do |format|
-      format.html { redirect_to @organization }
-    end
   end
 
   private
 
+  def authorize_organization_access
+    return if @organization.users.include?(current_user) || current_user.staff?
+
+    begin
+      github_organization.admin?(github_user.login) ? @organization.users << current_user : not_found
+    rescue
+      not_found
+    end
+  end
+
   def authorize_organization_addition
     github_organization = GitHubOrganization.new(current_user.github_client, new_organization_params[:github_id].to_i)
-    github_user         = GitHubUser.new(current_user.github_client)
-
     deny_access unless github_organization.admin?(github_user.login)
   end
 
@@ -90,22 +86,15 @@ class OrganizationsController < ApplicationController
       .merge(users: [current_user])
   end
 
-  def set_organization
-    @organization = Organization.find(params[:id])
+  def not_found
+    fail ActionController::RoutingError, 'Not Found'
   end
 
-  def set_github_organization_owners
-    organization_users_uids = @organization.users.pluck(:uid)
-    github_organization     = GitHubOrganization.new(current_user.github_client, @organization.github_id)
-
-    github_organization.organization_members(role: 'admin').delete_if do |member|
-      organization_users_uids.include?(member.id)
-    end
+  def set_organization
+    @organization = Organization.friendly.find(params[:id])
   end
 
   def set_users_github_organizations
-    github_user = GitHubUser.new(current_user.github_client)
-
     @users_github_organizations = github_user.admin_organization_memberships.map do |membership|
       unless Organization.find_by(github_id: membership.organization.id)
         [membership.organization.login, membership.organization.id]
@@ -117,5 +106,20 @@ class OrganizationsController < ApplicationController
     params
       .require(:organization)
       .permit(:title)
+  end
+
+  private
+
+  def github_organization
+    @existing_github_organization ||= GitHubOrganization.new(current_user.github_client, @organization.github_id)
+  end
+
+  def github_user
+    @github_user ||= GitHubUser.new(current_user.github_client)
+  end
+
+  def new_github_organization
+    @new_github_organization ||= GitHubOrganization.new(current_user.github_client,
+                                                        new_organization_params[:github_id].to_i)
   end
 end
