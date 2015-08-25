67e5695f9ccc7b633283833a84c78b3ccf622859
Mark Tareshawty <mark@marktareshawty.com>
2015-08-25 14:56:06 -0400

    copy paste error

--- app/controllers/group_assignment_invitations_controller.rb
+++ app/controllers/group_assignment_invitations_controller.rb
@@ -1,6 +1,4 @@
 class GroupAssignmentInvitationsController < InvitationsController
-  skip_before_action :set_organization, :authorize_organization_access
-
   def show
     @groups = @invitation.groups.map { |group| [group.title, group.id] }
   end
