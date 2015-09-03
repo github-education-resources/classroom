9736a0e92bf1ffac9859eb56ab0ed61bc89eda53
Mark Tareshawty <mark@marktareshawty.com>
2015-09-03 10:11:50 -0400

    make use or ActiveRecord, thanks @johndbritton!

--- app/services/group_assignment_service.rb
+++ app/services/group_assignment_service.rb
@@ -1,22 +1,15 @@
 class GroupAssignmentService
   def initialize(new_group_assignment_params, new_grouping_params)
+    @grouping_id                 = new_group_assignment_params[:grouping_id]
     @new_group_assignment_params = new_group_assignment_params
     @new_grouping_params         = new_grouping_params
   end
 
   def create_group_assignment
-    grouping = Grouping.find_by(id: @new_group_assignment_params[:grouping_id])
-    grouping = Grouping.new(@new_grouping_params) unless grouping.present?
+    group_assignment          = GroupAssignment.new(@new_group_assignment_params)
+    group_assignment.grouping = Grouping.where(id: @grouping_id).first_or_create!(@new_grouping_params)
 
-    group_assignment = GroupAssignment.new(@new_group_assignment_params)
     group_assignment.build_group_assignment_invitation
-
-    if grouping.new_record?
-      grouping.save!
-      group_assignment.grouping = grouping
-    end
-
     group_assignment.save!
-    group_assignment
   end
 end
