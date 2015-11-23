6185f2f424782930088e07308b64ba5de79e4a73
Chris Ewald <chrisewald@gmail.com>
2015-11-23 18:18:14 -0500

    fix N+1 errors from organizations Factory

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -2,6 +2,13 @@ FactoryGirl.define do
   factory :organization do
     title      { "#{Faker::Company.name} Class" }
     github_id  { rand(1..1_000_000) }
-    users      { [FactoryGirl.create(:user)] }
+
+    transient do
+      users_count 1
+    end
+
+    after(:build) do |organization, evaluator|
+      create_list(:user, evaluator.users_count, organizations: [organization])
+    end
   end
 end
