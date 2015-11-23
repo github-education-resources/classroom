50a5de824daa93b6f88bbbd6975969b3672a817e
Chris Ewald <chrisewald@gmail.com>
2015-11-23 16:51:35 -0500

    add bullet RSpec support config

---
+++ spec/support/bullet.rb
@@ -0,0 +1,18 @@
+if defined?(Bullet) && Bullet.enable?
+  RSpec.configure do |config|
+    config.before(:each) do
+      Bullet.start_request
+    end
+
+    config.around(:each, type: :controller) do |example|
+      Bullet.unused_eager_loading_enable = false
+      example.run
+      Bullet.unused_eager_loading_enable = true
+    end
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
+end
