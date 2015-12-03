b82726eaa6720df8a3d90ec6368d6994798f0d2b
Mark Tareshawty <mark@marktareshawty.com>
2015-12-03 11:51:10 -0500

    add specs

--- spec/controllers/stafftools/organizations_controller_spec.rb
+++ spec/controllers/stafftools/organizations_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Stafftools::OrganizationsController, type: :controller do
-  let(:user)    { GitHubFactory.create_owner_classroom_org.users.first }
+  let(:organization) { GitHubFactory.create_owner_classroom_org }
+  let(:user)         { organization.users.first }
 
   before(:each) do
     sign_in(user)
@@ -10,10 +11,23 @@ RSpec.describe Stafftools::OrganizationsController, type: :controller do
   describe 'GET #show', :vcr do
     context 'as an unauthorized user' do
       it 'returns a 404' do
+        expect { get :show, id: organization.id }.to raise_error(ActionController::RoutingError)
       end
     end
 
     context 'as an authorized user' do
+      before do
+        user.update_attributes(site_admin: true)
+        get :show, id: organization.id
+      end
+
+      it 'succeeds' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'sets the organization' do
+        expect(assigns(:organization).id).to eq(organization.id)
+      end
     end
   end
 end
