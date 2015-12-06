893181ece8a0819a81a74211277ed4c48f64aa5e
Mark Tareshawty <mark@marktareshawty.com>
2015-12-05 23:50:29 -0500

    Closes #295

---
+++ spec/models/null_git_hub_resource_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe NullGitHubResource do
+  subject { described_class.new }
+
+  describe '#null?' do
+    it 'returns true' do
+      expect(subject.null?).to be(true)
+    end
+  end
+end
