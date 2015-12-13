b8dff7c52cedcaab45005012b47af5cb52a68006
Jason Long <jlong@github.com>
2015-12-13 11:01:10 -0500

    Spacing twerks for search results

--- app/helpers/stafftools_resource_render_helper.rb
+++ app/helpers/stafftools_resource_render_helper.rb
@@ -1,9 +1,4 @@
 module StafftoolsResourceRenderHelper
-  def render_stafftools_resource_header(current_resource, previous_resource)
-    return if (current_resource._data['_type'] == previous_resource._data['_type'])
-    content_tag :h3, current_resource._data['_type'].capitalize.humanize.pluralize
-  end
-
   def render_stafftools_resource(resource)
     type = resource._data['_type']
     render partial: "stafftools/#{type.pluralize}/#{type}", locals: { type.to_sym => resource }
