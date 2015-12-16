de6fc9b716dff9f789bd75b0a15095f4384e4557
Mark Tareshawty <mark@marktareshawty.com>
2015-12-16 16:50:26 -0500

    finished specs

--- spec/controllers/stafftools/groupings_controller_spec.rb
+++ spec/controllers/stafftools/groupings_controller_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Stafftools::GroupingsController, type: :controller do
-  let(:user)    { GitHubFactory.create_owner_classroom_org.users.first }
+  let(:user)         { GitHubFactory.create_owner_classroom_org.users.first }
+  let(:organization) { user.organizations.first                             }
+
+  let(:grouping) { Grouping.create(organization: organization, title: 'Grouping 1') }
 
   before(:each) do
     sign_in(user)
@@ -10,10 +13,23 @@ RSpec.describe Stafftools::GroupingsController, type: :controller do
   describe 'GET #show', :vcr do
     context 'as an unauthorized user' do
       it 'returns a 404' do
+        expect { get :show, id: grouping.id }.to raise_error(ActionController::RoutingError)
       end
     end
 
     context 'as an authorized user' do
+      before do
+        user.update_attributes(site_admin: true)
+        get :show, id: grouping.id
+      end
+
+      it 'succeeds' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'sets the Grouping' do
+        expect(assigns(:grouping).id).to eq(grouping.id)
+      end
     end
   end
 end
