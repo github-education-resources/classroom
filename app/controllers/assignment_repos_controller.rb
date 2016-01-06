cd3c3b77c0b27c68035c5cee128a39cb06fd88a9
Mark Tareshawty <mark@marktareshawty.com>
2016-01-06 00:01:15 -0500

    move @assignment_repo to assignment_repo helper method

--- app/controllers/assignment_repos_controller.rb
+++ app/controllers/assignment_repos_controller.rb
@@ -1,10 +1,8 @@
 class AssignmentReposController < ApplicationController
   include OrganizationAuthorization
 
-  before_action :set_assignment_repo
-
   def recreate_repo
-    if @assignment_repo.update_attributes(github_repo_id: nil)
+    if assignment_repo.update_attributes(github_repo_id: nil)
       flash[:success] = 'Repository was successfully recreated'
     else
       flash[:error] = 'Repository failed to be recreated'
@@ -16,16 +14,17 @@ class AssignmentReposController < ApplicationController
   private
 
   def assignment
-    @assignment ||= @assignment_repo.assignment
+    @assignment ||= assignment_repo.assignment
   end
   helper_method :assignment
 
   def organization
-    @organization ||= @assignment_repo.organization
+    @organization ||= assignment_repo.organization
   end
   helper_method :organization
 
-  def set_assignment_repo
-    @assignment_repo = AssignmentRepo.find(params[:assignment_repo_id])
+  def assignment_repo
+    @assignment_repo ||= AssignmentRepo.find(params[:assignment_repo_id])
   end
+  helper_method :assignment_repo
 end
