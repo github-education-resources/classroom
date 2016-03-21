cebb41adeaed0076fe1daa21795e8caf8673af9f
Orthodex <ishaanmalhi.me>
2016-03-21 17:32:51 +0530

    Removed duplicate files, fixed indentations

--- lib/not_found_exception.rb
+++ lib/not_found_exception.rb
@@ -1,11 +1,11 @@
 class NotFoundException < ActionDispatch::PublicExceptions
 	def call(env)
-    status = env["PATH_INFO"][1..-1]
+    	status = env["PATH_INFO"][1..-1]
 
-    if status == "404"
-      Rails.application.routes.call(env)
-    else
-      super
-    end
-  end
+    	if status == "404"
+      		Rails.application.routes.call(env)
+    	else
+      		super
+    	end
+  	end
 end
