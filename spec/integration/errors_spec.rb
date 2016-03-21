4f6979a70911e841b1cef0a4a70ed5facee0941f
Orthodex <ishaanmalhi.me>
2016-03-21 17:21:18 +0530

    Added factories to errors_spec

--- spec/integration/errors_spec.rb
+++ spec/integration/errors_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Error page", :type => :request do
+RSpec.describe "Error 404", type: :request do
+
+  let(:organization)  { GitHubFactory.create_owner_classroom_org }
+  let(:user)          { organization.users.first                 }
+
   before(:all) do
     Rails.application.config.action_dispatch.show_exceptions = true
     Rails.application.config.consider_all_requests_local = false
@@ -14,11 +18,19 @@ RSpec.describe "Error page", :type => :request do
   describe 'GET #error404' do
     it 'returns not found error 404' do
       get '/404'
-      expect(response.status).to eq(404)
+      expect(response).to redirect_to(not_found_path)
     end
   end
 
   describe 'GET /any-other-page' do
+    
+    before(:each) do
+        get url_for(organization)
+        get response.redirect_url # http://www.example.com/login
+        get response.redirect_url # http://www.example.com/auth/github?scope=user%3Aemail%2Crepo%2Cdelete_repo%2Cadmin%3Aorg
+        get response.redirect_url # http://www.example.com/auth/github/callback
+    end
+
     it 'shows invalid invitation message' do
       get '/assignment-invitations/invalid-link'
       expect(response.status).to eq(404)
