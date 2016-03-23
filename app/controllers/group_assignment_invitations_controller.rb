ad130e9f8b56d3a526ed516adc33b979e48497dc
dinever <dinever@live.cn>
2016-03-23 10:21:19 -0400

    [style] Use `raise` to signal exceptions.

--- app/controllers/group_assignment_invitations_controller.rb
+++ app/controllers/group_assignment_invitations_controller.rb
@@ -62,7 +62,7 @@ class GroupAssignmentInvitationsController < ApplicationController
   def check_group_members_not_exceed(group)
     return unless group.present? && group_assignment.present? && group_assignment.max_members.present?
     return unless group.repo_accesses.count >= group_assignment.max_members
-    fail NotAuthorized, "This team has reached its maximum member limit of #{group_assignment.max_members}."
+    raise NotAuthorized, "This team has reached its maximum member limit of #{group_assignment.max_members}."
   end
 
   def decorated_group_assignment_repo
