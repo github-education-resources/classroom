f56f958b1bb480fb836fa01c969ab2efdeb8f0fd
Mark Tareshawty <tarebyte@github.com>
2016-03-24 21:44:21 -0400

    not needed

--- lib/github.rb
+++ lib/github.rb
@@ -2,7 +2,4 @@ module GitHub
   class Error < StandardError; end
   class Forbidden < Error; end
   class NotFound < Error; end
-
-  autoload :APIHeaders, 'github/api_headers'
-  autoload :Errors,     'github/errors'
 end
