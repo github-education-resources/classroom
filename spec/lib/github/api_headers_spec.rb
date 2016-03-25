6ca23fa468dcf9497ac8d963ef2cc3bbae7ce65d
Mark Tareshawty <tarebyte@github.com>
2016-03-24 22:30:53 -0400

    add tests

---
+++ spec/lib/github/api_headers_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe GitHub::APIHeaders do
+  subject { described_class }
+
+  describe '#no_cache_no_store' do
+    it "returns the 'Cache-Control' => 'no-cache, no-store'" do
+      expect(described_class.no_cache_no_store).to eql('Cache-Control' => 'no-cache, no-store')
+    end
+  end
+end
