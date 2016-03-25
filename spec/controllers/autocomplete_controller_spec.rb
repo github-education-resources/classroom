9ed8eb8676ade5098cec5eee2322be2e62307435
Cheng-Yu Hsu <crazyming411@gmail.com>
2016-03-25 22:13:16 +0800

    Refine specs for `AutocompleteController`.

--- spec/controllers/autocomplete_controller_spec.rb
+++ spec/controllers/autocomplete_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'set'
+
 RSpec.describe AutocompleteController, type: :controller do
   let(:organization) { GitHubFactory.create_owner_classroom_org }
   let(:user)         { organization.users.first                 }
@@ -9,30 +10,47 @@ RSpec.describe AutocompleteController, type: :controller do
   end
 
   describe 'GET #github_repos', :vcr do
-    it 'returns success and returns a subset of repos of the user' do
-      get :github_repos
+    context 'no search query is passed' do
+      before do
+        get :github_repos
+      end
 
-      expect(response).to have_http_status(:success)
+      it 'returns success' do
+        expect(response).to have_http_status(:success)
+      end
 
-      returned_repo_ids = assigns(:repos).map { |repo| repo[:id] }.to_set
-      actual_repo_ids = user.github_client.repos.map { |repo| repo[:id] }.to_set
-      expect(returned_repo_ids).to be_subset(actual_repo_ids)
+      it 'returns a subset of repos that the current user has access to' do
+        returned_repo_ids = assigns(:repos).map { |repo| repo[:id] }.to_set
+        actual_repo_ids = user.github_client.repos.map { |repo| repo[:id] }.to_set
+        expect(returned_repo_ids).to be_subset(actual_repo_ids)
+      end
 
-      expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
+      it 'renders correct template' do
+        expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
+      end
     end
 
-    it 'returns success and returns search results of a query' do
-      get :github_repos, query: 'rails/rails'
-      expect(response).to have_http_status(:success)
+    context 'a search query is passed' do
+      before do
+        get :github_repos, query: 'rails/rails'
+      end
 
-      returned_repo_ids = assigns(:repos).map { |repo| repo[:id] }.to_set
-      actual_repo_ids = user
-                        .github_client
-                        .search_repos('rails user:rails fork:true')[:items]
-                        .map { |repo| repo[:id] }.to_set
-      expect(returned_repo_ids).to be_subset(actual_repo_ids)
+      it 'returns success' do
+        expect(response).to have_http_status(:success)
+      end
 
-      expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
+      it 'returns search results of a query' do
+        returned_repo_ids = assigns(:repos).map { |repo| repo[:id] }.to_set
+        actual_repo_ids = user
+                          .github_client
+                          .search_repos('rails user:rails fork:true')[:items]
+                          .map { |repo| repo[:id] }.to_set
+        expect(returned_repo_ids).to be_subset(actual_repo_ids)
+      end
+
+      it 'renders correct template' do
+        expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
+      end
     end
   end
 end
