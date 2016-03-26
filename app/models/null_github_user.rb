ad24b2f7c3c94926f048753dae49d43be7d48e4b
Mark Tareshawty <tarebyte@github.com>
2016-03-26 17:15:35 -0400

    remove user_decorator
    - created GitHubResource which all GitHub models will inherit from,
    this will create the attribute methods on creation of the GitHub model
    
    - Update the NullGitHubResource to act the same way as GitHubResource
    
    - Update the stafftools index to use the new methods

--- app/models/null_github_user.rb
+++ app/models/null_github_user.rb
@@ -1,13 +1,13 @@
 class NullGitHubUser < NullGitHubResource
-  def html_url
-    '#'
-  end
 
-  def login
-    'ghost'
-  end
-
-  def name
-    'Deleted user'
+  # internal
+  def null_github_user_attributes
+    # Courtesy of https://api.github.com/users/ghost
+    {
+      'login':      'ghost',
+      'avatar_url': 'https://avatars.githubusercontent.com/u/10137?v=3',
+      'html_url':   'https://github.com/ghost',
+      'name':       'Deleted user'
+    }
   end
 end
