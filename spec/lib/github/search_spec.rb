f8431e0b276fd032d724c7097acf0f2e58f4c22f
Mark Tareshawty <tarebyte@github.com>
2016-03-26 14:28:27 -0400

    add tests for search client
    extract tests from autocomplete controller to search

---
+++ spec/lib/github/search_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe GitHub::Search do
+  let(:user) { GitHubFactory.create_classroom_student }
+
+  subject { described_class.new(user.token, user.uid) }
+
+  describe '#search_github_repositories', :vcr do
+    context 'search parameters are not defined' do
+      it 'queries the users repositories' do
+        search_url = "/user/#{user.uid}/repos?page=1&per_page=10&sort=updated"
+        subject.search_github_repositories('')
+        expect(WebMock).to have_requested(:get, github_url(search_url))
+      end
+
+      it 'returns a subset of repos that the user has access to' do
+        repo_ids = subject.search_github_repositories('').map(&:id).to_set
+        expect_repo_ids = user.github_client.repos.map(&:id).to_set
+
+        expect(repo_ids).to be_subset(expect_repo_ids)
+      end
+    end
+
+    context 'search parameters are defined' do
+      it 'queries the github repo search api' do
+        search_url = 'search/repositories?page=1&per_page=10&q=rails%20in:name%20fork:true%20user:rails'
+        subject.search_github_repositories('rails/rails')
+
+        expect(WebMock).to have_requested(:get, github_url(search_url))
+      end
+
+      it 'returns search results of a query' do
+        returned_repo_ids = subject.search_github_repositories('rails/rails').map(&:id).to_set
+        actual_repo_ids = user
+                          .github_client
+                          .search_repos('rails user:rails fork:true')[:items]
+                          .map(&:id).to_set
+
+        expect(returned_repo_ids).to be_subset(actual_repo_ids)
+      end
+    end
+  end
+end
