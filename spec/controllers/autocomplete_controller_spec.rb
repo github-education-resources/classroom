71d445710ee2e6228c4001dcdd29b3e76bfa4c54
Mark Tareshawty <tarebyte@github.com>
2016-03-28 09:31:52 -0400

    remove array expectation because we are not passing the results in locally to the partial

--- spec/controllers/autocomplete_controller_spec.rb
+++ spec/controllers/autocomplete_controller_spec.rb
@@ -10,40 +10,16 @@ RSpec.describe AutocompleteController, type: :controller do
   end
 
   describe 'GET #github_repos', :vcr do
-    context 'no search query is passed' do
-      before do
-        get :github_repos
-      end
-
-      it 'returns success' do
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'returns an array of repos' do
-        expect(assigns(:repos)).to be_kind_of(Array)
-      end
-
-      it 'renders correct template' do
-        expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
-      end
+    before do
+      get :github_repos
     end
 
-    context 'a search query is passed' do
-      before do
-        get :github_repos, query: 'rails/rails'
-      end
+    it 'returns success' do
+      expect(response).to have_http_status(:success)
+    end
 
-      it 'returns success' do
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'returns search results of a query' do
-        expect(assigns(:repos)).to be_kind_of(Array)
-      end
-
-      it 'renders correct template' do
-        expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
-      end
+    it 'renders correct template' do
+      expect(response).to render_template(partial: 'autocomplete/_repository_suggestions')
     end
   end
 end
