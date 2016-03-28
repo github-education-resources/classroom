029ccc2d8f6b216a72c5f955bd0cc60d99d41ea4
Mark Tareshawty <tarebyte@github.com>
2016-03-28 09:09:36 -0400

    allow the method to return the results with an error message

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,10 +1,12 @@
 class AutocompleteController < ApplicationController
   def github_repos
-    github_search = GitHub::Search.new(current_user.token, current_user.uid, auto_paginate: false)
-    @repos        = github_search.search_github_repositories(params[:query])
+    github_search          = GitHub::Search.new(current_user.token, current_user.uid, auto_paginate: false)
+    results, error_message = github_search.search_github_repositories(params[:query])
 
     respond_to do |format|
-      format.html { render partial: 'autocomplete/repository_suggestions', locals: { repos: @repos } }
+      format.html do
+        render partial: 'autocomplete/repository_suggestions', locals: { repos: results, error_message: error_message }
+      end
     end
   end
 end
