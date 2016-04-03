233b39084b8af322054180fb227a5654783342f8
Mark Tareshawty <tarebyte@github.com>
2016-04-03 15:30:03 -0400

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  class NotAuthorized < StandardError
-  end
+  class NotAuthorized < StandardError; end
 
   helper_method :current_user, :logged_in?, :staff?, :true_user
 
