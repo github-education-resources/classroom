75dcec72b402943a4fb583eabaee2735fdc18af3
Mark Tareshawty <tarebyte@github.com>
2016-04-03 15:29:58 -0400

    update controller concerns with new keyword args

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -7,8 +7,8 @@ module StarterCode
 
     if repo_name =~ %r{^[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+$}
       begin
-        github_repository = GitHubRepository.new(current_user.github_client, nil)
-        github_repository.repository(repo_name).id
+        github_repository = GitHubRepository.new(id: nil, access_token: current_user.access_token)
+        github_repository.repository(full_name: repo_name).id
       rescue ArgumentError => err
         raise GitHub::Error, err.message
       end
@@ -21,7 +21,7 @@ module StarterCode
   def validate_starter_code_repository_id(repo_id)
     if repo_id.is_a?(Integer) || (repo_id.is_a?(String) && repo_id =~ /^[0-9]+$/)
       begin
-        GitHubRepository.new(current_user.github_client, repo_id.to_i).repository.id
+        GitHubRepository.new(id: repo_id.to_i, access_token: current_user.access_token).repository.id
       rescue ArgumentError => err
         raise GitHub::Error, err.message
       end
