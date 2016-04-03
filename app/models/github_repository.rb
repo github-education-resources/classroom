8662b9f0f041c64a9c881fe7750411ef9e161e08
Mark Tareshawty <tarebyte@github.com>
2016-04-03 15:26:15 -0400

    update GitHub models
    They will inherit from GitHubResource with attributes. All methods are
    now using keyword arguments.

--- app/models/github_repository.rb
+++ app/models/github_repository.rb
@@ -1,39 +1,31 @@
-class GitHubRepository
-  attr_reader :id
-
-  def initialize(client, id)
-    @client = client
-    @id     = id
-  end
-
-  # Public
-  #
-  def add_collaborator(collaborator)
+class GitHubRepository < GitHubResource
+  def add_collaborator(github_user:)
     GitHub::Errors.with_error_handling do
-      @client.add_collaborator(@id, collaborator)
+      client.add_collaborator(id, github_user.login)
     end
   end
 
-  # Public
-  #
-  def full_name
-    GitHub::Errors.with_error_handling { @client.repository(@id).full_name }
+  def disabled?
+    return @disabled if defined?(@disabled)
+    @disabled = full_name == 'Deleted repository'
   end
 
-  # Public
-  #
-  def get_starter_code_from(source)
+  def get_starter_code_from(source:)
     GitHub::Errors.with_error_handling do
-      credentials = { vcs_username: @client.login, vcs_password: @client.access_token }
-      @client.start_source_import(@id, 'git', "https://github.com/#{source.full_name}", credentials)
+      credentials = { vcs_username: client.login, vcs_password: client.access_token }
+      client.start_source_import(id, 'git', "https://github.com/#{source.full_name}", credentials)
     end
   end
 
-  # Public
-  #
-  def repository(full_repo_name = nil)
+  def repository(full_name: nil)
     GitHub::Errors.with_error_handling do
-      @client.repository(full_repo_name || @id)
+      client.repository(full_name || id)
     end
+  end
+
+  private
+
+  def github_attributes
+    %w(full_name html_url)
   end
 end
--- app/models/github_team.rb
+++ app/models/github_team.rb
@@ -1,46 +1,41 @@
-class GitHubTeam
-  attr_reader :id
-
-  def initialize(client, id)
-    @client = client
-    @id     = id
-  end
-
-  # Public
-  #
-  def add_team_membership(new_user_github_login)
+class GitHubTeam < GitHubResource
+  def add_team_membership(github_user:)
     GitHub::Errors.with_error_handling do
-      @client.add_team_membership(@id, new_user_github_login)
+      client.add_team_membership(id, github_user.login)
     end
   end
 
-  # Public
-  #
-  def remove_team_membership(user_github_login)
+  def remove_team_membership(github_user:)
     GitHub::Errors.with_error_handling do
-      @client.remove_team_membership(@id, user_github_login)
+      client.remove_team_membership(id, github_user.login)
     end
   end
 
-  # Publc
-  #
-  def add_team_repository(full_name)
+  def add_team_repository(github_repository:)
     GitHub::Errors.with_error_handling do
-      unless @client.add_team_repository(@id, full_name)
+      unless client.add_team_repository(id, github_repository.full_name)
         raise GitHub::Error, 'Could not add team to the GitHub repository'
       end
     end
   end
 
-  def team(options = {})
-    GitHub::Errors.with_error_handling { @client.team(@id, options) }
+  def team(**options)
+    GitHub::Errors.with_error_handling { client.team(id, options) }
   end
 
-  # Public
-  #
-  def team_repository?(full_name)
+  def team_repository?(github_repository:)
     GitHub::Errors.with_error_handling do
-      @client.team_repository?(@id, full_name)
+      client.team_repository?(id, github_repository.full_name)
     end
+  end
+
+  def disabled?
+    return @disabled if defined?(@disabled)
+    @disabled = (slug == 'ghost')
+  end
+
+  # Internal
+  def github_attributes
+    %w(name slug)
   end
 end
