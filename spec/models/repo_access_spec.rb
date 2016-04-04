02f20aa44016d240fe2d60c0375e2dffe2b8cbe6
Mark Tareshawty <tarebyte@github.com>
2016-04-03 21:11:56 -0400

    update repo_access_spec

--- spec/models/repo_access_spec.rb
+++ spec/models/repo_access_spec.rb
@@ -16,9 +16,7 @@ RSpec.describe RepoAccess, type: :model do
     describe 'before_validation' do
       describe '#add_membership_to_github_organization' do
         it 'adds the users membership to the GitHub organization' do
-          student_github_login = GitHubUser.new(student.github_client, student.uid).login
-          add_membership_github_url = github_url("/orgs/#{organization.title}/memberships/#{student_github_login}")
-
+          add_membership_github_url = github_url("/orgs/#{organization.title}/memberships/#{student.github_user.login}")
           expect(WebMock).to have_requested(:put, add_membership_github_url)
         end
       end
@@ -36,8 +34,7 @@ RSpec.describe RepoAccess, type: :model do
           it 'removes the user' do
             @repo_access.destroy
 
-            student_github_login = GitHubUser.new(student.github_client, student.uid).login
-            delete_request_url   = "/organizations/#{organization.github_id}/members/#{student_github_login}"
+            delete_request_url = "/organizations/#{organization.github_id}/members/#{student.github_user.login}"
             expect(WebMock).to have_requested(:delete, github_url(delete_request_url))
           end
         end
@@ -47,8 +44,7 @@ RSpec.describe RepoAccess, type: :model do
             owner_repo_access = RepoAccess.create(user: organization.users.first, organization: organization)
             owner_repo_access.destroy
 
-            student_github_login = GitHubUser.new(student.github_client, student.uid).login
-            delete_request_url   = "/organizations/#{organization.github_id}/members/#{student_github_login}"
+            delete_request_url = "/organizations/#{organization.github_id}/members/#{student.github_user.login}"
             expect(WebMock).to_not have_requested(:delete, github_url(delete_request_url))
           end
         end
