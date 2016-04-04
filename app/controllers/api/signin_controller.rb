5ba1c973e6924298c0143c415036da611a1beb05
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2016-04-04 15:43:36 +0530

    Added authentication api

---
+++ app/controllers/api/signin_controller.rb
@@ -0,0 +1,12 @@
+class API::SigninController < ApplicationController
+
+  def index
+    if logged_in?
+      respond_to do |format|
+        format.json { render :file => "/api/signin/loggedin.json.erb", :content_type => 'application/json' }
+      end
+    else
+      redirect_to login_path
+    end
+  end
+end
---
+++ spec/controllers/api/signin_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe API::SigninController, type: :controller do
+  describe "GET #index"do
+    context 'unauthenticated request' do
+      before do
+        sign_out
+      end
+
+      it "redirects to login path" do
+        get :index
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+end
