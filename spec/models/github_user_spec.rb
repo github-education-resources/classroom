ae7a2c596eec6fb516a2aee8662c1a1beac40034
Mark Tareshawty <tarebyte@github.com>
2016-04-03 23:58:18 -0400

    update github_user_spec and cassettes

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -1,53 +1,53 @@
 require 'rails_helper'
 
 describe GitHubUser do
+  it_behaves_like 'a GitHubResource descendant with attributes'
+
   before do
     Octokit.reset!
-    @client = oauth_client
   end
 
-  let(:github_user)       { GitHubUser.new(@client, @client.user.id) }
-  let(:other_user)        { GitHubFactory.create_classroom_student   }
-  let(:other_github_user) { GitHubUser.new(@client, other_user.uid)  }
-
-  describe '#login', :vcr do
-    it 'gets the login of the user' do
-      user = @client.user
-
-      expect(github_user.login).to eql(user.login)
-      expect(WebMock).to have_requested(:get, github_url("/user/#{user.id}"))
+  context 'with GitHub user', :vcr do
+    let(:organization) { GitHubFactory.create_owner_classroom_org }
+    let(:github_user) do
+      user = organization.users.first
+      user.github_user
     end
 
-    it 'gets the login of another user' do
-      user = @client.user(other_user.uid)
-
-      expect(other_github_user.login).to eql(user.login)
-      expect(WebMock).to have_requested(:get, github_url("/user/#{user.id}")).twice
-    end
-  end
-
-  describe '#organization_memberships', :vcr do
-    it 'returns an array of organizations that the user belongs to' do
-      organization_memberships = github_user.organization_memberships
-
-      expect(WebMock).to have_requested(:get, github_url('/user/memberships/orgs?state=active'))
-      expect(organization_memberships).to be_kind_of(Array)
-    end
-  end
-
-  describe '#user', :vcr do
-    it 'gets the client users info' do
-      id = @client.user.id
-
-      expect(github_user.user.id).to eql(id)
-      expect(WebMock).to have_requested(:get, github_url("/user/#{id}"))
+    let(:other_github_user) do
+      other_user = GitHubFactory.create_classroom_student
+      other_user.github_user
     end
 
-    it 'gets another users info' do
-      id = @client.user(other_user.uid).id
+    describe '#accept_membership_to' do
+    end
 
-      expect(other_github_user.user.id).to eql(id)
-      expect(WebMock).to have_requested(:get, github_url("/user/#{id}")).twice
+    describe '#active_admin?' do
+      it 'verifies if the user is an admin of the organization' do
+        expect(github_user.active_admin?(github_organization: organization.github_organization)).to be_truthy
+      end
+    end
+
+    describe '#client_scopes' do
+      it 'returns an Array of scopes' do
+        expect(github_user.client_scopes).to eq(%w(admin:org delete_repo repo user:email))
+      end
+    end
+
+    describe '#disabled?' do
+      it 'returns true if the user is not present' do
+        missing_github_user = GitHubUser.new(id: 1_000_000_000, access_token: github_user.access_token)
+        expect(missing_github_user.disabled?).to be_truthy
+      end
+    end
+
+    describe '#organization_memberships', :vcr do
+      it 'returns an array of organizations that the user belongs to' do
+        organization_memberships = github_user.organization_memberships
+
+        expect(WebMock).to have_requested(:get, github_url('/user/memberships/orgs?per_page=100&state=active'))
+        expect(organization_memberships).to be_kind_of(Array)
+      end
     end
   end
 end
