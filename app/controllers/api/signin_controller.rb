48a6cb337f3bc411b45245439f2a2f9ad35b205d
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2016-04-04 15:54:17 +0530

    Fixes issues with linter

--- app/controllers/api/signin_controller.rb
+++ app/controllers/api/signin_controller.rb
@@ -1,9 +1,8 @@
 class API::SigninController < ApplicationController
-
   def index
     if logged_in?
       respond_to do |format|
-        format.json { render :file => "/api/signin/loggedin.json.erb", :content_type => 'application/json' }
+        format.json { render 'file' => '/api/signin/loggedin.json.erb', 'content_type' => 'application/json' }
       end
     else
       redirect_to login_path
--- spec/controllers/api/signin_controller_spec.rb
+++ spec/controllers/api/signin_controller_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe API::SigninController, type: :controller do
-  describe "GET #index"do
+  describe 'GET #index' do
     context 'unauthenticated request' do
       before do
         sign_out
       end
 
-      it "redirects to login path" do
+      it 'redirects to login path' do
         get :index
         expect(response).to redirect_to(login_path)
       end
     end
   end
-
 end
