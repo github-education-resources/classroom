caadb25385294171ebc5cae70eb22f8907dcd43e
dinever <dingpeixuan911@gmail.com>
2016-04-14 12:54:32 -0400

    Move `GitHub::Repository.present?` to `GitHubRepository.present?`

--- app/models/concerns/nameable.rb
+++ app/models/concerns/nameable.rb
@@ -5,8 +5,7 @@ module Nameable
   def github_repo_name(assignment_slug, user_login)
     suffix_number = 0
     suffix_number += 1 while
-        GitHub::Repository.present?(organization.github_client,
-                                    full_name(assignment_slug, user_login, suffix_number))
+        GitHubRepository.present?(organization.github_client, full_name(assignment_slug, user_login, suffix_number))
     suffixed_repo_name(assignment_slug, user_login, suffix_number)
   end
 
