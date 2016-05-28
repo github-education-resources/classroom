a85908c196fac9469a609d505a47a10ea39bf9ad
Mark Tareshawty <tarebyte@github.com>
2016-05-28 12:11:58 -0400

    Use ActionDispatch::Session::CacheStore instead of :cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_classroom_session'
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 2.hours
