4c2ae6edb93ea004eb6b6d89aea0a50741382dcd
Mark Tareshawty <tarebyte@github.com>
2016-06-12 09:22:00 -0400

    remove other duplicate migrations

--- db/migrate/20160416105605_add_last_active_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastActiveAtToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :last_active_at, :timestamp
-  end
-end
--- db/migrate/20160605003409_set_user_last_active_at_value.rb
+++
@@ -1,8 +0,0 @@
-class SetUserLastActiveAtValue < ActiveRecord::Migration
-  def change
-    User.find_each(batch_size: 100) do |user|
-      user.update_columns(last_active_at: user.updated_at)
-      User.update_index('stafftools#user') { user }
-    end
-  end
-end
