5c1ad70bbab0615d2b795eb5a767950754596b6c
Mark Tareshawty <tarebyte@github.com>
2016-06-12 22:30:59 -0400

    derp

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -21,7 +21,7 @@ module StarterCode
     valid_integer_or_number_string = repo_id.is_a?(Integer) || (repo_id.is_a?(String) && repo_id.to_s =~ /^[0-9]+$/)
     raise GitHub::Error, INVALID_SELECTION unless valid_integer_or_number_string
 
-    possible_github_repository = GitHubRepository.new(current_user.github_client, repo_id.to_i).on_github?
+    possible_github_repository = GitHubRepository.new(current_user.github_client, repo_id.to_i)
     raise GitHub::Error, INVALID_SELECTION unless possible_github_repository.on_github?
 
     possible_github_repository.id
