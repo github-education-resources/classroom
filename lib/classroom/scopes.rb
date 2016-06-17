d81fcef3ee8827a5ad9baf8c3a66305c58f6ce65
Cheng-Yu Hsu <m@cyhsu.me>
2016-06-17 16:29:14 +0800

    separate scopes

--- lib/classroom/scopes.rb
+++ lib/classroom/scopes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 module Classroom
   module Scopes
-    TEACHER                  = %w(user:email repo delete_repo admin:org admin:org_hook).freeze
-    GROUP_ASSIGNMENT_STUDENT = %w(repo admin:org user:email).freeze
-    ASSIGNMENT_STUDENT       = %w(repo user:email).freeze
+    TEACHER                  = %w(user:email repo delete_repo admin:org).freeze
+    GROUP_ASSIGNMENT_STUDENT = %w(admin:org user:email).freeze
+    ASSIGNMENT_STUDENT       = %w(user:email).freeze
+
+    module ExplicitSubmission
+      TEACHER                  = %w(user:email repo delete_repo admin:org admin:org_hook).freeze
+      GROUP_ASSIGNMENT_STUDENT = %w(repo admin:org user:email).freeze
+      ASSIGNMENT_STUDENT       = %w(repo user:email).freeze
+    end
   end
 end
