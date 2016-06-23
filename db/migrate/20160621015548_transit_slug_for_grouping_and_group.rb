8da9fe5dc8eedd3343effa16dc828abe971c5c02
Mark Tareshawty <tarebyte@github.com>
2016-06-23 16:25:45 -0400

    nvm I lied

--- db/migrate/20160621015548_transit_slug_for_grouping_and_group.rb
+++ db/migrate/20160621015548_transit_slug_for_grouping_and_group.rb
@@ -1,24 +1,48 @@
 class TransitSlugForGroupingAndGroup < ActiveRecord::Migration
   def up
-    %w(Grouping Group).each do |model|
-      klass = model.constantize
-
-      klass.includes(:organization).all.each do |g|
-        slug = g.slugify
-        suffix_number = 0
-
-        loop do
-          break unless klass.where(slug: suffixed_slug(slug, suffix_number), organization: g.organization).present?
-          suffix_number += 1
-        end
-
-        g.slug = suffixed_slug(slug, suffix_number)
-        g.save!(validate: false)
-      end
-    end
+    slugify_all_groupings
+    slugify_all_groups
   end
 
   private
+
+  def slugify_all_groupings
+    Grouping.includes(:organization).all.each do |grouping|
+      slug = grouping.slugify
+      suffix_number = 0
+
+      loop do
+        break unless Grouping.where(
+          slug: suffixed_slug(slug, suffix_number),
+          organization: grouping.organization
+        ).present?
+
+        suffix_number += 1
+      end
+
+      grouping.slug = suffixed_slug(slug, suffix_number)
+      grouping.save!(validate: false)
+    end
+  end
+
+  def slugify_all_groups
+    Group.includes(:grouping).all.each do |group|
+      slug = group.slugify
+      suffix_number = 0
+
+      loop do
+        break unless Group.where(
+          slug: suffixed_slug(slug, suffix_number),
+          grouping: group.grouping
+        ).present?
+
+        suffix_number += 1
+      end
+
+      group.slug = suffixed_slug(slug, suffix_number)
+      group.save!(validate: false)
+    end
+  end
 
   def suffixed_slug(slug, suffix_number)
     return "#{slug}" if suffix_number.zero?
