a4437d5f3fd2c829ba3f93ae20f5f62fcbcd39de
Mark Tareshawty <tarebyte@github.com>
2016-07-03 19:52:34 -0400

    remove unused actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 class PagesController < ApplicationController
   layout 'layouts/pages'
 
-  skip_before_action :authenticate_user!, :set_organization, :authorize_organization_access
+  skip_before_action :authenticate_user!
 
   def home
     redirect_to organizations_path if logged_in?
