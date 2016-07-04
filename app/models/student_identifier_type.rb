929974b1262bb654634134db86e45f2659544247
dinever <dingpeixuan911@gmail.com>
2016-07-04 11:41:10 +0800

    Bring back default scope

--- app/models/student_identifier_type.rb
+++ app/models/student_identifier_type.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class StudentIdentifierType < ActiveRecord::Base
+  default_scope { where(deleted_at: nil) }
+
   belongs_to :organization, -> { unscope(where: :deleted_at) }
   has_many :student_identifiers
 
