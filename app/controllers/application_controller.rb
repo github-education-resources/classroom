c72ced11c924cc2e19b14d5323c6f89b6c9c67ed
Mark Tareshawty <tarebyte@github.com>
2016-07-04 22:37:38 -0400

    remove use of redirect :back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,7 +72,7 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    redirect_to :back
+    redirect_back(fallback_location: (request.referer || root_path))
   end
 
   def logged_in?
