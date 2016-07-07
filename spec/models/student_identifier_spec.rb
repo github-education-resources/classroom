2266db51cc56bf80ec37f9ec24ce89a465ac3e1c
dinever <dingpeixuan911@gmail.com>
2016-07-07 22:09:12 +0800

    Remove unused helper import

--- spec/models/student_identifier_spec.rb
+++ spec/models/student_identifier_spec.rb
@@ -2,8 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe StudentIdentifier, type: :model do
-  include ActiveJob::TestHelper
-
   it_behaves_like 'a default scope where deleted_at is not present'
 
   describe 'uniqueness' do
