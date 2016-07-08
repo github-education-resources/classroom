7a0b4f62efdd9d0ca4c561c35446a88fe2d78d86
dinever <dingpeixuan911@gmail.com>
2016-07-08 17:09:16 +0800

    Implement 3 new endpoints for StudentIdentifierType

---
+++ app/controllers/student_identifier_types_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+class StudentIdentifierTypesController < ApplicationController
+  include OrganizationAuthorization
+
+  before_action :save_referer, only: [:new]
+
+  def index
+    @student_identifier_types = @organization.student_identifier_types
+  end
+
+  def new
+    @student_identifier_type = StudentIdentifierType.new
+  end
+
+  def create
+    @student_identifier_type = StudentIdentifierType.new(student_identifier_type_params)
+    if @student_identifier_type.save
+      flash[:success] = "\"#{student_identifier_type.name}\" has been created!"
+      redirect_to session.delete(:return_to) || { action: 'index' }
+    else
+      render :new
+    end
+  end
+
+  def student_identifier_type_params
+    params
+      .require(:student_identifier_type)
+      .permit(:name, :description, :content_type)
+      .merge(organization: @organization)
+  end
+
+  private
+
+  def set_student_identifier_type
+    @student_identifier_type = StudentIdentifierType.find_by(id: params[:id])
+  end
+
+  def save_referer
+    session[:return_to] = request.referer
+  end
+
+  def student_identifier_type
+    @student_identifier_type ||= StudentIdentifierType.find_by(id: params[:id])
+  end
+  helper_method :student_identifier_type
+end
---
+++ spec/controllers/student_identifier_types_controller_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe StudentIdentifierTypesController, type: :controller do
+  include ActiveJob::TestHelper
+
+  let(:organization)  { GitHubFactory.create_owner_classroom_org }
+  let(:user)          { organization.users.first                 }
+  let(:student)       { GitHubFactory.create_classroom_student   }
+
+  let(:student_identifier_type) { GitHubFactory.create_student_identifier(organization) }
+
+  before do
+    sign_in(user)
+  end
+
+  describe 'GET #index', :vcr do
+    it 'returns success status' do
+      get :index, organization_id: organization.slug
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new', :vcr do
+    it 'returns success status' do
+      get :new, organization_id: organization.slug
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #create', :vcr do
+    it 'creates a new StudentIdentifierType' do
+      expect do
+        post :create,
+             organization_id: organization.slug,
+             student_identifier_type: { name: 'Test', description: 'Test', content_type: 'text' }
+      end.to change { StudentIdentifierType.count }
+    end
+  end
+end
