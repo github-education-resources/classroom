f64004a4b0385c3e38cba3f322c5976c39370ca2
Mark Tareshawty <tarebyte@github.com>
2016-07-17 08:04:39 -0400

    put back comments

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,11 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
+
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.acronym 'GitHub'
   inflect.acronym 'API'
