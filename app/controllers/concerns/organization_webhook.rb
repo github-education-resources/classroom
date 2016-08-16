77aa7bf41e39d1262ec0757c9d2ed873ffb17c44
Cheng-Yu Hsu <m@cyhsu.me>
2016-08-16 10:16:08 +0800

    Fix job execution exception when reosurce is deleted

--- app/controllers/concerns/organization_webhook.rb
+++ app/controllers/concerns/organization_webhook.rb
@@ -9,10 +9,14 @@ module OrganizationWebhook
 
     @organization.create_organization_webhook(organization_webhook_events_url(@organization),
                                               current_user.github_client)
-    PingOrganizationWebhookJob.set(wait: 1.minute).perform_later(@organization, current_user)
+    schedule_ping_org_hook_job(@organization, current_user)
     yield if block_given?
   rescue => err
     @organization.destroy
     raise err
+  end
+
+  def schedule_ping_org_hook_job(organization, user)
+    PingOrganizationWebhookJob.set(wait: 1.minute).perform_later(organization.id, user.id)
   end
 end
--- app/jobs/ping_organization_webhook_job.rb
+++ app/jobs/ping_organization_webhook_job.rb
@@ -2,11 +2,21 @@
 class PingOrganizationWebhookJob < ApplicationJob
   queue_as :github_webhook
 
-  def perform(organization, user)
-    return if organization.is_webhook_active?
+  def should_perform_job?(organization, user)
+    (organization.present? && user.present?) &&
+      (organization.webhook_id.present? && !organization.is_webhook_active?)
+  end
 
-    organization.ping_org_hook(user.github_client)
+  def perform(organization_id, user_id)
+    organization = Organization.find_by(id: organization_id)
+    user = User.find_by(id: user_id)
+
+    return unless should_perform_job?(organization, user)
+
+    organization.ping_organization_webhook(user.github_client)
   ensure
-    self.class.set(wait: 1.minute).perform_later(organization, user) unless organization.is_webhook_active?
+    if should_perform_job?(organization, user)
+      self.class.set(wait: 1.minute).perform_later(organization_id, user_id)
+    end
   end
 end
