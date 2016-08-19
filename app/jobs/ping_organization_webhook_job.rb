081b6986ba74991005d6844cdb601ffb8b8494fe
Cheng-Yu Hsu <m@cyhsu.me>
2016-08-19 23:59:58 +0800

    Make use of sidekiq's custom error handling

--- app/jobs/ping_organization_webhook_job.rb
+++ app/jobs/ping_organization_webhook_job.rb
@@ -2,21 +2,17 @@
 class PingOrganizationWebhookJob < ApplicationJob
   queue_as :github_webhook
 
-  def should_perform_job?(organization, user)
-    (organization.present? && user.present?) &&
-      (organization.webhook_id.present? && !organization.is_webhook_active?)
+  rescue_from(GitHub::Error) do
+    retry_job wait: 1.minute, queue: :github_webhook_failures
   end
 
   def perform(organization_id, user_id)
-    organization = Organization.find_by(id: organization_id)
-    user = User.find_by(id: user_id)
-
-    return unless should_perform_job?(organization, user)
-
-    organization.ping_organization_webhook(user.github_client)
-  ensure
-    if should_perform_job?(organization, user)
-      self.class.set(wait: 1.minute).perform_later(organization_id, user_id)
+    organization = Organization.includes(:users).find(organization_id)
+    user = organization.users.find(user_id)
+    if organization.webhook_id.present? && !organization.is_webhook_active?
+      organization.ping_organization_webhook(user.github_client)
     end
+  rescue ActiveRecord::RecordNotFound
+    return
   end
 end
