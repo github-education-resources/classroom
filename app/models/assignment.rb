1ac349e9a060c919c0d5bb969d94e0b22bdc84e1
dinever <dingpeixuan911@gmail.com>
2016-08-30 15:40:30 -0400

    Better validation for slug

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -26,8 +26,10 @@ class Assignment < ApplicationRecord
   validates :title, length: { maximum: 60 }
 
   validates :slug, uniqueness: { scope: :organization_id }
+  validates :slug, presence: true
+  validates :slug, length: { maximum: 60 }
   validates :slug, format: { with: /\A[-a-zA-Z0-9_]+\z/,
-                             message: "only contain letters, numbers, dashes and underscores" }
+                             message: "should only contain letters, numbers, dashes and underscores" }
 
   validate :uniqueness_of_slug_across_organization
 
