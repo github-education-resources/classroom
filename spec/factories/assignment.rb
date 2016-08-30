9b12a06d0688973559aa6731a7e50c275ae27e79
dinever <dingpeixuan911@gmail.com>
2016-08-30 17:17:26 -0400

    Remove slug from FactoryGirl

--- spec/factories/assignment.rb
+++ spec/factories/assignment.rb
@@ -2,7 +2,6 @@
 FactoryGirl.define do
   factory :assignment do
     title        { "#{Faker::Company.name} Assignment" }
-    slug         { title.parameterize                  }
     organization { FactoryGirl.create(:organization)   }
     creator      { organization.users.first            }
   end
