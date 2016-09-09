0af6388a0ff88a2f0a198519a01326d16534da8d
jiteshjha <jiteshjha96@gmail.com>
2016-09-09 20:28:11 +0530

    I18n setup + shared/footer I18n
    Ordered Gemfile

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ module GitHubClassroom
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    # config.i18n.default_locale = :de
+
+    # Available locales
+    I18n.available_locales = [:en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
