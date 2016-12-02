fd3eadc55b6d341e0619b7cc0076836baf5b2fde
Mark Tareshawty <tarebyte@github.com>
2016-12-02 00:40:41 -0500

    Create rake task to migrate Organization's default repo permissions

---
+++ app/services/organization_default_repository_permission_migrator.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+class OrganizationDefaultRepositoryPermissionMigrator
+  attr_reader :organization
+
+  class Result
+    def self.success
+      new(:success)
+    end
+
+    def self.failed(error)
+      new(:failed, error: error)
+    end
+
+    attr_reader :error
+
+    def initialize(status, error: nil)
+      @status = status
+      @error  = error
+    end
+
+    def success?
+      @status == :success
+    end
+
+    def failed?
+      @status == :failed
+    end
+  end
+
+  def self.perform(organization:)
+    new(organization: organization).perform
+  end
+
+  def initialize(organization:)
+    @organization = organization
+  end
+
+  # Public: Migrate an Organizations default repository permission to none.
+  #
+  # Examples
+  #
+  #   org = Organization.first
+  #   OrganizationDefaultRepositoryPermissionMigrator.perform(organization: org)
+  #   # => true
+  #
+  # Returns true if successful, otherwise nil.
+  # rubocop:disable MethodLength
+  # rubocop:disable AbcSize
+  def perform
+    base_message = "Organization: #{organization.id} doesn't have any users"
+
+    users = organization.users.to_a
+    return Result.failed(base_message) if users.empty?
+
+    authenticated_users = users.keep_if { |user| valid_user?(user) }
+    return Result.failed("#{base_message} with valid tokens") if authenticated_users.empty?
+
+    authenticated_user = authenticated_users.sample
+
+    begin
+      github_organization = GitHubOrganization.new(authenticated_user.github_client, organization.github_id)
+      github_organization.update_default_repository_permission!('none')
+    rescue GitHub::Error => err
+      return Result.failed("For Organization #{organization.id}: #{err.message}")
+    end
+
+    Result.success
+  end
+  # rubocop:enable AbcSize
+  # rubocop:enable MethodLength
+
+  private
+
+  def valid_user?(user)
+    github_user = user.github_user
+    return false unless github_user.authorized_access_token?
+
+    github_organization = GitHubOrganization.new(user.github_client, organization.github_id)
+    github_organization.admin?(github_user.login_no_cache)
+  end
+end
---
+++ lib/tasks/organization_default_repository_permission.rake
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'ruby-progressbar'
+
+namespace :organization_default_repository_permission do
+  desc "Migrate all Organizations to have default repository permission 'none'"
+  task migrate: :environment do
+    progress_bar = ProgressBar.create(
+      title: 'Iterating over Organizations',
+      starting_at: 0,
+      total: Organization.count,
+      format: '%t: %a %e %c/%C (%j%%) %R |%B|',
+      throttle_rate: 0.5,
+      output: Rails.env.test? ? StringIO.new : STDERR
+    )
+
+    Organization.includes(:users).find_in_batches(batch_size: 100) do |orgs|
+      orgs.each do |org|
+        result = OrganizationDefaultRepositoryPermissionMigrator.perform(organization: org)
+        puts result.error if result.failed?
+        progress_bar.increment
+      end
+    end
+  end
+end
