b092c7465f3d38e10d52e12d3f3f17de31df4ebd
Mark Tareshawty <tarebyte@github.com>
2016-12-15 23:06:49 -0500

    add #destroy controller actions

--- app/controllers/stafftools/assignment_repos_controller.rb
+++ app/controllers/stafftools/assignment_repos_controller.rb
@@ -5,6 +5,18 @@ module Stafftools
 
     def show; end
 
+    def destroy
+      assignment = @assignment_repo.assignment
+
+      if @assignment_repo.destroy
+        flash[:success] = 'Assignment repository was destroyed'
+        redirect_to stafftools_assignment_path(assignment.id)
+      else
+        flash[:error] = 'Could not delete assignment repository'
+        render :show
+      end
+    end
+
     private
 
     def set_assignment_repo
