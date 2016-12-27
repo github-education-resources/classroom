d0718a06f1517247b2ec66dcc6db6da1cb1a6ee6
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2016-12-27 11:10:15 -0500

    Tests

--- spec/controllers/stafftools/organizations_controller_spec.rb
+++ spec/controllers/stafftools/organizations_controller_spec.rb
@@ -31,4 +31,45 @@ RSpec.describe Stafftools::OrganizationsController, type: :controller do
       end
     end
   end
+
+  describe 'DELETE #remove_user', :vcr do
+    context 'as an unauthorized user' do
+      it 'returns a 404' do
+        expect { delete :remove_user, params: { id: organization.id, user_id: user.id } }
+          .to raise_error(ActionController::RoutingError)
+      end
+    end
+
+    context 'as an authorized user, remove fails on GitHub end' do
+      before do
+        user.update_attributes(site_admin: true)
+        allow_any_instance_of(GitHubOrganization).to receive(:remove_organization_member).and_return(false)
+        delete :remove_user, params: { id: organization.id, user_id: user.id }
+      end
+
+      it 'shows informative flash message' do
+        expect(flash[:error]).to eql('Could not remove the owner')
+      end
+
+      it 'does not remove user from the Organization' do
+        expect(organization.users.reload.include?(user)).to be_truthy
+      end
+    end
+
+    context 'as an authorized user, remove succeeds on GitHub end' do
+      before do
+        user.update_attributes(site_admin: true)
+        allow_any_instance_of(GitHubOrganization).to receive(:remove_organization_member).and_return(true)
+        delete :remove_user, params: { id: organization.id, user_id: user.id }
+      end
+
+      it 'deletes user from the Organization' do
+        expect(organization.users.reload.include?(user)).to be_falsey
+      end
+
+      it 'redirects to stafftools organization path' do
+        expect(response).to redirect_to(stafftools_organization_path(organization.id))
+      end
+    end
+  end
 end
