aa8f4469c4e9e5b6a877f2a091e9ecf17b164955
Mark Tareshawty <tarebyte@github.com>
2017-01-08 17:05:17 -0500

    Move job helper includes to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,6 +51,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include ActiveJob::TestHelper, type: :job
+  config.include ActiveJob::TestHelper, type: :controller
   config.include AuthenticationHelper,  type: :request
   config.include AuthenticationHelper,  type: :controller
 end
