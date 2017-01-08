cba7dd28fb9c5c9779a0fe459d725ed57a5be7a5
Mark Tareshawty <tarebyte@github.com>
2017-01-08 17:07:09 -0500

    Don't infer location

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,20 +37,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
   config.include GitHubFactory
 
   config.include ActiveJob::TestHelper, type: :job
