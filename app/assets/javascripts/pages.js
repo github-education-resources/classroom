7ce656ca709ae722a49e0f25f53220b5343d95d0
James Brooks <james@brks.cc>
2017-01-13 15:59:58 +0000

    Add modal when "See how it works" button clicked on homepage.

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,17 @@
+// This is a manifest file that'll be compiled into pages.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery/dist/jquery
+//= require jquery.turbolinks
+//= require jquery-ujs/src/rails
+//
+//= require remodal/dist/remodal
