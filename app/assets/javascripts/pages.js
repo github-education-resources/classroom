faa2f428769d6028d355b627fe4c40d9e94b4f85
James Brooks <james@brks.cc>
2017-01-13 23:23:16 +0000

    Style video modal on homepage and add play/stop commands on modal events.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,17 +1,35 @@
-// This is a manifest file that'll be compiled into pages.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery/dist/jquery
 //= require jquery.turbolinks
 //= require jquery-ujs/src/rails
-//
 //= require remodal/dist/remodal
+
+(function() {
+  var player;
+
+  ready = function() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var lastScriptTag = $('script')[$('script').length - 1];
+    $(tag).insertAfter(lastScriptTag);
+  }
+
+  $(document).ready(ready)
+}).call(this);
+
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('getting-started', {
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  $(document).on('opened', '.video', function () {
+    player.playVideo();
+  });
+
+  $(document).on('closed', '.video', function (e) {
+    player.stopVideo();
+  });
+}
