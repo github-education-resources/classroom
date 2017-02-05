08b8074837881bcb343e6a974a04d8f615970ebe
Mark Tareshawty <tarebyte@github.com>
2017-02-05 12:23:21 -0500

    Use memcache in development and only show in peek bar when used

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -13,10 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :dalli_store, 'localhost:11211'
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
     }
