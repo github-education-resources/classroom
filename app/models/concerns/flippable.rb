8998830322323d9dc0ff6c609950ba50bdf09db7
Mark Tareshawty <tarebyte@github.com>
2017-05-30 16:09:07 -0400

    Add helper method for flippable models

--- app/models/concerns/flippable.rb
+++ app/models/concerns/flippable.rb
@@ -3,6 +3,21 @@
 module Flippable
   extend ActiveSupport::Concern
 
+  # Public: Check if an feature has been enabled for an
+  # actor.
+  #
+  # # flippable_feature - The Symbol name of the feature
+  #
+  # Example:
+  #
+  #   current_user.feature_enabled?(student_identifier)
+  #   # => false
+  #
+  # Returns a Boolean.
+  def feature_enabled?(flippable_feature)
+    GitHubClassroom.flipper[flippable_feature].enabled?(self)
+  end
+
   def flipper_id
     "#{self.class}:#{id}"
   end
