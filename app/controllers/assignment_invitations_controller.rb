57f4077d68ecc4a9fc16b7b148aaa1a2a088864e
Mark Tareshawty <tarebyte@github.com>
2017-05-30 20:31:10 -0400

    Extract shared parts into InvitationsControllerMethods module

--- app/controllers/assignment_invitations_controller.rb
+++ app/controllers/assignment_invitations_controller.rb
@@ -1,78 +1,60 @@
 # frozen_string_literal: true
 
 class AssignmentInvitationsController < ApplicationController
-  layout 'layouts/invitations'
+  include InvitationsControllerMethods
 
   before_action :check_user_not_previous_acceptee, only: [:show]
-  before_action :ensure_github_repo_exists, only: [:successful_invitation]
+  before_action :ensure_submission_repository_exists, only: [:success]
 
-  def accept_invitation
-    create_assignment_repo { redirect_to successful_invitation_assignment_invitation_path }
+  def accept
+    create_submission do
+      redirect_to success_assignment_invitation_path
+    end
   end
 
   def show; end
 
-  def successful_invitation; end
+  def success; end
 
   private
 
-  def required_scopes
-    GitHubClassroom::Scopes::ASSIGNMENT_STUDENT
+  def ensure_submission_repository_exists
+    return not_found unless current_submission
+    return if current_submission
+              .github_repository
+              .present?(headers: GitHub::APIHeaders.no_cache_no_store)
+
+    current_submission.destroy
+    remove_instance_variable(:@current_submission)
+
+    create_submission
   end
 
-  def assignment
-    @assignment ||= invitation.assignment
+  def check_user_not_previous_acceptee
+    return if current_submission.nil?
+    redirect_to success_invitation_assignment_invitation_path
   end
-  helper_method :assignment
 
-  def assignment_repo
-    @assignment_repo ||= AssignmentRepo.find_by(assignment: assignment, user: current_user)
-  end
-  helper_method :assignment_repo
-
-  def invitation
-    @invitation ||= AssignmentInvitation.find_by!(key: params[:id])
-  end
-  helper_method :invitation
-
-  def organization
-    @organization ||= assignment.organization
-  end
-  helper_method :organization
-
-  def create_assignment_repo
-    result = invitation.redeem_for(current_user)
+  def create_submission
+    result = current_invitation.redeem_for(current_user)
 
     if result.success?
       yield if block_given?
     else
       flash[:error] = result.error
-      redirect_to assignment_invitation_path(invitation)
+      redirect_to assignment_invitation_path(current_invitation)
     end
   end
 
-  def new_student_identifier_params
-    params
-      .require(:student_identifier)
-      .permit(:value)
-      .merge(user: current_user,
-             organization: organization,
-             student_identifier_type: assignment.student_identifier_type)
+  def current_submission
+    @current_submission ||= AssignmentRepo.find_by(assignment: current_assignment, user: current_user)
   end
 
-  def check_user_not_previous_acceptee
-    return unless assignment.users.include?(current_user)
-    redirect_to successful_invitation_assignment_invitation_path
+  def current_invitation
+    @current_invitation ||= AssignmentInvitation.find_by!(key: params[:id])
   end
 
-  def ensure_github_repo_exists
-    return not_found unless assignment_repo
-    return if assignment_repo
-              .github_repository
-              .present?(headers: GitHub::APIHeaders.no_cache_no_store)
-
-    assignment_repo.destroy
-    @assignment_repo = nil
-    create_assignment_repo
+  def required_scopes
+    GitHubClassroom::Scopes::ASSIGNMENT_STUDENT
   end
 end
