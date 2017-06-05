f1a49aac7e59e66c91bec54396714200a1214fab
Mark Tareshawty <tarebyte@github.com>
2017-06-05 11:37:29 -0400

    Remove identifier-form javascript

--- app/assets/javascripts/identifier-form.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  $('.assignment_invitations.identifier, .group_assignment_invitations.identifier').ready(function() {
-    return $('form').on('change keyup', function() {
-      return $('.js-form-submit').prop('disabled', $('#student_identifier_value').val().length === 0);
-    });
-  });
-}).call(this);
