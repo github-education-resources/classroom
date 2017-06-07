31acfc95ab332733c080a0bac0da971a46039bb3
Nathaniel Woodthorpe <nwoodthorpe@github.com>
2017-06-07 14:53:36 -0700

    Rubo:policeman:

--- lib/github/token.rb
+++ lib/github/token.rb
@@ -3,6 +3,15 @@
 module GitHub
   module Token
     class << self
+      EXPANSIONS = {
+        user:               ['read:user', 'user:email', 'user:follow'],
+        repo:               ['repo:status', 'repo_deployment', 'public_repo'],
+        'admin:org':        ['write:org', 'read:org'],
+        'admin:public_key': ['write:public_key', 'read:public_key'],
+        'admin:repo_hook':  ['write:repo_hook', 'read:repo_hook'],
+        'admin:gpg_key':    ['write:gpg_key', 'read:gpg_key']
+      }.freeze
+
       def scopes(token, client = nil)
         GitHub::Errors.with_error_handling do
           github_client = client.present? ? client : GitHubClassroom.github_client
@@ -16,15 +25,6 @@ module GitHub
       # Having a scope like 'user' is actually read:user, user:email and user:follow
       # This method expands these collection scopes to make checking which scopes we have more simple
       def expand_scopes(scopes)
-        expansions = {
-          user:               ['read:user', 'user:email', 'user:follow'],
-          repo:               ['repo:status', 'repo_deployment', 'public_repo'],
-          'admin:org':        ['write:org', 'read:org'],
-          'admin:public_key': ['write:public_key', 'read:public_key'],
-          'admin:repo_hook':  ['write:repo_hook', 'read:repo_hook'],
-          'admin:gpg_key':    ['write:gpg_key', 'read:gpg_key']
-        }
-
         scopes
           .map { |scope| expansions.key?(scope.to_sym) ? expansions[scope.to_sym] : scope }
           .flatten
