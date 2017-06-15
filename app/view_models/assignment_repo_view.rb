fa7ead46fab8e9555ebda682b61621253bbb0fa6
Nathaniel Woodthorpe <nwoodthorpe@github.com>
2017-06-15 10:53:45 -0700

    :eyes:

--- app/view_models/assignment_repo_view.rb
+++ app/view_models/assignment_repo_view.rb
@@ -4,7 +4,7 @@ class AssignmentRepoView < SharedAssignmentRepoView
   include ActionView::Helpers::TextHelper
   attr_reader :assignment_repo
 
-  delgate :github_user, to: :assignment_repo
+  delegate :github_user, to: :assignment_repo
 
   def avatar_url
     avatar_url_for(github_user, 96)
