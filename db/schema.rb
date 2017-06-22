d6338ccab6ea4c9ea554bd9ebc2eb9f90e87125c
Mark Tareshawty <tarebyte@github.com>
2017-06-21 22:28:40 -0700

    Back feature previewing by a database column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620200357) do
+ActiveRecord::Schema.define(version: 20170622032909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -199,6 +199,7 @@ ActiveRecord::Schema.define(version: 20170620200357) do
     t.datetime "updated_at", null: false
     t.boolean "site_admin", default: false
     t.datetime "last_active_at", null: false
+    t.boolean "feature_previewer", default: false
     t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
