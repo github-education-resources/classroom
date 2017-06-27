a480c0a39188806a14b7cfa89e900d863ad104e3
Wilfried Hounyo <wilfriede@github.com>
2017-06-27 15:01:57 -0700

    Add ability to parse yaml front_matter to GitHubBlob

--- app/models/github_blob.rb
+++ app/models/github_blob.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+require 'safe_yaml/load'
+
 class GitHubBlob
+  YAML_FRONT_MATTER_REGEXP = %r!\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)!m
+
+  attr_reader :data, :body
+
   def initialize(github_repository, sha, **options)
     repo = github_repository
     @blob = repo.client.blob(repo.full_name, sha, options)
+    @data = nil
+    @body = nil
+    read_contents
   end
 
   def utf_content
@@ -21,5 +30,14 @@ class GitHubBlob
   def decoded_content
     return @blob.content unless @blob.content != 'utf-8'
     Base64.decode64(@blob.content)
+  end
+
+  def read_contents
+    match = YAML_FRONT_MATTER_REGEXP.match(decoded_content)
+    if match
+      @body = match.post_match
+      @data = SafeYAML.load(match.to_s)
+    end
+    @data
   end
 end
