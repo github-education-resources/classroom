28e1d830620574f92fe97b572a70259da320cdd8
Mark Tareshawty <tarebyte@github.com>
2017-07-12 10:49:48 -0400

    Use the short key if we have it, don't show both

--- app/helpers/invitation_helper.rb
+++ app/helpers/invitation_helper.rb
@@ -13,36 +13,48 @@ module InvitationHelper
     # #=> {
     #      :type => "assignment_invitation",
     #      :key  => "7309ffd1f7b5e7af09dfcd11fab371d7",
-    #      :short_key => "aEE8Cq",
     #      :url  => "http://github.dev/assignment-invitations/7309ffd1f7b5e7af09dfcd11fab371d7",
-    #      :short_url => "http://github.dev/a/aEE8Cq"
     #     }
     #
     # Returns a Hash of attributes
     def attributes(invitation, base_url)
-      type = invitation.class.to_s.underscore
-
       {
-        type: type,
-        key: invitation.key,
-        short_key: invitation.short_key,
-        url: Rails.application.routes.url_helpers.send("#{type}_url".to_sym, id: invitation.key, host: base_url),
-        short_url: short_url(invitation.short_key, type, base_url)
+        type: invitation_type(invitation),
+        key: invitation_key(invitation),
+        url: invitation_url(invitation: invitation, base_url: base_url)
       }
     end
 
     private
 
-    def short_url(short_key, type, base_url)
-      if short_key
-        Rails.application.routes.url_helpers.send(
-          "#{type}_short_url".to_sym,
-          short_key: short_key,
-          host: base_url
-        )
-      else
-        ''
-      end
+    def invitation_key(invitation)
+      return invitation.short_key if invitation.short_key.present?
+      invitation.key
     end
+
+    def invitation_type(invitation)
+      invitation.class.to_s.underscore
+    end
+
+    # rubocop:disable Style/MethodLength
+    def invitation_url(invitation:, base_url:)
+      type = invitation_type(invitation)
+      url  = nil
+
+      options = {}.tap do |opts|
+        opts[:host] = base_url
+
+        url = if invitation.short_key.present?
+                opts[:short_key] = invitation.short_key
+                "#{type}_short_url"
+              else
+                opts[:id] = invitation.key
+                "#{type}_url"
+              end
+      end
+
+      Rails.application.routes.url_helpers.send(url.to_sym, options)
+    end
+    # rubocop:enable Style/MethodLength
   end
 end
