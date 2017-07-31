b50e9d11e9a6440db16074a30499a3791f4111fc
Mark Tareshawty <tarebyte@github.com>
2017-07-31 18:19:01 -0400

    We're using RSpec not MiniTest

--- spec/lib/github_classroom/null_statsd_spec.rb
+++ spec/lib/github_classroom/null_statsd_spec.rb
@@ -6,12 +6,12 @@ describe GitHubClassroom::NullStatsD do
   subject { described_class.new }
 
   it "responds to #increment nil" do
-    assert subject.respond_to?("increment")
-    assert_predicate subject.increment("foo", bad: "option"), :nil?
+    expect(subject).to respond_to(:increment)
+    expect(subject.increment("foo", bad: "option")).to be_nil
   end
 
   it "responds to #time with nil" do
-    assert subject.respond_to?("time")
-    assert_predicate subject.time("foo", bad: "option"), :nil?
+    expect(subject).to respond_to(:time)
+    expect(subject.time("foo", bad: "option")).to be_nil
   end
 end
