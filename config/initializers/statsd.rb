0368c946aa6fa696a6379c1747575c65f28f5365
Nathaniel Woodthorpe <nwoodthorpe@github.com>
2017-07-31 11:23:38 -0700

    Hound

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -7,7 +7,7 @@ end
 module GitHubClassroom
   def self.statsd
     if Rails.env.production?
-      @statsd ||= ::Datadog::Statsd.new('localhost', 8125)
+      @statsd ||= ::Datadog::Statsd.new("localhost", 8125)
     else
       @stats ||= ::StubStatsd.new
     end
