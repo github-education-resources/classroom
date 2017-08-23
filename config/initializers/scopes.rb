433e15df94baa9cd2ef436eb4bbd8ad289fc597b
Mark Tareshawty <tarebyte@github.com>
2017-08-23 17:28:57 -0400

    Add repo:invite scope

--- config/initializers/scopes.rb
+++ config/initializers/scopes.rb
@@ -5,6 +5,6 @@ module GitHubClassroom
     TEACHER                  = %w[user:email repo:status repo_deployment
                                   public_repo delete_repo write:org read:org admin:org_hook].freeze
     GROUP_ASSIGNMENT_STUDENT = %w[write:org read:org user:email].freeze
-    ASSIGNMENT_STUDENT       = %w[user:email].freeze
+    ASSIGNMENT_STUDENT       = %w[user:email repo:invite].freeze
   end
 end
