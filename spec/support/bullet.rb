9efd3c97b8db55e980a23db9ab6772c157763f12
Mark Tareshawty <tarebyte@github.com>
2017-09-04 19:25:52 -0400

    Don't skip over controllers for tests

--- spec/support/bullet.rb
+++ spec/support/bullet.rb
@@ -6,12 +6,6 @@ if defined?(Bullet) && Bullet.enable?
       Bullet.start_request
     end
 
-    config.around(:each, type: :controller) do |example|
-      Bullet.unused_eager_loading_enable = false
-      example.run
-      Bullet.unused_eager_loading_enable = true
-    end
-
     config.after(:each) do
       Bullet.perform_out_of_channel_notifications if Bullet.notification?
       Bullet.end_request
