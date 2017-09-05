f9099274b3efa5ae275acf685d0164a4bc0fcf6d
Mark Tareshawty <tarebyte@github.com>
2017-09-05 10:20:32 -0400

    Update specs for scoping issue (should fail)

--- spec/controllers/orgs/assignment_repos_controller_spec.rb
+++ spec/controllers/orgs/assignment_repos_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe AssignmentReposController, type: :controller do
+RSpec.describe Orgs::AssignmentReposController, type: :controller do
   let(:organization) { classroom_org     }
   let(:user)         { classroom_teacher }
 
@@ -20,22 +20,45 @@ RSpec.describe AssignmentReposController, type: :controller do
       end
 
       it "redirect to login path" do
-        get :show, params: { organization_id: organization.slug, assignment_id: assignment.id, id: assignment_repo.id }
+        get :show, params: {
+          organization_id: organization.slug,
+          assignment_id: assignment.slug,
+          id: assignment_repo.id
+        }
+
         expect(response).to redirect_to(login_path)
       end
     end
 
     context "authenticated user" do
-      before do
-        get :show, params: { organization_id: organization.slug, assignment_id: assignment.id, id: assignment_repo.id }
+      context "with scoped resource" do
+        before do
+          get :show, params: {
+            organization_id: organization.slug,
+            assignment_id: assignment.slug,
+            id: assignment_repo.id
+          }
+        end
+
+        it "returns success status" do
+          expect(response).to have_http_status(:success)
+        end
+
+        it "sets the AssignmentRepo" do
+          expect(assigns[:assignment_repo].id).to eql(assignment_repo.id)
+        end
       end
 
-      it "returns success status" do
-        expect(response).to have_http_status(:success)
-      end
+      context "with improperly scope resource" do
+        it "returns not found" do
+          get :show, params: {
+            organization_id: organization.slug,
+            assignment_id: "#{assignment.slug}-a",
+            id: assignment_repo.id
+          }
 
-      it "sets the AssignmentRepo" do
-        expect(assigns[:assignment_repo].id).to eql(assignment_repo.id)
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
   end
