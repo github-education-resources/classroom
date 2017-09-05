f42e031568d7d644f2bce5720b052d4ca3eb1449
Mark Tareshawty <tarebyte@github.com>
2017-09-05 10:21:34 -0400

    Fix failing specs by properly scoping resources

--- app/controllers/orgs/assignment_repos_controller.rb
+++ app/controllers/orgs/assignment_repos_controller.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-class AssignmentReposController < ApplicationController
-  include OrganizationAuthorization
+module Orgs
+  class AssignmentReposController < Orgs::Controller
+    layout false
 
-  layout false
+    def show
+      @assignment_repo = AssignmentRepo.includes(:user).find_by!(id: params[:id], assignment: current_assignment)
+    end
 
-  def show
-    @assignment_repo = AssignmentRepo.includes(:user).find_by!(id: params[:id])
+    private
+
+    def current_assignment
+      return @current_assignment if defined?(@current_assignment)
+      @current_assignment = current_organization.assignments.find_by(slug: params[:assignment_id])
+    end
   end
 end
--- app/controllers/orgs/group_assignment_repos_controller.rb
+++ app/controllers/orgs/group_assignment_repos_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
-class GroupAssignmentReposController < ApplicationController
-  include OrganizationAuthorization
+module Orgs
+  class GroupAssignmentReposController < Orgs::Controller
+    layout false
 
-  layout false
+    def show
+      @group_assignment_repo = GroupAssignmentRepo.includes(:group).find_by!(
+        id: params[:id],
+        group_assignment: current_group_assignment
+      )
+    end
 
-  def show
-    @group_assignment_repo = GroupAssignmentRepo.includes(:group).find_by!(id: params[:id])
+    private
+
+    def current_group_assignment
+      return @current_assignment if defined?(@current_assignment)
+      @current_assignment = current_organization.group_assignments.find_by(slug: params[:group_assignment_id])
+    end
   end
 end
