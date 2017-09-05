b8b1b64e6a2014db99a417092c61eb120c3dec47
Mark Tareshawty <tarebyte@github.com>
2017-09-04 21:02:28 -0400

    Add relationships and validation

--- app/models/organization_user.rb
+++ app/models/organization_user.rb
@@ -2,4 +2,12 @@
 
 class OrganizationUser < ApplicationRecord
   self.table_name = "organizations_users"
+
+  update_index("stafftools#organization_user") { self }
+
+  belongs_to :organization
+  belongs_to :user
+
+  validates :organization, presence: true
+  validates :user,         presence: true
 end
