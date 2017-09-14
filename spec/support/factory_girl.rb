f550f824997faca14fa3be283684e3d56ac084c6
Mark Tareshawty <tarebyte@github.com>
2017-09-14 15:22:57 -0400

    Run the factory_girl linter as a separate rake task

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,14 +2,4 @@
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      # Test factories in spec/factories are working.
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
-  end
 end
