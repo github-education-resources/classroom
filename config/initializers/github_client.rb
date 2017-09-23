3473cae10aed54fdac172c6389c7e76dd2b7e6db
Dustin Wheeler <mdwheele@ncsu.edu>
2017-09-23 00:40:57 -0400

    Refactor GitHubClassroom.github_client to remove excessive merge! calls
    See: https://github.com/education/classroom/pull/857#discussion_r110309411

--- config/initializers/github_client.rb
+++ config/initializers/github_client.rb
@@ -2,26 +2,27 @@
 
 module GitHubClassroom
   def self.github_client(options = {})
-    client_options = {
-      client_id:     Rails.application.secrets.github_client_id,
-      client_secret: Rails.application.secrets.github_client_secret
-    }.merge!(options)
+    client_options = {}.tap do |opts|
+      opts[:user_agent]    = "GitHub Classroom"
+      opts[:auto_paginate] = true
 
-    if client_options.has_key?(:access_token)
-      client_options.delete(:client_id)
-      client_options.delete(:client_secret)
-    end
+      if options.has_key?(:access_token)
+        opts[:access_token] = options[:access_token]
+      else
+        opts[:client_id]     = Rails.application.secrets.github_client_id
+        opts[:client_secret] = Rails.application.secrets.github_client_secret
+      end
 
-    if is_ghe
-      client_options.merge!({
-        api_endpoint: "https://%{hostname}/api/v3/" % [hostname: Rails.application.secrets.github_enterprise_hostname]
-      })
+      if enterprise_instance?
+        api_endpoint = "https://#{Rails.application.secrets.github_enterprise_hostname}/api/v3"
+        opts[:api_endpoint] = api_endpoint
+      end
     end
 
     Octokit::Client.new(client_options)
   end
 
-  def self.is_ghe()
-    !!(Rails.application.secrets.github_enterprise_enabled == true)
+  def self.enterprise_instance?
+    Rails.application.secrets.github_enterprise_enabled == "true"
   end
 end
