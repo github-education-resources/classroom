427e371ec1dc76b3d7c3a9743df8cf1629b56f5b
Dustin Wheeler <mdwheele@ncsu.edu>
2017-09-23 00:51:44 -0400

    Key for enabling enterprise is ENTERPRISE_ENABLED
    See: https://github.com/education/classroom/pull/857#discussion_r110309935

--- config/initializers/github_client.rb
+++ config/initializers/github_client.rb
@@ -23,6 +23,6 @@ module GitHubClassroom
   end
 
   def self.enterprise_instance?
-    Rails.application.secrets.github_enterprise_enabled == "true"
+    Rails.application.secrets.enterprise_enabled == "true"
   end
 end
