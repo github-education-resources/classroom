11d15de790df1436721ceeccd7d6c307a6c91735
Dustin Wheeler <mdwheele@ncsu.edu>
2017-09-23 00:55:56 -0400

    Use double-quoted strings (style)

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -2,8 +2,8 @@
 
 OmniAuth.config.test_mode = true
 
-VCR.use_cassette 'auth_user' do
-  token = ENV['TEST_CLASSROOM_OWNER_GITHUB_TOKEN'] ||= 'some-token'
+VCR.use_cassette "auth_user" do
+  token = ENV["TEST_CLASSROOM_OWNER_GITHUB_TOKEN"] ||= "some-token"
   user = GitHubClassroom.github_client(access_token: token).user
 
   OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
