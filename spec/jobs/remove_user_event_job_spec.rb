b69e6a56c4be61a2e1f153b092104bd69ae16db3
Andrew <arolson56@gmail.com>
2017-10-01 18:22:50 -0700

    add job to remove a User from an Organization when they are removed from the GitHub Organization

---
+++ spec/jobs/remove_user_event_job_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe RemoveUserJob, type: :job do
+  let(:organization) { classroom_org }
+  let(:payload)      { json_payload('webhook_events/user_removed.json') }
+
+  context 'Action removed', :vcr do
+
+    it 'deletes user from organization' do
+      github_user_id = payload.dig('member','id')
+      
+      RemoveUserJob.perform_now(payload)
+      expect { organization.users.find_by(github_id: github_user_id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
