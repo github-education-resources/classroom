a953352f223002bc2e9b335348cc038d237da896
Andrew <arolson56@gmail.com>
2017-11-19 08:43:44 -0800

    rename for member event job

---
+++ app/jobs/member_event_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Documentation: https://developer.github.com/v3/activity/events/types/#membershipevent
+class MemberEventJob < ApplicationJob
+  queue_as :github_event
+  def perform(payload_body)
+    return true unless payload_body["action"] == "removed"
+
+    github_user_id = payload_body.dig("member", "id")
+    github_organization_id = payload_body.dig("organization", "id")
+
+    organization = Organization.find_by(github_id: github_organization_id)
+    user = organization.users.find_by(id: github_user_id)
+    return true if user.nil?
+
+    user.update_attributes(deleted_at: Time.zone.now)
+    DestroyResourceJob.perform_later(current_organization)
+  end
+end
