47dd7ec1ba7310c5120628306d17bb45b7f5e1d7
Nathaniel Woodthorpe <d12@github.com>
2018-01-11 17:32:39 -0500

    Add spec for ordering stuff on roster entry model

--- spec/models/roster_entry_spec.rb
+++ spec/models/roster_entry_spec.rb
@@ -25,4 +25,26 @@ RSpec.describe RosterEntry, type: :model do
       expect(subject.save).to be_truthy
     end
   end
+
+  describe "order_for_view" do
+    let(:organization) { classroom_org                                   }
+    let(:assignment)   { create(:assignment, organization: organization) }
+
+    let(:roster)   { create(:roster) }
+
+    let(:student1) { create(:user) }
+    let(:student2) { create(:user) }
+
+    let!(:assignment_repo) { create(:assignment_repo, assignment: assignment, user: student1) }
+
+    let(:linked_accepted_entry)     { create(:roster_entry, roster: roster, user: student1) }
+    let(:not_linked_entry)          { create(:roster_entry, roster: roster)                 }
+    let(:linked_not_accepted_entry) { create(:roster_entry, roster: roster, user: student2) }
+
+    it "orders correctly" do
+      expected_ordering = [linked_accepted_entry, linked_not_accepted_entry, not_linked_entry]
+
+      expect(RosterEntry.order_for_view(assignment).to_a).to equal(expected_ordering)
+    end
+  end
 end
