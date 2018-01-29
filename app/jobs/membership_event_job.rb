29c0b71ed7dfde49e3878550c72098f3d173cfef
Kassandre <kpedro@student.42.fr>
2018-01-29 16:26:22 +0100

    Update perform based on tarebyte's comments

--- app/jobs/membership_event_job.rb
+++ app/jobs/membership_event_job.rb
@@ -9,14 +9,14 @@ class MembershipEventJob < ApplicationJob
 
     user_id = payload_body.dig("member", "id")
     team_id = payload_body.dig("team", "id")
+
     user = User.find_by(uid: user_id)
-    group_id = Group.find_by(github_team_id: team_id).id
+    return true unless user.present?
 
-    repo_accesses = GroupAssignmentRepo.find_by(group_id: group_id).repo_accesses
+    group = Group.find_by(github_team_id: team_id)
+    return true unless group.present?
 
-    repo_access = repo_accesses.find_by(user_id: user.id)
-    repo_access&.delete
-
-    # Do we remove github_repo as well ?
+    group.repo_accesses.find_by(user_id: user.id)
+    repo_access&.destroy
   end
 end
