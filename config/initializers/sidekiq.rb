2ad4d9a2d54474b77b7fdceff4aae6d28e40e938
Mark Tareshawty <tarebyte@github.com>
2018-02-01 00:06:01 -0500

    Update config files with new ports and configurations

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
+  config.redis = { url: GitHubClassroom::REDIS_URL }
+
   # Add chewy middleware from lib/sidekiq/chewy_middleware.rb
   config.server_middleware do |chain|
     chain.add Sidekiq::ChewyMiddleware, :atomic
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: GitHubClassroom::REDIS_URL }
 end
