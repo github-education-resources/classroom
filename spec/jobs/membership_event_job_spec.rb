79e0f133b8369124e413f4a3f7a0b3eeac2c8ff8
Kassandre <kpedro@student.42.fr>
2018-02-02 18:17:48 +0100

    First shot spec membership and add MembershipEventJob cassette

--- spec/jobs/membership_event_job_spec.rb
+++ spec/jobs/membership_event_job_spec.rb
@@ -2,24 +2,32 @@
 
 require "rails_helper"
 
-RSpec.describe RepositoryEventJob, type: :job do
-  let(:payload)      { json_payload("webhook_events/member_removed.json") }
- https://developer.github.com/v3/activity/events/types/#membershipevent
-  context "ACTION removed", :vcr do
-    after do
-      Group.destroy_all
-    end
+RSpec.describe MembershipEventJob, type: :job do
+  let(:payload)      { json_payload("webhook_events/team_member_removed.json") }
+  let(:organization) { create(:organization, github_id: payload.dig("organization", "id")) }
+  let(:student)      { create(:user, uid: payload.dig("member", "id")) }
+  let(:repo_access)  { RepoAccess.create(user: student, organization: organization) }
+  let(:grouping)     { create(:grouping, organization: organization) }
+  let(:group)        { Group.create(title: "Group 1", grouping: grouping) }
 
-    it "deletes the matching RepasAccess" do
-      # group_assignment = create(:group_assignment, title: "Intro to Rails", organization: organization)
-      # group            = Group.create(title: "Group 1", grouping: group_assignment.grouping)
-      #
-      # group_assignment_repo = GroupAssignmentRepo.create(group_assignment: group_assignment, group: group)
-      #
-      # payload["repository"]["id"] = group_assignment_repo.github_repo_id
-      #
-      # RepositoryEventJob.perform_now(payload)
-      # expect { group_assignment_repo.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  let(:group_assignment) do
+    create(:group_assignment,
+           grouping: grouping,
+           title: "Learn JavaScript",
+           organization: organization,
+           public_repo: true,
+           starter_code_repo_id: 1_062_897)
+  end
+
+  before(:each) do
+    group.repo_accesses << repo_access
+    @group_assignment_repo = GroupAssignmentRepo.create(group_assignment: group_assignment, group: group)
+  end
+
+  context "Action removed", :vcr do
+    it "removes user from team" do
+      MembershipEventJob.perform_now(payload)
+      expect { organization.users.find_by(uid: student.uid) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
