916f73648b39197429b1bf1eb80c8584de277cef
Mark Tareshawty <tarebyte@github.com>
2018-02-03 11:32:07 -0500

    Add failbot reporting to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
+  config.error_handlers << proc { |ex, ctx_hash| Failbot.report(ex, ctx_hash) }
+
   # Add chewy middleware from lib/sidekiq/chewy_middleware.rb
   config.server_middleware do |chain|
     chain.add Sidekiq::ChewyMiddleware, :atomic
