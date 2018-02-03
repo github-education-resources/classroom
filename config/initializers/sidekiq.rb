51fcf950029c26b0d3d21ab2a360afbcf78a316c
Mark Tareshawty <tarebyte@github.com>
2018-02-03 12:49:28 -0500

    Make the error more readable for Failbot

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require "sidekiq/failbot/error_handler"
+
 Sidekiq.configure_server do |config|
-  config.error_handlers << proc { |ex, ctx_hash| Failbot.report(ex, ctx_hash) }
+  config.error_handlers << proc { |ex, ctx_hash| Sidekiq::Failbot::ErrorHandler.report(ex, ctx_hash) }
 
   # Add chewy middleware from lib/sidekiq/chewy_middleware.rb
   config.server_middleware do |chain|
