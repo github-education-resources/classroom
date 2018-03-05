bb110316c12858f6ea9172133d70f6cbcb14b7a5
Kassandre <kpedro@student.42.fr>
2018-03-05 18:23:39 +0100

    Transfer assignments ownership

--- app/jobs/organization_event_job.rb
+++ app/jobs/organization_event_job.rb
@@ -9,26 +9,24 @@ class OrganizationEventJob < ApplicationJob
 
     github_user_id = payload_body.dig("membership", "user", "id")
     github_organization_id = payload_body.dig("organization", "id")
-    organization = Organization.find_by(github_id: github_organization_id)
-    user = organization.users.find_by(uid: github_user_id)
+    @organization = Organization.find_by(github_id: github_organization_id)
+    @user = @organization.users.find_by(uid: github_user_id)
 
-    return true unless user
+    return true unless @user
 
-    transfer_assigments(user, organization) if organization.all_assignments
-    organization.users.delete(user)
+    transfer_assignments if user_owns_assignments?
+    @organization.users.delete(@user)
   end
 
-  def transfer_assigments(user, organization)
-    puts "==========TRANSFER============="
-    other_owner = organization.users.where.not(id: user.id).first
-    puts "other_owner is : #{other_owner.inspect}"
-    organization.all_assignments.map do |a|
-      puts "Creator id is: #{a.creator_id}"
-      puts "New creator id is: #{other_owner.id}"
-      a.creator_id = other_owner.id if a.creator_id == user.id
+  def user_owns_assignments?
+    @organization.all_assignments.map{ |a| a.creator_id }.include? @user.id
+  end
+
+  def transfer_assignments
+    new_owner = @organization.users.where.not(id: @user.id).first
+    @organization.all_assignments.map do |a|
+      a.creator_id = new_owner.id if a.creator_id == @user.id
+      a.save
     end
-    puts "==============================="
-    puts organization.all_assignments.inspect
-    puts "==========TRANSFER============="
   end
 end
--- spec/jobs/organization_event_job_spec.rb
+++ spec/jobs/organization_event_job_spec.rb
@@ -6,14 +6,28 @@ RSpec.describe OrganizationEventJob, type: :job do
   let(:payload) { json_payload("webhook_events/member_removed.json") }
 
   context "ACTION member_removed", :vcr do
-    it "deletes user from organization" do
+    before(:each) do
       github_user_id = payload.dig("membership", "user", "id")
-      organization = create(:organization, github_id: payload.dig("organization", "id"))
-      user = create(:user, uid: github_user_id, organization_ids: organization.id)
-      organization.users << user
+      @organization = create(:organization, github_id: payload.dig("organization", "id"))
+      @user = create(:user, uid: github_user_id, organization_ids: @organization.id)
+    end
+
+    it "deletes user from organization" do
+      @organization.users << @user
 
       OrganizationEventJob.perform_now(payload)
-      expect { organization.users.find(uid: user.uid) }.to raise_error(ActiveRecord::RecordNotFound)
+
+      expect { @organization.users.find(uid: @user.uid) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "deletes user from organization and transfers assignment ownership" do
+      @organization.users << @user
+      assignment = create(:assignment, organization: @organization, creator: @user)
+
+      OrganizationEventJob.perform_now(payload)
+
+      expect { @organization.users.find(uid: @user.uid) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(assignment.reload.creator_id).not_to eq(@user.id)
     end
   end
 end
