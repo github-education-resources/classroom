e4fc917b78ecd48fdc06a2ec595292edb1137ec9
Kassandre <kpedro@student.42.fr>
2018-03-14 13:21:13 +0100

    Switch condition

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -36,7 +36,7 @@ class Organization < ApplicationRecord
   end
 
   def github_client
-    token = Rails.env.test? ? users.limit(1).order("RANDOM()").pluck(:token)[0] : users.first.token
+    token = Rails.env.test? ? users.first.token : users.limit(1).order("RANDOM()").pluck(:token)[0]
 
     Octokit::Client.new(access_token: token)
   end
