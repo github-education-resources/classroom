fb9c0c22b33d9df7816e605eea3e3b9027f3f7a8
Kassandre <kpedro@student.42.fr>
2018-04-12 23:37:26 +0200

    Team deleted event

---
+++ spec/jobs/team_event_job_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TeamEventJob, type: :job do
+  let(:payload)      { json_payload("webhook_events/team_deleted.json") }
+  let(:organization) { classroom_org }
+
+  context "ACTION member_removed", :vcr do
+    before(:each) do
+      Group.destroy_all
+    end
+
+    it "deletes team" do
+      group_assignment = create(:group_assignment, title: "Intro to Rails #1", organization: organization)
+      group = Group.create(title: "GROUP #1",
+                           github_team_id: payload.dig("team", "id"),
+                           grouping: group_assignment.grouping)
+
+      payload["team"]["id"] = group.github_team_id
+
+      MembershipEventJob.perform_now(payload)
+
+      expect{ Group.find(github_team_id: group.github_team_id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
