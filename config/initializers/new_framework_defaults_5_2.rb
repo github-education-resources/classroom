595653cfa84c8104c087de388a6fe6fa19714efd
Kassandre <kpedro@student.42.fr>
2018-04-12 22:27:35 +0200

    Magic comment and duplicated files

--- config/initializers/new_framework_defaults_5_2.rb
+++ config/initializers/new_framework_defaults_5_2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 #
 # This file contains migration options to ease your Rails 5.2 upgrade.
@@ -5,6 +7,9 @@
 # Once upgraded flip defaults one by one to migrate to the new default.
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
+
+# Make `form_with` generate non-remote forms.
+Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Make Active Record use stable #cache_key alongside new #cache_version method.
 # This is needed for recyclable cache keys.
