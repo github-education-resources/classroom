ab76e0b53d18b9d814dd862315912b8f0260c50e
BenEmdon <benemdon@github.com>
2018-06-18 14:28:08 -0700

    Changed namespacing and superclass

--- app/jobs/assignment_repo/create_github_repository_job.rb
+++ app/jobs/assignment_repo/create_github_repository_job.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-# Should this class be namespaced inside AssignmentRepo?
-class CreateGitHubRepositoryJob < ActiveJob
-  queue_as :create_repository
+class AssignmentRepo
+  class CreateGitHubRepositoryJob < ApplicationJob
+    queue_as :create_repository
 
-  # Create an AssignmentRepo.
-  #
-  # assignment - The Assignment that will own the AssignmentRepo.
-  # user       - The User that the AssignmentRepo will belong to.
-  def perform(assignment, user)
+    # Create an AssignmentRepo.
+    #
+    # assignment - The Assignment that will own the AssignmentRepo.
+    # user       - The User that the AssignmentRepo will belong to.
+    def perform(assignment, user)
+      creator = Creator.new(assignment: assignment, user: user)
 
-    creator = AssignmentRepo::Creator.new(assignment: assignment, user: user)
+      creator.verify_organization_has_private_repos_available!
 
-    creator.verify_organization_has_private_repos_available!
+      assignment_repo = assignment.assignment_repos.build(
+        github_repo_id: creator.create_github_repository!,
+        user: user
+      )
 
-    assignment_repo = assignment.assignment_repos.build(
-      github_repo_id: creator.create_github_repository!,
-      user: user
-    )
+      creator.add_user_to_repository!(assignment_repo.github_repo_id)
 
-    creator.add_user_to_repository!(assignment_repo.github_repo_id)
+      # on success kick off next cascading job
 
-    # on success kick off next cascading job
-
-  rescue Result::Error => err
-    creator.delete_github_repository(assignment_repo.try(:github_repo_id))
-    AssignmentRepo::Creator.failed(err.message)
+    rescue Creator::Result::Error => err
+      creator.delete_github_repository(assignment_repo.try(:github_repo_id))
+      Creator::Result.failed(err.message)
+    end
   end
 end
