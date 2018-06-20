eb0a295efa7a2da91a9393246937e007c995c850
BenEmdon <benemdon@github.com>
2018-06-20 11:48:16 -0700

    Please the hounds

--- app/controllers/concerns/starter_code.rb
+++ app/controllers/concerns/starter_code.rb
@@ -9,7 +9,9 @@ module StarterCode
   def starter_code_repository_id(repo_name)
     return if repo_name.blank?
 
+    # rubocop:disable Metrics/LineLength
     raise GitHub::Error, WRONG_FORMAT unless repo_name.match?(%r{^#{GitHub::Search::USERNAME_REGEX}\/#{GitHub::Search::REPOSITORY_REGEX}$})
+    # rubocop:enable Metrics/LineLength
 
     begin
       # rubocop:disable Rails/DynamicFindBy
