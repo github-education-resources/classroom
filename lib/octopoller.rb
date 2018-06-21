06355b69bef5aa30840a0beb03ea460eff4390b0
BenEmdon <benemdon@github.com>
2018-06-21 13:17:33 -0700

    Added specs, and cleaned up poll implementation

--- lib/octopoller.rb
+++ lib/octopoller.rb
@@ -3,27 +3,23 @@
 module Octopoller
   class TimeoutError < StandardError; end
 
-  def poll(wait: 1, timeout: 15, error_handler: nil, &block)
-    if wait < 0
-      raise ArgumentError, "Cannot poll backwards in time"
-    end
-    if timeout < 0
-      raise ArgumentError, "Timed out without even being able to try"
-    end
+  # rubocop:disable MethodLength
+  def poll(wait: 1, timeout: 15, error_handler: nil)
+    raise ArgumentError, "Cannot poll backwards in time" if wait.negative?
+    raise ArgumentError, "Timed out without even being able to try" if timeout.negative?
 
-    start = Time.now
-    while Time.now < start + timeout
+    start = Time.now.utc
+    while Time.now.utc < start + timeout
       begin
-        return block.call
-      rescue Exception => e
-        unless error_handler.nil?
-          error_handler.call(e)
-        end
+        return yield
+      rescue => e
+        error_handler&.call(e) if error_handler
         sleep wait
       end
     end
     raise TimeoutError, "Polling timed out paitently"
   end
+  # rubocop:enable MethodLength
 
   module_function :poll
 end
