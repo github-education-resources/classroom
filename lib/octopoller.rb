de7f86422be08eb155f7b197dcbec3b4c0568fd2
BenEmdon <benemdon@github.com>
2018-06-21 13:37:42 -0700

    Added docs

--- lib/octopoller.rb
+++ lib/octopoller.rb
@@ -3,6 +3,15 @@
 module Octopoller
   class TimeoutError < StandardError; end
 
+  # Polls until a sccuessfu
+  # Continues to poll when an error is caight
+  #
+  # wait - The time delay in seconds between polls (default is 1)
+  # timeout - The maximum number of seconds the poller poll (default is 15)
+  # error_handler - A proc that will be passed an instance of each raised error
+  # yield - A block that will execute, and if it raises an error it will re-run until success or the timeout is reached
+  # raise - Raises a Octopoller::TimeoutError if the timout is reached
+  #
   # rubocop:disable MethodLength
   def poll(wait: 1, timeout: 15, error_handler: nil)
     raise ArgumentError, "Cannot poll backwards in time" if wait.negative?
