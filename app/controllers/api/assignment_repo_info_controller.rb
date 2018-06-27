f12d25b860b0c8854aa51b5eb6352620ba49b8ae
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-06-26 23:10:18 -0700

    added security headers for cors

--- app/controllers/api/assignment_repo_info_controller.rb
+++ app/controllers/api/assignment_repo_info_controller.rb
@@ -4,13 +4,14 @@ class API::AssignmentRepoInfoController < ApplicationController
     include OrganizationAuthorization
 
     before_action :set_assignment
+    before_action :add_security_headers
 
     def repos
       assignment_repos = AssignmentRepo.where(assignment: @assignment)
       assignment_repo_params = assignment_repos.map{ |a| {
           id: a.id,
           username: a.user.github_user.login, 
-          repo_url: a.github_repository.html_url
+          repoUrl: a.github_repository.html_url,
       }}
       paginate json: assignment_repo_params
     end
@@ -26,5 +27,12 @@ class API::AssignmentRepoInfoController < ApplicationController
 
     def set_assignment
       @assignment = @organization.assignments.includes(:assignment_invitation).find_by!(slug: params[:id])
+    end
+
+    def add_security_headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'GET'
+      response.headers['Access-Control-Allow-Headers'] = '*'
+      response.headers['Access-Control-Expose-Headers'] = 'Total, Link, Per-Page'
     end
 end
