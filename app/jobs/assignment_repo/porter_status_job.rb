e97e290d20638ea9735381de8c8c3b2b0b4a61f7
BenEmdon <benemdon@github.com>
2018-06-29 10:50:23 -0700

    Pass assignment repo instead of GitHub repo

--- app/jobs/assignment_repo/porter_status_job.rb
+++ app/jobs/assignment_repo/porter_status_job.rb
@@ -6,29 +6,28 @@ class AssignmentRepo
     queue_as :porter_status
     retry_on Octopoller::TimeoutError, queue: :porter_status
 
-    def perform(github_repository_id, user)
-      github_repository = GitHubRepository.find!(github_repository_id)
+    def perform(assignment_repo, user)
+      github_repository = assignment_repo.github_repository
 
       result = Octopoller.poll do # TODO log errors
-        case github_repository.import_progress[:status]
+        progress = github_repository.import_progress[:status]
+        case progress
         when *GitHubRepository::IMPORT_ONGOING
           raise ImportInProgress, Creator::IMPORT_ONGOING
         when *GitHubRepository::IMPORT_ERRORS
-          return Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
+          Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
         when GitHubRepository::IMPORT_COMPLETE
-          return Creator::REPOSITORY_CREATION_COMPLETE
+          Creator::REPOSITORY_CREATION_COMPLETE
         end
       end
 
       case result
       when Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
-        # broadcast that the user should retry
         ActionCable.server.broadcast(
           RepositoryCreationStatusChannel.channel(user_id: user.id),
           text: result
         )
       when Creator::REPOSITORY_CREATION_COMPLETE
-        # broadcast good vibes
         ActionCable.server.broadcast(
           RepositoryCreationStatusChannel.channel(user_id: user.id),
           text: result
