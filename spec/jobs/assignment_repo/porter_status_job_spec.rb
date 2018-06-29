fcb911408974f3868aa9c8c171899f78bd7e966c
BenEmdon <benemdon@github.com>
2018-06-29 10:50:56 -0700

    Added success state spec

--- spec/jobs/assignment_repo/porter_status_job_spec.rb
+++ spec/jobs/assignment_repo/porter_status_job_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe AssignmentRepo::PorterStatusJob, type: :job do
 
   let(:assignment) do
     options = {
-      title: "Learn Elm",
-      starter_code_repo_id: 1_062_897,
+      title: "small-test-repo",
+      starter_code_repo_id: 2276615,
       organization: organization,
       students_are_repo_admins: true
     }
@@ -29,7 +29,7 @@ RSpec.describe AssignmentRepo::PorterStatusJob, type: :job do
 
   before(:each) do
     github_organization = GitHubOrganization.new(@client, organization.github_id)
-    @github_repository  = github_organization.create_repository("test-repository", private: true)
+    @repo = github_organization.create_repository(assignment.title, private: true)
   end
 
   after do
@@ -38,7 +38,8 @@ RSpec.describe AssignmentRepo::PorterStatusJob, type: :job do
   end
 
   after(:each) do
-    @client.delete_repository(@github_repository.id)
+    @client.delete_repository(@repo.id)
+    AssignmentRepo.destroy_all
   end
 
   describe "successful repo creation", :vcr do
@@ -46,5 +47,52 @@ RSpec.describe AssignmentRepo::PorterStatusJob, type: :job do
       subject.perform_later
       expect(subject).to have_been_enqueued.on_queue("porter_status")
     end
+
+    context "started importing starter code" do
+      before do
+        creator = AssignmentRepo::Creator.new(assignment: assignment, user: student)
+        creator.push_starter_code!(@repo.id)
+        @assignment_repo = AssignmentRepo.new(assignment: assignment)
+        @assignment_repo.github_repo_id = @repo.id
+        @assignment_repo.save!
+      end
+
+      it "completes when porter status is complete" do
+        stub_request(:get, github_url("/repos/#{@repo.full_name}/import"))
+          .to_return(
+            {
+              status: 201,
+              body: request_stub("importing"),
+              headers: { "Content-Type": "application/json"}
+            }
+          ).times(2).then
+          .to_return(
+            {
+              status: 200,
+              body: request_stub("complete"),
+              headers: { "Content-Type": "application/json"}
+            }
+          ).times(2)
+        subject.perform_now(@assignment_repo, student)
+        expect(@assignment_repo.github_repository.imported?).to be_truthy
+      end
+    end
+  end
+
+  def request_stub(status)
+    {
+      "vcs": "git",
+      "use_lfs": "undecided",
+      "vcs_url": "https://github.com/rtyley/small-test-repo",
+      "status": "#{status}",
+      "commit_count": nil,
+      "status_text": "Importing...",
+      "authors_count": 0,
+      "import_percent": nil,
+      "url": "https://api.github.com/repos/classroom-test-org-edon/small-test-repo/import",
+      "html_url": "https://github.com/classroom-test-org-edon/small-test-repo/import",
+      "authors_url": "https://api.github.com/repos/classroom-test-org-edon/small-test-repo/import/authors",
+      "repository_url": "https://api.github.com/repos/classroom-test-org-edon/small-test-repo"
+    }.to_json
   end
 end
