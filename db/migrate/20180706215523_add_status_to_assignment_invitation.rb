bbb7eda76b9a42df101a58516fbf9a68ddf1dd41
BenEmdon <benemdon@github.com>
2018-07-11 15:54:04 -0700

    Roll back migration in favor of #1386

--- db/migrate/20180706215523_add_status_to_assignment_invitation.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToAssignmentInvitation < ActiveRecord::Migration[5.1]
-  def change
-    add_column :assignment_invitations, :status, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180706215523) do
+ActiveRecord::Schema.define(version: 20180223161920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20180706215523) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.string "short_key"
-    t.integer "status"
     t.index ["assignment_id"], name: "index_assignment_invitations_on_assignment_id"
     t.index ["deleted_at"], name: "index_assignment_invitations_on_deleted_at"
     t.index ["key"], name: "index_assignment_invitations_on_key", unique: true
