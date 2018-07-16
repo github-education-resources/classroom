815165f8228be7774334a286cc3f97f6f562e26d
BenEmdon <benemdon@github.com>
2018-07-16 11:21:00 -0700

    No longer retry on Octopoller::TimeoutError Instead rescue and kick off a PorterStatusJob

--- app/jobs/assignment_repo/porter_status_job.rb
+++ app/jobs/assignment_repo/porter_status_job.rb
@@ -3,7 +3,6 @@
 class AssignmentRepo
   class PorterStatusJob < ApplicationJob
     queue_as :porter_status
-    retry_on Octopoller::TimeoutError, attempts: 10, queue: :porter_status
 
     # rubocop:disable MethodLength
     # rubocop:disable AbcSize
@@ -11,40 +10,44 @@ class AssignmentRepo
     def perform(assignment_repo, user)
       github_repository = assignment_repo.github_repository
 
-      result = Octopoller.poll(timeout: 30) do
-        begin
-          progress = github_repository.import_progress[:status]
-          case progress
-          when GitHubRepository::IMPORT_COMPLETE
-            Creator::REPOSITORY_CREATION_COMPLETE
-          when *GitHubRepository::IMPORT_ERRORS
+      begin
+        result = Octopoller.poll(timeout: 30) do
+          begin
+            progress = github_repository.import_progress[:status]
+            case progress
+            when GitHubRepository::IMPORT_COMPLETE
+              Creator::REPOSITORY_CREATION_COMPLETE
+            when *GitHubRepository::IMPORT_ERRORS
+              Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
+            when *GitHubRepository::IMPORT_ONGOING
+              logger.info AssignmentRepo::Creator::IMPORT_ONGOING
+              :re_poll
+            end
+          rescue GitHub::Error => error
+            logger.warn error.to_s
             Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
-          when *GitHubRepository::IMPORT_ONGOING
-            logger.info AssignmentRepo::Creator::IMPORT_ONGOING
-            :re_poll
           end
-        rescue GitHub::Error => error
-          logger.warn error.to_s
-          Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
         end
-      end
 
-      case result
-      when Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
-        assignment_repo.assignment.invitation&.errored!
-        ActionCable.server.broadcast(
-          RepositoryCreationStatusChannel.channel(user_id: user.id),
-          text: result,
-          status: assignment_repo.assignment.invitation&.status
-        )
-        logger.warn result.to_s
-      when Creator::REPOSITORY_CREATION_COMPLETE
-        assignment_repo.assignment.invitation&.completed!
-        ActionCable.server.broadcast(
-          RepositoryCreationStatusChannel.channel(user_id: user.id),
-          text: result,
-          status: assignment_repo.assignment.invitation&.status
-        )
+        case result
+        when Creator::REPOSITORY_STARTER_CODE_IMPORT_FAILED
+          assignment_repo.assignment.invitation&.errored!
+          ActionCable.server.broadcast(
+            RepositoryCreationStatusChannel.channel(user_id: user.id),
+            text: result,
+            status: assignment_repo.assignment.invitation&.status
+          )
+          logger.warn result.to_s
+        when Creator::REPOSITORY_CREATION_COMPLETE
+          assignment_repo.assignment.invitation&.completed!
+          ActionCable.server.broadcast(
+            RepositoryCreationStatusChannel.channel(user_id: user.id),
+            text: result,
+            status: assignment_repo.assignment.invitation&.status
+          )
+        end
+      rescue Octopoller::TimeoutError
+        PorterStatusJob.perform_later(assignment_repo, user)
       end
     end
     # rubocop:enable MethodLength
