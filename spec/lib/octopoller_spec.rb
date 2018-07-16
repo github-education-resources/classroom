30ac53a04f9356fd45db70541378f461d5556e63
BenEmdon <benemdon@github.com>
2018-07-16 11:25:12 -0700

    Remove commented out test 🔪

--- spec/lib/octopoller_spec.rb
+++ spec/lib/octopoller_spec.rb
@@ -45,40 +45,4 @@ RSpec.describe Octopoller do
     end.to raise_error(StandardError, "An error occuered")
   end
 
-
-  # describe "polls and captures unique values in order" do
-  #   before(:all) do
-  #     @errors = []
-  #     error_handler = proc { |error| @errors << error }
-  #     counter = 0
-  #     Octopoller.poll(wait: 0.1, error_handler: error_handler) do
-  #       counter += 1
-  #       case counter
-  #       when 1
-  #         re_poll
-  #         "Counter is 1"
-  #       when 2
-  #         re_poll
-  #         "Counter is 2"
-  #       when 3
-  #         re_poll
-  #         "Counter is 3"
-  #       when 4
-  #         counter
-  #       end
-  #     end
-  #   end
-
-  #   it "captured the first error" do
-  #     expect { raise @errors[0] }.to raise_error(StandardError, "Counter is 1")
-  #   end
-
-  #   it "captured the second error" do
-  #     expect { raise @errors[1] }.to raise_error(StandardError, "Counter is 2")
-  #   end
-
-  #   it "captured the thrid error" do
-  #     expect { raise @errors[2] }.to raise_error(StandardError, "Counter is 3")
-  #   end
-  # end
 end
