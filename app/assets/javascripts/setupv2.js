b27807475e5a463e7f3c9ae79dca1952e47c51da
BenEmdon <benemdon@github.com>
2018-07-16 17:31:15 -0700

    Refactored visual state code to reset each progress check

--- app/assets/javascripts/setupv2.js
+++ app/assets/javascripts/setupv2.js
@@ -2,16 +2,20 @@
   var POLL_INTERVAL = 3000;
   var check_progress,
     display_progress,
-    display_retry_button,
+    get_status,
+    hide_border_and_background,
+    hide_indicator_image,
     hide_retry_button,
     indicate_completion,
     indicate_failure,
     indicate_in_progress,
+    indicate_waiting,
     invitation_path,
     is_hidden,
     progress_path,
     ready,
     setup_retry_button,
+    show_retry_button,
     show_success,
     start_job,
     success_path;
@@ -20,67 +24,7 @@
     return element.hasClass("d-none");
   };
 
-  indicate_completion = function(step_indicator) {
-    $(step_indicator).addClass("border-green bg-green-light");
-
-    var status, completion_indicator;
-    status = $(step_indicator).find(".status");
-    if (is_hidden(status)) {
-      status.removeClass("d-none");
-    }
-
-    completion_indicator = $(status).find(".complete-indicator");
-    failure_indicator = $(status).find(".failure-indicator");
-    spinner_indicator = $(status).find(".spinner");
-
-    $(completion_indicator).show();
-    $(failure_indicator).hide();
-    $(spinner_indicator).hide();
-
-    hide_retry_button();
-  };
-
-  indicate_failure = function(step_indicator) {
-    $(step_indicator).addClass("border-red bg-red-light");
-
-    var status, failure_indicator;
-    status = $(step_indicator).find(".status");
-    if (is_hidden(status)) {
-      status.removeClass("d-none");
-    }
-
-    completion_indicator = $(status).find(".complete-indicator");
-    failure_indicator = $(status).find(".failure-indicator");
-    spinner_indicator = $(status).find(".spinner");
-
-    $(failure_indicator).show();
-    $(completion_indicator).hide();
-    $(spinner_indicator).hide();
-
-    display_retry_button();
-  };
-
-  indicate_in_progress = function(step_indicator) {
-    var status;
-    status = $(step_indicator).find(".status");
-    if (is_hidden(status)) {
-      status.removeClass("d-none");
-    }
-
-    completion_indicator = $(status).find(".complete-indicator");
-    failure_indicator = $(status).find(".failure-indicator");
-    spinner_indicator = $(status).find(".spinner");
-
-    $(step_indicator).addClass("border-green");
-
-    $(spinner_indicator).show();
-    $(failure_indicator).hide();
-    $(completion_indicator).hide();
-
-    hide_retry_button();
-  };
-
-  display_retry_button = function() {
+  show_retry_button = function() {
     var retry_button = $("#retry-button");
     if (is_hidden(retry_button)) {
       retry_button.removeClass("d-none");
@@ -94,28 +38,93 @@
     }
   };
 
+  hide_border_and_background = function(step_indicator) {
+    if (step_indicator.hasClass("border-green bg-green-light")) {
+      step_indicator.removeClass("border-green bg-green-light");
+    }
+    if (step_indicator.hasClass("border-red bg-red-light")) {
+      step_indicator.removeClass("border-red bg-red-light");
+    }
+    if (step_indicator.hasClass("border-green bg-white")) {
+      step_indicator.removeClass("border-green bg-white");
+    }
+  };
+
+  hide_indicator_image = function(step_indicator) {
+    var status = step_indicator.find(".status");
+    status.find(".complete-indicator").hide();
+    status.find(".failure-indicator").hide();
+    status.find(".spinner").hide();
+  };
+
+  get_status = function(step_indicator) {
+    var status = step_indicator.find(".status");
+    if (is_hidden(status)) {
+      status.removeClass("d-none");
+    }
+    return status;
+  };
+
+  indicate_waiting = function(step_indicator) {
+    hide_border_and_background(step_indicator);
+    hide_indicator_image(step_indicator);
+  };
+
+  indicate_completion = function(step_indicator) {
+    hide_border_and_background(step_indicator);
+    hide_indicator_image(step_indicator);
+    step_indicator.addClass("border-green bg-green-light");
+    var status = get_status(step_indicator);
+
+    status.find(".complete-indicator").show();
+  };
+
+  indicate_failure = function(step_indicator) {
+    hide_border_and_background(step_indicator);
+    hide_indicator_image(step_indicator);
+    step_indicator.addClass("border-red bg-red-light");
+    var status = get_status(step_indicator);
+
+    status.find(".failure-indicator").show();
+  };
+
+  indicate_in_progress = function(step_indicator) {
+    hide_border_and_background(step_indicator);
+    hide_indicator_image(step_indicator);
+    step_indicator.addClass("border-green bg-white");
+    var status = get_status(step_indicator);
+
+    status.find(".spinner").show();
+  };
+
   display_progress = function(progress) {
     var create_repo_progress_indicator = $("#create-repo-progress");
     var import_repo_progress_indicator = $("#import-repo-progress");
     switch(progress.status) {
       case "creating_repo":
         indicate_in_progress(create_repo_progress_indicator);
+        indicate_waiting(import_repo_progress_indicator);
+        hide_retry_button();
         break;
       case "importing_starter_code":
         indicate_completion(create_repo_progress_indicator);
         indicate_in_progress(import_repo_progress_indicator);
+        hide_retry_button();
         break;
       case "errored_creating_repo":
         indicate_failure(create_repo_progress_indicator);
         indicate_failure(import_repo_progress_indicator);
+        show_retry_button();
         break;
       case "errored_importing_starter_code":
         indicate_completion(create_repo_progress_indicator);
         indicate_failure(import_repo_progress_indicator);
+        show_retry_button();
         break;
       case "completed":
         indicate_completion(create_repo_progress_indicator);
         indicate_completion(import_repo_progress_indicator);
+        hide_retry_button();
         setTimeout(show_success, 500);
         break;
     }
