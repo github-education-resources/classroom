f576ad9a4a6bfc1e016265ae4126104fea4dc7d6
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-07-17 11:21:45 -0700

    moved security headers to application controller

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,16 +2,27 @@
 
 class API::ApplicationController < ApplicationController
   include Rails::Pagination
+
+  before_action :add_security_headers
   
   def authenticate_user!
     return become_active if logged_in? && adequate_scopes? && current_user.authorized_access_token?
-    return_forbidden()
+    render_forbidden()
   end
 
-  def return_forbidden()
+  private
+
+  def render_forbidden()
     render json: {
       message: "Unauthorized"
     }, status: :forbidden
+  end
+
+  def add_security_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Expose-Headers'] = 'Total, Link, Per-Page'
   end
 
 end
