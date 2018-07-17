50761a9d21385be5e6fd6372ba668b7d3447dd48
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-07-16 17:12:32 -0700

    new api assignments controller test

--- spec/controllers/api/assignments_controller_spec.rb
+++ spec/controllers/api/assignments_controller_spec.rb
@@ -1,32 +1,79 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe API::AssignmentsController, type: :controller do
-#   let(:organization)      { classroom_org                                   }
-#   let(:user)              { classroom_teacher                               }
-#   let(:assignment)        { create(:assignment, organization: organization) }
-#   let(:group_assignment)  { create(:group_assignment, organization: organization) }
+RSpec.describe API::AssignmentsController, type: :controller do
+  let(:organization)      { classroom_org                                   }
+  let(:user)              { classroom_teacher                               }
+  
 
-#   before do
-#     sign_in_as(user)
-#   end
+  before do
+    sign_in_as(user)
+  end
 
-#   describe "GET #index", :vcr do
+  describe "GET #index", :vcr do
 
-#     context "individual assignment" do
-#       before do
-#         get :index, params: {organization_id: organization.slug, type: "individual"}
-#       end
+    context "only individual assignment" do
+      before do
+        @assignment = create(:assignment, organization: organization)
+        get :index, params: {organization_id: organization.slug}
+      end
 
-#       it "returns success" do
-#         expect(response).to have_http_status(:success)
-#       end
+      it "returns success" do
+        expect(response).to have_http_status(:success)
+      end
 
-#       it "returns all of user's indiviudal assignments" do
-#         expect(json.length).to eql(1)
-#       end
-#     end
+      it "returns only one assignment" do
+        expect(json.length).to eql(1)
+      end
 
-#   end
-# end
+      it "returns assignment title" do
+        expect(json.first['title']).to eql(@assignment.title)
+      end
+
+      it "returns individual assignment type" do
+        expect(json.first['type']).to eql("individual")
+      end
+    end
+
+    context "only group assignment" do
+      before do
+        @group_assignment = create(:group_assignment, organization: organization)
+        get :index, params: {organization_id: organization.slug}
+      end
+
+      it "returns success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns only one assignment" do
+        expect(json.length).to eql(1)
+      end
+
+      it "returns group assignment title" do
+        expect(json.first['title']).to eql(@group_assignment.title)
+      end
+
+      it "returns group assignment type" do
+        expect(json.first['type']).to eql("group")
+      end
+    end
+
+    context "both individual and group assignments" do
+      before do
+        @group_assignment = create(:group_assignment, organization: organization)
+        @individual_assignment = create(:assignment, organization: organization)
+        get :index, params: {organization_id: organization.slug}
+      end
+
+      it "returns success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns both assignments" do
+        expect(json.length).to eql(2)
+      end
+    end
+
+  end
+end
