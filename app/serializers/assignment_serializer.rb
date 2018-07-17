08f526f5688f1a1e4d61d014f176012ed3a70fce
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-07-17 11:22:26 -0700

    add organization github id to serializers for future use

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,8 +1,14 @@
 class AssignmentSerializer < ActiveModel::Serializer
+  attributes :id
   attributes :title
   attributes :type
+  attributes :organization_github_id
 
   def type
     :individual
+  end
+
+  def organization_github_id
+    object.organization.github_id
   end
 end
--- app/serializers/group_assignment_serializer.rb
+++ app/serializers/group_assignment_serializer.rb
@@ -1,8 +1,14 @@
 class GroupAssignmentSerializer < ActiveModel::Serializer
+  attributes :id
   attributes :title
   attributes :type
+  attributes :organization_github_id
 
   def type
     :group
+  end
+
+  def organization_github_id
+    object.organization.github_id
   end
 end
