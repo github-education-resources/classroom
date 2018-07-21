2981e9c869fb74a71e4b4d346d5a30de5b948424
BenEmdon <benemdon@github.com>
2018-07-20 17:25:14 -0700

    Added test

--- app/models/invite_status.rb
+++ app/models/invite_status.rb
@@ -5,13 +5,13 @@ class InviteStatus < ApplicationRecord
   belongs_to :user
 
   enum status: {
-    unaccepted:                     0
-    accepted:                       1
-    waiting:                        2
-    creating_repo:                  3
-    importing_starter_code:         4
-    completed:                      5
-    errored_creating_repo:          6
-    errored_importing_starter_code: 7
+    unaccepted:                     0,
+    accepted:                       1,
+    waiting:                        2,
+    creating_repo:                  3,
+    importing_starter_code:         4,
+    completed:                      5,
+    errored_creating_repo:          6,
+    errored_importing_starter_code: 7,
   }
 end
--- spec/models/invite_status_spec.rb
+++ spec/models/invite_status_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe InviteStatus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { InviteStatus }
+
+  it "has a default status of unaccepted" do
+    invite_status = subject.create()
+    expect(invite_status.unaccepted?).to be_truthy
+  end
 end
