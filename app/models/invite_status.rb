c66ebadae06ad09b56e973a070f415755336d68c
BenEmdon <benemdon@github.com>
2018-07-23 15:32:41 -0700

    Added validation that: - assignment_invitation is present - user is present - uniqueness of the set <assignment_invitation, user>

--- app/models/invite_status.rb
+++ app/models/invite_status.rb
@@ -4,6 +4,14 @@ class InviteStatus < ApplicationRecord
   belongs_to :assignment_invitation
   belongs_to :user
 
+  validates :user_id, uniqueness: {
+    scope: :assignment_invitation_id,
+    message: "should only have 1 invitation per user"
+  }
+
+  validates :assignment_invitation, presence: true
+  validates :user, presence: true
+
   enum status: {
     unaccepted:                     0,
     accepted:                       1,
