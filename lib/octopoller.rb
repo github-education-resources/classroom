22edbf77cdc1b9c73a3d9b0a9e86f0f2535e9842
BenEmdon <benemdon@github.com>
2018-07-24 20:50:45 -0700

    Added try function

--- lib/octopoller.rb
+++ lib/octopoller.rb
@@ -2,17 +2,18 @@
 
 module Octopoller
   class TimeoutError < StandardError; end
+  class TooManyAttemptsError < StandardError; end
 
   # Polls until success
-  # Re-runs when an error is caught
+  # Re-runs when the block returns `:re_poll`
   #
-  # wait - The time delay in seconds between polls (default is 1 second)
+  # wait    - The time delay in seconds between polls (default is 1 second)
   # timeout - The maximum number of seconds the poller will run (default is 15 seconds)
-  # error_handler - A proc that will run with each instance of an error
-  # yield - A block that will execute, and if it raises an error it will re-run until success or the timeout is reached
-  # raise - Raises an Octopoller::TimeoutError if the timeout is reached
+  # yield   - A block that will execute, and if `:re_poll` is returned it will re-run
+  #         - Re-runs until success or the timeout is reached
+  # raise   - Raises an Octopoller::TimeoutError if the timeout is reached
   def poll(wait: 1.second, timeout: 15.seconds)
-    raise ArgumentError, "Cannot poll backwards in time" if wait.negative?
+    raise ArgumentError, "Cannot wait backwards in time" if wait.negative?
     raise ArgumentError, "Timed out without even being able to try" if timeout.negative?
 
     start = Time.now.utc
@@ -24,5 +25,38 @@ module Octopoller
     raise TimeoutError, "Polling timed out paitently"
   end
 
+  # Tries until success
+  # Re-runs when the block returns `:retry`
+  #
+  # wait      - The time delay in seconds between attempts (default is 1 second)
+  #           - When given the argument `:exponentially` the action will be retried with exponetial backoff
+  # attempts  - The maximum number of attempts the action will be performed (default is 15)
+  # yield     - A block that will execute, and if `:retry` is returned it will re-run the action
+  #           - re-run the action until success or the max number of attempts is reached
+  # raise     - Raises an Octopoller::TooManyTriesError if the max number of attempts is reached
+  #
+  # rubocop:disable MethodLength
+  # rubocop:disable CyclomaticComplexity
+  # rubocop:disable PerceivedComplexity
+  def try(wait: 1.second, attempts: 15)
+    exponential_backoff = wait == :exponentially
+    raise ArgumentError, "Cannot wait backwards in time" unless exponential_backoff || wait.positive?
+    raise ArgumentError, "Cannot try something a negative number of attempts" if attempts.negative?
+    raise ArgumentError, "Cannot try something zero attempts" if attempts.zero?
+
+    wait = 0.5.seconds if exponential_backoff
+    attempts.times do
+      block_value = yield
+      return block_value unless block_value == :retry
+      sleep wait
+      wait *= 2 if exponential_backoff
+    end
+    raise TooManyAttemptsError, "Tried maximum number of attempts"
+  end
+  # rubocop:enable MethodLength
+  # rubocop:enable CyclomaticComplexity
+  # rubocop:enable PerceivedComplexity
+
   module_function :poll
+  module_function :try
 end
--- spec/lib/octopoller_spec.rb
+++ spec/lib/octopoller_spec.rb
@@ -3,47 +3,128 @@
 require "rails_helper"
 
 RSpec.describe Octopoller do
-  it "does not accept negative wait time" do
-    expect { Octopoller.poll(wait: -(1.second)) {} }
-      .to raise_error(ArgumentError, "Cannot poll backwards in time")
-  end
+  describe "poll" do
+    it "does not accept negative wait time" do
+      expect { Octopoller.poll(wait: -(1.second)) {} }
+        .to raise_error(ArgumentError, "Cannot wait backwards in time")
+    end
 
-  it "does not accept negative timeout" do
-    expect { Octopoller.poll(timeout: -(1.second)) {} }
-      .to raise_error(ArgumentError, "Timed out without even being able to try")
-  end
+    it "does not accept negative timeout" do
+      expect { Octopoller.poll(timeout: -(1.second)) {} }
+        .to raise_error(ArgumentError, "Timed out without even being able to try")
+    end
 
-  it "polls until timeout" do
-    expect do
-      Octopoller.poll(wait: 0.1.seconds, timeout: 0.5.seconds) do
-        :re_poll
+    it "polls until timeout" do
+      expect do
+        Octopoller.poll(wait: 0.01.seconds, timeout: 0.05.seconds) do
+          :re_poll
+        end
+      end.to raise_error(Octopoller::TimeoutError, "Polling timed out paitently")
+    end
+
+    it "exits on successful return" do
+      result = Octopoller.poll { "Success!" }
+      expect(result).to eq("Success!")
+    end
+
+    it "polls until successful return" do
+      polled = false
+      result = Octopoller.poll(wait: 0.01.seconds) do
+        if polled
+          "Success!"
+        else
+          polled = true
+          :re_poll
+        end
       end
-    end.to raise_error(Octopoller::TimeoutError, "Polling timed out paitently")
+      expect(result).to eq("Success!")
+    end
+
+    it "doesn't swallow a raised error" do
+      expect do
+        Octopoller.poll(wait: 0.01.seconds) do
+          raise StandardError, "An error occuered"
+        end
+      end.to raise_error(StandardError, "An error occuered")
+    end
   end
 
-  it "exits on successful return" do
-    result = Octopoller.poll { "Success!" }
-    expect(result).to eq("Success!")
-  end
+  describe "try" do
+    it "does not accept negative wait time" do
+      expect { Octopoller.try(wait: -(1.second)) {} }
+        .to raise_error(ArgumentError, "Cannot wait backwards in time")
+    end
 
-  it "polls until successful return" do
-    polled = false
-    result = Octopoller.poll(wait: 0.1.seconds) do
-      if polled
-        "Success!"
-      else
-        polled = true
-        :re_poll
+    it "does not accept negative attempts" do
+      expect { Octopoller.try(attempts: -1) {} }
+        .to raise_error(ArgumentError, "Cannot try something a negative number of attempts")
+    end
+
+    it "does not accept negative attempts" do
+      expect { Octopoller.try(attempts: 0) {} }
+        .to raise_error(ArgumentError, "Cannot try something zero attempts")
+    end
+
+    it "try until max attempts reached" do
+      expect do
+        Octopoller.try(wait: 0.01.seconds, attempts: 2) do
+          :retry
+        end
+      end.to raise_error(Octopoller::TooManyAttemptsError, "Tried maximum number of attempts")
+    end
+
+    it "tries exactly the number of attempts" do
+      attempts = 0
+      expect do
+        Octopoller.try(wait: 0.01.seconds, attempts: 4) do
+          attempts += 1
+          :retry
+        end
+      end.to raise_error(Octopoller::TooManyAttemptsError, "Tried maximum number of attempts")
+      expect(attempts).to eq(4)
+    end
+
+    describe "tries with expoential back off" do
+      before(:all) do
+        start = Time.now.utc
+        @times = []
+        Octopoller.try(wait: :exponentially, attempts: 3) do
+          @times << Time.now.utc
+          :retry if @times.count < 3
+        end
+        @times = @times.map { |time| time - start }
+      end
+
+      it "attempts double in wait time" do
+        expect(@times[1]).to be > @times[0] * 2
+        expect(@times[2]).to be > @times[1] * 2
       end
     end
-    expect(result).to eq("Success!")
-  end
 
-  it "doesn't swallow a raised error" do
-    expect do
-      Octopoller.poll(wait: 0.1.seconds) do
-        raise StandardError, "An error occuered"
+    it "exits on successful return" do
+      result = Octopoller.try { "Success!" }
+      expect(result).to eq("Success!")
+    end
+
+    it "try until successful return" do
+      tried = false
+      result = Octopoller.try(wait: 0.01.seconds) do
+        if tried
+          "Success!"
+        else
+          tried = true
+          :retry
+        end
       end
-    end.to raise_error(StandardError, "An error occuered")
+      expect(result).to eq("Success!")
+    end
+
+    it "doesn't swallow a raised error" do
+      expect do
+        Octopoller.try(wait: 0.01.seconds) do
+          raise StandardError, "An error occuered"
+        end
+      end.to raise_error(StandardError, "An error occuered")
+    end
   end
 end
