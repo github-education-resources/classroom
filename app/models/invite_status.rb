31e283dc91b76432dfa08c5016b466d390692773
BenEmdon <benemdon@github.com>
2018-07-26 13:45:48 -0700

    Please the hound

--- app/models/invite_status.rb
+++ app/models/invite_status.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class InviteStatus < ApplicationRecord
-  ERRORED_STATUSES = %w[errored_creating_repo errored_importing_starter_code]
-  SETUP_STATUSES = %w[accepted waiting creating_repo importing_starter_code]
+  ERRORED_STATUSES = %w[errored_creating_repo errored_importing_starter_code].freeze
+  SETUP_STATUSES = %w[accepted waiting creating_repo importing_starter_code].freeze
 
   belongs_to :assignment_invitation
   belongs_to :user
