f36828075f14fc911f4ba6b377f3b9c5dcab93f1
BenEmdon <benemdon@github.com>
2018-08-03 10:28:43 -0700

    Added specs

---
+++ spec/models/group_invite_status_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe GroupInviteStatus, type: :model do
+  subject { GroupInviteStatus }
+  let(:organization) { classroom_org }
+  let(:grouping)     { create(:grouping, organization: organization) }
+  let(:group)        { create(:group, grouping: grouping) }
+  let(:invitation)   { create(:group_assignment_invitation) }
+
+  describe "valid", :vcr do
+    let(:invite_status) do
+      create(:group_invite_status, group: group, group_assignment_invitation: invitation)
+    end
+
+    it "has a default status of unaccepted" do
+      expect(invite_status.unaccepted?).to be_truthy
+    end
+
+    describe "errored?" do
+      it "is errored? when errored_creating_repo?" do
+        invite_status.errored_creating_repo!
+        expect(invite_status.errored?).to be_truthy
+      end
+
+      it "is errored? when errored_importing_starter_code?" do
+        invite_status.errored_importing_starter_code!
+        expect(invite_status.errored?).to be_truthy
+      end
+    end
+
+    describe "setting_up?" do
+      it "is setting_up? when accepted?" do
+        invite_status.accepted!
+        expect(invite_status.setting_up?).to be_truthy
+      end
+
+      it "is setting_up? when waiting?" do
+        invite_status.waiting!
+        expect(invite_status.setting_up?).to be_truthy
+      end
+
+      it "is setting_up? when creating_repo?" do
+        invite_status.creating_repo!
+        expect(invite_status.setting_up?).to be_truthy
+      end
+
+      it "is setting_up? when errored_importing_starter_code?" do
+        invite_status.importing_starter_code!
+        expect(invite_status.setting_up?).to be_truthy
+      end
+    end
+
+    describe "relationships" do
+      it "has a group" do
+        expect(invite_status.group).to eq(group)
+      end
+
+      it "has a group_assignment_invitation" do
+        expect(invite_status.group_assignment_invitation).to eq(invitation)
+      end
+    end
+  end
+
+  describe "invalid", :vcr do
+    it "without group_id" do
+      invite_status = subject.new(group_assignment_invitation_id: invitation.id)
+      expect(invite_status.valid?).to be_falsey
+    end
+
+    it "without group_assignment_invitation_id" do
+      invite_status = subject.new(group_id: group.id)
+      expect(invite_status.valid?).to be_falsey
+    end
+
+    it "when the set of group_id and group_assignment_invitation_id is not unique" do
+      create(:group_invite_status, group_id: group.id, group_assignment_invitation_id: invitation.id)
+      invite_status = subject.new(group_id: group.id, group_assignment_invitation_id: invitation.id)
+      expect { invite_status.save! }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Group should only have 1 invitation per group")
+    end
+  end
+end
