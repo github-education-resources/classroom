5308b8bcb8a4c33a52fde8849d964c4d7a11e456
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-15 10:38:45 -0700

    update footers to be green, new image with green background, based on wireframe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  layout "layouts/pages", except: :desktop
-  layout "layouts/desktop", only: :desktop
+  layout "layouts/pages"
 
   skip_before_action :authenticate_user!
 
