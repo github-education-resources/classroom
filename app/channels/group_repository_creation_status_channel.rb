076114fc78cd27b6b8ddda447dd23693ee0c604d
BenEmdon <benemdon@github.com>
2018-08-24 15:16:45 -0400

    Please the 🐕

--- app/channels/group_repository_creation_status_channel.rb
+++ app/channels/group_repository_creation_status_channel.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GroupRepositoryCreationStatusChannel < ApplicationCable::Channel
   def self.channel(group_id:, invitation_id:)
     "#{channel_name}_#{group_id}_#{invitation_id}"
