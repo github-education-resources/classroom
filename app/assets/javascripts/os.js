7a8b875dc607e679edc430bc2fb92b826cf74106
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-31 15:39:11 -0500

    please the hound and cleanup css

--- app/assets/javascripts/os.js
+++ app/assets/javascripts/os.js
@@ -3,16 +3,16 @@
   var htmlElement = document.documentElement;
 
   if (navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)) {
-    htmlElement.className = 'mac'
+    htmlElement.className = 'mac';
   } else {
-    htmlElement.className = 'windows'
+    htmlElement.className = 'windows';
   }
 
   document.addEventListener('click', function(event) {
     var target = event.target;
     if (target.getAttribute && target.getAttribute('data-action') === 'switch-os') {
       event.preventDefault();
-      htmlElement.className = target.getAttribute('data-os')
+      htmlElement.className = target.getAttribute('data-os');
     }
   })
 })();
