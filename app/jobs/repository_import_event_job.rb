865f65c1ed5f489ebbab2d1034207e9c7eeec2c6
BenEmdon <benemdon@github.com>
2018-09-10 17:34:16 -0400

    Remove repo_url from the repository_import_event_job

--- app/jobs/repository_import_event_job.rb
+++ app/jobs/repository_import_event_job.rb
@@ -10,20 +10,19 @@ class RepositoryImportEventJob < ApplicationJob
   def perform(payload_body)
     github_repo_id = payload_body.dig("repository", "id")
     status = payload_body.dig("status")
-    html_url = payload_body.dig("repository", "html_url")
 
     repo = AssignmentRepo.find_by(github_repo_id: github_repo_id)
     return if repo.blank?
     # Group assignments coming in a follow up PR
     # repo ||= GroupAssignmentRepo.find_by(github_repo_id: github_repo_id)
 
-    handle_assignment_repo(repo, status, html_url)
+    handle_assignment_repo(repo, status)
   end
 
   private
 
   # rubocop:disable MethodLength
-  def handle_assignment_repo(assignment_repo, status, html_url)
+  def handle_assignment_repo(assignment_repo, status)
     user = assignment_repo.user
     invitation = assignment_repo.assignment.invitation
     invite_status = invitation.status(user)
@@ -33,35 +32,33 @@ class RepositoryImportEventJob < ApplicationJob
     case status
     when "success"
       invite_status.completed!
-      broadcast_assignment_repo_success(user, invite_status, html_url)
+      broadcast_assignment_repo_success(user, invite_status)
       GitHubClassroom.statsd.increment("v3_exercise_repo.import.success")
     when "failure"
       invite_status.errored_importing_starter_code!
-      broadcast_assignment_repo_failure(user, invite_status, html_url)
+      broadcast_assignment_repo_failure(user, invite_status)
       GitHubClassroom.statsd.increment("v3_exercise_repo.import.failure")
     end
   end
   # rubocop:enable MethodLength
 
-  def broadcast_assignment_repo_success(user, invite_status, html_url)
+  def broadcast_assignment_repo_success(user, invite_status)
     ActionCable.server.broadcast(
       RepositoryCreationStatusChannel.channel(user_id: user.id),
       text: CREATE_COMPLETE,
       status: invite_status.status,
       percent: 100,
-      status_text: "Done",
-      repo_url: html_url
+      status_text: "Done"
     )
   end
 
-  def broadcast_assignment_repo_failure(user, invite_status, html_url)
+  def broadcast_assignment_repo_failure(user, invite_status)
     ActionCable.server.broadcast(
       RepositoryCreationStatusChannel.channel(user_id: user.id),
       error: IMPORT_FAILED,
       status: invite_status.status,
       percent: nil,
-      status_text: "Failed",
-      repo_url: html_url
+      status_text: "Failed"
     )
   end
 end
