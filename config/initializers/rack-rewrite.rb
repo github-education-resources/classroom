7151db8131270e2244156425519ed63db2f91056
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-16 22:48:44 -0500

    update rewrite to point to new server URL + add docs

--- config/initializers/rack-rewrite.rb
+++ config/initializers/rack-rewrite.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+# URL for Nuts Server deployed on Heroku, redirects download/update requests to latest release
+# on GitHub Releases with platform parameters
+RELEASE_SERVER_URL = "http://classroom-desktop-releases.herokuapp.com"
+
 Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-  r301   %r{/desktop/download(.*)},  'http://classroom-desktop-deploy-test.herokuapp.com/download$1'
-  r301   %r{/desktop/update(.*)},  'http://classroom-desktop-deploy-test.herokuapp.com/update$1'
+  r301   %r{/assistant/download(.*)},  "#{RELEASE_SERVER_URL}/download$1"
+  r301   %r{/assistant/update(.*)},  "#{RELEASE_SERVER_URL}/update$1"
 end
