994fa4d0b9a2991282240fedf63106fecc16c185
Nathaniel Woodthorpe <d12@github.com>
2018-09-18 18:08:37 -0400

    Remove index as well

--- db/migrate/20180918215254_global_relay_fields_for_assignments_orgs_and_users.rb
+++ db/migrate/20180918215254_global_relay_fields_for_assignments_orgs_and_users.rb
@@ -1,6 +1,7 @@
 class GlobalRelayFieldsForAssignmentsOrgsAndUsers < ActiveRecord::Migration[5.1]
   def change
     # With GraphQL in Classroom, an objects global_relay_id will refer to it's local ID. The GitHub global relay ID refers to it's external counterparts ID
+    remove_index :assignment_repos, :global_relay_id
     rename_column :assignment_repos, :global_relay_id, :github_global_relay_id
 
     add_column :group_assignment_repos, :global_relay_id, :string
