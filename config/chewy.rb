a9084ae775f0fa0945e5404f3208601435a974f2
BenEmdon <benemdon@github.com>
2018-09-19 12:12:36 -0400

    Move chewy config/chewy.rb into config/initializers/chewy.rb

--- config/chewy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Use ActiveJob config for async index updates.
-Chewy.strategy(:active_job)
-
-# Chewy wraps controller actions in the atomic strategy by default.
-# We change this to use ActiveJob here. Makes index updates async, outside
-# of the request.
-Chewy.request_strategy = :active_job
--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
-# configure chewy
-require "./config/chewy"
+## Configure chewy
+
+# Use ActiveJob config for async index updates.
+Chewy.strategy(:active_job)
+
+# Chewy wraps controller actions in the atomic strategy by default.
+# We change this to use ActiveJob here. Makes index updates async, outside
+# of the request.
+Chewy.request_strategy = :active_job
 
 # Set Chewy to log level INFO in production
 if Rails.env.production?
