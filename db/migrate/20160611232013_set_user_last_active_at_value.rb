efeffd3364805cdc0b9bd65c8220b8833364f079
BenEmdon <benemdon@github.com>
2018-09-20 17:01:29 -0400

    Remove last usage of stafftool index

--- db/migrate/20160611232013_set_user_last_active_at_value.rb
+++ db/migrate/20160611232013_set_user_last_active_at_value.rb
@@ -2,7 +2,7 @@ class SetUserLastActiveAtValue < ActiveRecord::Migration[4.2]
   def change
     User.find_each(batch_size: 100) do |user|
       user.update_columns(last_active_at: user.updated_at)
-      User.update_index('stafftools#user') { user }
+      User.update_index('user#user') { user }
     end
   end
 end
