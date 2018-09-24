a98ad2d3008b6624d35136516a2ec5e2cde76ed9
BenEmdon <benemdon@github.com>
2018-09-24 16:54:56 -0400

    Move flipper from user to group_assignment

--- app/controllers/application_controller/feature_flags_dependency.rb
+++ app/controllers/application_controller/feature_flags_dependency.rb
@@ -9,10 +9,6 @@ class ApplicationController
     not_found unless student_identifier_enabled?
   end
 
-  def ensure_group_import_resiliency_enabled
-    not_found unless group_import_resiliency_enabled?
-  end
-
   def student_identifier_enabled?
     logged_in? && current_user.feature_enabled?(:student_identifier)
   end
@@ -27,11 +23,6 @@ class ApplicationController
     logged_in? && current_user.feature_enabled?(:import_resiliency)
   end
   helper_method :import_resiliency_enabled?
-
-  def group_import_resiliency_enabled?
-    logged_in? && current_user.feature_enabled?(:group_import_resiliency)
-  end
-  helper_method :group_import_resiliency_enabled?
 
   def download_repositories_enabled?
     logged_in? && current_user.feature_enabled?(:download_repositories)
