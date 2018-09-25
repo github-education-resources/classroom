b673801048c7d2517509a176af45cfaabd086849
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-24 19:09:06 -0500

    all the renames

--- app/assets/javascripts/desktop-protocol-handler.js
+++ app/assets/javascripts/desktop-protocol-handler.js
@@ -12,7 +12,7 @@
     displayDownloadMessage();
   });
 
-  $("#js-desktop-open-btn").click(function(e)
+  $("#js-assistant-open-btn").click(function(e)
   {
     e.preventDefault();
 
@@ -20,7 +20,7 @@
     var iFrame = $('#js-hidden-iframe')[0];
 
     timeIndex = setTimeout(function() {
-      window.location = "http://www.classroom.github.com/desktop";
+      window.location = "http://www.classroom.github.com/assistant";
     }, 3000);
 
     // attempt to open deep link in iframe to avoid exposing link to user
