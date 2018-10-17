3861e48360a42026ef996a0f77bc4883b53de22e
Nathaniel Woodthorpe <d12@github.com>
2018-10-17 15:28:08 -0700

    Styling

--- app/graphql/types/user.rb
+++ app/graphql/types/user.rb
@@ -19,12 +19,7 @@ class Types
     field :login, String, description: "The user's GitHub login.", null: false
 
     def login
-      GitHubLoader.load_on_relay_node(
-        object.github_global_relay_id,
-        "User",
-        "login",
-        context: context
-      ).then do |results|
+      GitHubLoader.load_on_relay_node(object.github_node_id, "User", "login", context: context).then do |results|
         results.dig("data", "node", "login")
       end
     end
@@ -32,12 +27,7 @@ class Types
     field :github_url, String, description: "The user's GitHub profile URL.", null: true
 
     def github_url
-      GitHubLoader.load_on_relay_node(
-        object.github_global_relay_id,
-        "User",
-        "url",
-        context: context
-      ).then do |results|
+      GitHubLoader.load_on_relay_node(object.github_node_id, "User", "url", context: context).then do |results|
         results.dig("data", "node", "url")
       end
     end
@@ -45,12 +35,7 @@ class Types
     field :avatar_url, String, description: "The user's GitHub avatar URL.", null: true
 
     def avatar_url
-      GitHubLoader.load_on_relay_node(
-        object.github_global_relay_id,
-        "User",
-        "avatarUrl",
-        context: context
-      ).then do |results|
+      GitHubLoader.load_on_relay_node(object.github_node_id, "User", "avatarUrl", context: context).then do |results|
         results.dig("data", "node", "avatar_url")
       end
     end
