fc1fd57b2b6603f1ee7e186cb23866d6bcd48561
BenEmdon <benemdon@github.com>
2018-10-18 16:57:19 -0400

    Order paginated requests

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -27,18 +27,27 @@ class AssignmentsController < ApplicationController
     end
   end
 
+  # rubocop:disable MethodLength
   # rubocop:disable Metrics/AbcSize
   def show
     if @organization.roster
-      @roster_entries = @organization.roster.roster_entries.page(params[:students_page]).order_for_view(@assignment)
+      @roster_entries = @organization.roster.roster_entries
+        .order(:id)
+        .page(params[:students_page])
+        .order_for_view(@assignment)
 
       @unlinked_user_repos = AssignmentRepo
+        .order(:id)
         .where(assignment: @assignment, user: @unlinked_users)
         .page(params[:unlinked_accounts_page])
     else
-      @assignment_repos = AssignmentRepo.where(assignment: @assignment).page(params[:page])
+      @assignment_repos = AssignmentRepo
+        .where(assignment: @assignment)
+        .order(:id)
+        .page(params[:page])
     end
   end
+  # rubocop:enable MethodLength
   # rubocop:enable Metrics/AbcSize
 
   def edit; end
