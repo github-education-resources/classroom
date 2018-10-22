11c706d9902d4d4cac460c9250b154a2b8440c23
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-10-22 00:51:19 -0500

    add comma for redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     GitHub::Forbidden,
     GitHub::NotFound,
     NotAuthorized,
-    Organization::NoValidTokensError with: :flash_and_redirect_back_with_message
+    Organization::NoValidTokensError, with: :flash_and_redirect_back_with_message
   rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound, with: :render_404
 
   rescue_from ActionController::InvalidAuthenticityToken do
