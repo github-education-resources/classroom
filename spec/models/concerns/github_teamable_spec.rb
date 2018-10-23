a91372d62dce497fa03777fbb47182f494dfa3ef
BenEmdon <benemdon@github.com>
2018-10-23 11:45:53 -0400

    Add tests

--- spec/models/concerns/github_teamable_spec.rb
+++ spec/models/concerns/github_teamable_spec.rb
@@ -28,12 +28,14 @@ shared_examples_for "github_teamable" do
   end
 
   describe "#destroy_github_team", :vcr do
-    it "returns true" do
-      model.github_team_id = nil
-      expect(model.destroy_github_team).to be_truthy
+    context "without github_team_id" do
+      it "returns true" do
+        model.github_team_id = nil
+        expect(model.destroy_github_team).to be_truthy
+      end
     end
 
-    context "has a github_team_id" do
+    context "with a github_team_id" do
       before do
        expect_any_instance_of(GitHubOrganization)
         .to receive(:delete_team)
@@ -47,6 +49,49 @@ shared_examples_for "github_teamable" do
 
       it "returns true on success" do
         expect(model.destroy_github_team).to be_truthy
+      end
+    end
+
+    context "raises a GitHub::Error" do
+      before do
+        expect_any_instance_of(GitHubOrganization)
+          .to receive(:delete_team)
+          .and_raise(GitHub::Error)
+      end
+
+      it "doesn't change the github_team_id" do
+        model.destroy_github_team
+        expect(model.github_team_id_changed?).to be_falsy
+      end
+
+      it "returns false on failure" do
+        expect(model.destroy_github_team).to be_falsy
+      end
+    end
+  end
+
+  describe "#silently_destroy_github_team", :vcr do
+    context "when #destroy_github_team returns true" do
+      before do
+        expect(model)
+        .to receive(:destroy_github_team)
+        .and_return(true)
+      end
+
+      it "returns true" do
+        expect(model.silently_destroy_github_team).to be_truthy
+      end
+    end
+
+    context "when #destroy_github_team returns false" do
+      before do
+        expect(model)
+        .to receive(:destroy_github_team)
+        .and_return(false)
+      end
+
+      it "returns true" do
+        expect(model.silently_destroy_github_team).to be_truthy
       end
     end
   end
