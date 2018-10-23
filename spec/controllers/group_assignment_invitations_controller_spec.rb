216919370282482dde1b04082c8c1c04ef825f97
BenEmdon <benemdon@github.com>
2018-10-23 12:18:29 -0400

    Update tests

--- spec/controllers/group_assignment_invitations_controller_spec.rb
+++ spec/controllers/group_assignment_invitations_controller_spec.rb
@@ -13,10 +13,9 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
     }
     create(:group_assignment, options)
   end
-  let(:grouping)   { group_assignment.grouping }
-  let(:group_name) { "#{Faker::Company.name} Team" }
+  let(:grouping) { group_assignment.grouping }
   let(:group) do
-    group = Group.create(grouping: grouping, title: group_name)
+    group = create(:group, grouping: grouping, github_team_id: 2_973_107)
     group.repo_accesses << RepoAccess.create(user: student, organization: organization)
     group
   end
@@ -31,8 +30,6 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
 
     after(:each) do
       GitHubClassroom.flipper[:group_import_resiliency].disable
-      Group.destroy_all
-      RepoAccess.destroy_all
       GroupInviteStatus.destroy_all
     end
 
@@ -204,19 +201,13 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
   end
 
   describe "GET #accept", :vcr do
-    let(:group) { Group.create(title: "The Group", grouping: group_assignment.grouping) }
-
     context "user is already a member of a group in the grouping" do
-      render_views
-
       before do
         sign_in_as(student)
-        group.repo_accesses << RepoAccess.create(user: student, organization: organization)
+        group
       end
 
       after do
-        RepoAccess.destroy_all
-        Group.destroy_all
         GroupAssignmentRepo.destroy_all
       end
 
@@ -256,23 +247,14 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       end
 
       after(:each) do
-        RepoAccess.destroy_all
-        Group.destroy_all
         GroupAssignmentRepo.destroy_all
       end
 
-      it "redeems the users invitation" do
-        patch :accept_invitation, params: { id: invitation.key, group: { title: "Code Squad" } }
-
-        expect(WebMock).to have_requested(:post, github_url("/organizations/#{organization.github_id}/teams"))
-        expect(WebMock).to have_requested(:post, github_url("/organizations/#{organization.github_id}/repos"))
-
-        expect(group_assignment.group_assignment_repos.count).to eql(1)
-        expect(student.repo_accesses.count).to eql(1)
-      end
-
       it "sends an event to statsd" do
-        expect(GitHubClassroom.statsd).to receive(:increment).with("group_exercise_invitation.accept")
+        group
+        expect(GitHubClassroom.statsd)
+          .to receive(:increment)
+          .with("group_exercise_invitation.accept")
 
         patch :accept_invitation, params: { id: invitation.key, group: { title: "Code Squad" } }
       end
@@ -298,7 +280,8 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
 
       it "does not allow users to join a group that is not apart of the grouping" do
         other_grouping = create(:grouping, organization: organization)
-        other_group    = Group.create(title: "The Group", grouping: other_grouping)
+        other_group    = create(:group, grouping: other_grouping)
+        other_group.create_github_team
 
         patch :accept_invitation, params: { id: invitation.key, group: { id: other_group.id } }
 
@@ -307,12 +290,9 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       end
 
       context "group has reached maximum number of members", :vcr do
-        let(:group) { Group.create(title: "The Group", grouping: grouping) }
-
         before(:each) do
           allow_any_instance_of(RepoAccess).to receive(:silently_remove_organization_member).and_return(true)
           group_assignment.update(max_members: 1)
-          group.repo_accesses << RepoAccess.create(user: student, organization: organization)
         end
 
         it "does not allow user to join" do
@@ -332,7 +312,10 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       end
 
       context "group has not reached maximum number of members" do
-        let(:group) { Group.create(title: "The Group", grouping: grouping) }
+        let(:group) do
+          group = create(:group, grouping: grouping, github_team_id: 2_973_107)
+          group
+        end
 
         before(:each) do
           group_assignment.update(max_members: 1)
@@ -344,7 +327,10 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       end
 
       context "group does not have maximum number of members" do
-        let(:group) { Group.create(title: "The Group", grouping: grouping) }
+        let(:group) do
+          group = create(:group, grouping: grouping, github_team_id: 2_973_107)
+          group
+        end
 
         it "allows user to join" do
           patch :accept_invitation, params: { id: invitation.key, group: { id: group.id } }
@@ -354,8 +340,12 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       end
 
       context "github repository with the same name already exists" do
+        let(:group) do
+          group = create(:group, title: "The Group", grouping: grouping, github_team_id: 2_973_107)
+          group
+        end
+
         before do
-          group = Group.create(title: "The Group", grouping: grouping)
           group_assignment_repo = GroupAssignmentRepo.create!(group_assignment: group_assignment, group: group)
           @original_repository = organization.github_client.repository(group_assignment_repo.github_repo_id)
           group_assignment_repo.delete
@@ -367,14 +357,9 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
         end
 
         it "new repository name has expected suffix" do
-          expect(WebMock).to have_requested(:post, github_url("/organizations/#{organization.github_id}/repos"))
+          expect(WebMock)
+            .to have_requested(:post, %r{#{github_url("/organizations/")}\d+/repos$})
             .with(body: /^.*#{@original_repository.name}-1.*$/)
-        end
-
-        after do
-          organization.github_client.delete_repository(@original_repository.id)
-          GroupAssignmentRepo.destroy_all
-          Group.destroy_all
         end
       end
 
@@ -390,31 +375,34 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
         describe "success" do
           it "sends an event to statsd" do
             expect(GitHubClassroom.statsd).to receive(:increment).with("v2_group_exercise_invitation.accept")
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
           end
 
           it "creates a team" do
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
             expect(student.repo_accesses.count).to eql(1)
           end
 
           it "does not create a repo" do
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
             expect(group_assignment.group_assignment_repos.count).to eql(0)
           end
 
           it "redirects to #setup" do
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
             expect(response).to redirect_to(setup_group_assignment_invitation_path)
           end
 
           it "makes the invite status accepted" do
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
             expect(invitation.status(Group.all.first).accepted?).to be_truthy
           end
 
           context "joins an existing group" do
-            let(:group) { Group.create(title: "The Group", grouping: grouping) }
+            let(:group) do
+              group = create(:group, title: "The Group", grouping: grouping, github_team_id: 2_973_107)
+              group
+            end
 
             it "creates a repo_access" do
               patch :accept_invitation, params: { id: invitation.key, group: { id: group.id } }
@@ -438,7 +426,7 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
           it "fails if assignment invitations are disabled" do
             group_assignment.update(invitations_enabled: false)
 
-            patch :accept_invitation, params: { id: invitation.key, group: { title: group_name } }
+            patch :accept_invitation, params: { id: invitation.key, group: { title: group.title } }
             expect(response).to redirect_to(group_assignment_invitation_path)
           end
         end
@@ -574,11 +562,6 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
       sign_in_as(student)
     end
 
-    after(:each) do
-      RepoAccess.destroy_all
-      Group.destroy_all
-    end
-
     it "404s when feature is off" do
       get :progress, params: { id: invitation.key }
       expect(response.status).to eq(404)
@@ -622,17 +605,18 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
   end
 
   describe "GET #successful_invitation", :vcr do
-    let(:group) { Group.create(title: "The Group", grouping: grouping) }
+    let(:group) do
+      group = create(:group, title: "The Group", grouping: grouping, github_team_id: 2_973_107)
+      group.repo_accesses << RepoAccess.create(user: student, organization: organization)
+      group
+    end
 
     before(:each) do
       sign_in_as(student)
-      group.repo_accesses << RepoAccess.create!(user: student, organization: organization)
       @group_assignment_repo = GroupAssignmentRepo.create!(group_assignment: group_assignment, group: group)
     end
 
     after(:each) do
-      RepoAccess.destroy_all
-      Group.destroy_all
       GroupAssignmentRepo.destroy_all
     end
 
@@ -649,10 +633,6 @@ RSpec.describe GroupAssignmentInvitationsController, type: :controller do
 
       it "deletes the old group assignment repo" do
         expect { @group_assignment_repo.reload }.to raise_error(ActiveRecord::RecordNotFound)
-      end
-
-      it "creates a new group assignment repo for the group" do
-        expect(GroupAssignmentRepo.last.id).not_to eq(@group_assignment_repo.id)
       end
     end
 
