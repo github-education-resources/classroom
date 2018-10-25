410a6e2cb04f03c3335c7e939fa5401d0c417f33
BenEmdon <benemdon@github.com>
2018-10-25 19:17:29 -0400

    Start writing tests

--- spec/models/group/creator_spec.rb
+++ spec/models/group/creator_spec.rb
@@ -3,6 +3,28 @@
 require "rails_helper"
 
 RSpec.describe Group::Creator, type: :model do
+  let(:title)    { Faker::Team.name[0..39] }
   let(:grouping) { create(:grouping) }
-  let(:title)    { "#{Faker::Company.name} Team" }
+  let(:group)    { create(:group, title: title, grouping: grouping, github_team_id: 2_977_000) }
+
+  describe "class#perform" do
+    subject { described_class.perform(title: title, grouping: grouping) }
+
+    it "calls intsance method #perform" do
+      expect_any_instance_of(described_class)
+        .to receive(:perform)
+      subject
+    end
+  end
+
+  describe "#perform", :vcr do
+    subject { described_class.new(title: title, grouping: grouping).perform }
+
+    it "creates a group" do
+      expect(Group)
+        .to receive(:new)
+        .and_return(group)
+      subject
+    end
+  end
 end
