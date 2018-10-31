edfecf47dd3ef8cb928f40fb0872614d2a3a74cd
BenEmdon <benemdon@github.com>
2018-10-31 13:44:07 -0400

    More tests

--- spec/lib/no_emoji_validator_spec.rb
+++ spec/lib/no_emoji_validator_spec.rb
@@ -10,16 +10,30 @@ class TestModel
 end
 
 RSpec.describe NoEmojiValidator do
-  it "ignores nil values" do
-    test_object = TestModel.new(title: nil)
-    expect(test_object.valid?).to eq(true)
+  context "title is nil" do
+    let(:test_object) { TestModel.new(title: nil) }
+
+    it "passes validation" do
+      expect(test_object.valid?).to eq(true)
+    end
   end
 
-  it "validates fields that contain emojis" do
-    test_object = TestModel.new(title: "😃")
-    expect(test_object.valid?).to eq(false)
-    expect(test_object.errors.count).to eq(1)
-    expect(test_object.errors.details[:title])
-      .to eq([{ error: "title is not allowed to have emojis." }])
+  context "title has no emojis" do
+    let(:test_object) { TestModel.new(title: ":sparkles:") }
+
+    it "passes validation" do
+      expect(test_object.valid?).to eq(true)
+    end
+  end
+
+  context "title has an emoji" do
+    let(:test_object) { TestModel.new(title: "✨") }
+
+    it "fails validation" do
+      expect(test_object.valid?).to eq(false)
+      expect(test_object.errors.count).to eq(1)
+      expect(test_object.errors.details[:title])
+        .to eq([{ error: "title is not allowed to have emojis." }])
+    end
   end
 end
