3a7f94553288eaf34bf3fdaf9c8cb79acbc7a0e7
BenEmdon <benemdon@github.com>
2018-11-12 17:55:25 -0500

    Slight optimization by reducing writes to the database

--- app/models/organization/creator.rb
+++ app/models/organization/creator.rb
@@ -66,17 +66,18 @@ class Organization
       begin
         github_organization = GitHubOrganization.new(users.first.github_client, github_id)
 
-        org_webhook = OrganizationWebhook.find_or_create_by(github_organization_id: github_id)
+        organization_webhook = OrganizationWebhook.find_or_initialize_by(github_organization_id: github_id)
         webhook_id = create_organization_webhook!
 
-        org_webhook.update!(github_id: webhook_id)
+        organization_webhook.github_id = webhook_id
+        organization_webhook.save!
         organization.update_attributes!(
           github_id: github_id,
           title: title,
           users: users,
           webhook_id: webhook_id,
           github_global_relay_id: github_organization.node_id,
-          organization_webhook: org_webhook
+          organization_webhook: organization_webhook
         )
       rescue ActiveRecord::RecordInvalid => err
         raise Result::Error, err.message
