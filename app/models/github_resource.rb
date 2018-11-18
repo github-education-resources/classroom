eb5546c86916232372584ca1a22aed5f68207550
BenEmdon <benemdon@github.com>
2018-11-18 16:21:59 -0500

    Cleaned up docs

--- app/models/github_resource.rb
+++ app/models/github_resource.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# A parent class so that we always have
-# the `id`, `client`, `access_token`, and an optional `org_id`
-# attr_readers.
+# A parent class so that we always have the `id`, `client`, `access_token`,
+# and an optional `org_id` attr_readers.
 
 class GitHubResource < GitHubModel
   attr_reader :id, :client, :access_token, :org_id
