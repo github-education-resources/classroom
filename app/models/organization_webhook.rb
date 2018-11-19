9c4fd3e9c5e0e4761a5918e1109a8f1d6a3772ee
BenEmdon <benemdon@github.com>
2018-11-19 15:02:41 -0500

    Added users_with_admin_org_hook_scope and admin_org_hook_scope_github_client

--- app/models/organization_webhook.rb
+++ app/models/organization_webhook.rb
@@ -9,7 +9,48 @@ class OrganizationWebhook < ApplicationRecord
   validates :github_organization_id, presence:   true
   validates :github_organization_id, uniqueness: true
 
-  def github_organization
-    @github_organization ||= organizations&.first&.github_organization
+  def admin_org_hook_scope_github_client
+    if Rails.env.test?
+      token = users.first.token unless users.first.nil?
+    else
+      token = user_with_admin_org_hook_scope.sample
+    end
+    Octokit::Client.new(access_token: token)
+  end
+
+  private
+
+  # Internal: Find Users that has the `admin:org_hook` scope
+  # for creating the organization webhook.
+  #
+  # Example:
+  #
+  #   users_with_admin_org_hook_scope
+  #   # => [#<User:0x007fd7f0320a30
+  #      id: 1,
+  #      uid: 564113,
+  #      token: "8675309",
+  #      created_at: Sun, 20 Nov 2016 16:29:43 UTC +00:00,
+  #      updated_at: Tue, 22 Nov 2016 03:46:10 UTC +00:00,
+  #      site_admin: true,
+  #      last_active_at: Fri, 25 Nov 2016 03:39:54 UTC +00:00>]
+  #
+  # Returns a User or nil if one could not be found.
+  #
+  # Warning: This could potentially take very long for organizations
+  # of a large size, so invoke cautiously.
+  def users_with_admin_org_hook_scope
+    return @users_with_scope if defined?(@users_with_scope)
+
+    @users_with_scope = []
+
+    users.find_in_batches(batch_size: 100) do |users|
+      users.each do |user|
+        next unless user.github_client_scopes.include?("admin:org_hook")
+        @users_with_scope << user
+      end
+    end
+
+    @users_with_scope
   end
 end
