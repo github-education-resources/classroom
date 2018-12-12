67a89b23d7a114df24ebab485332c8f785ca896f
BenEmdon <benemdon@github.com>
2018-12-12 15:19:28 -0500

    Added OrganizationWebhookHealthService

---
+++ app/services/organization_webhook_health_service.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+class OrganizationWebhookHealthService
+  class Result
+    def self.success
+      new(:success)
+    end
+
+    def self.failed(error)
+      new(:failed, error: error)
+    end
+
+    attr_reader :error
+
+    def initialize(status, error: nil)
+      @status = status
+      @error  = error
+    end
+
+    def success?
+      @status == :success
+    end
+
+    def failed?
+      @status == :failed
+    end
+  end
+
+  def initialize(all_organizations: false)
+    @all_organizations = all_organizations
+  end
+
+  # Public: Iterates through OrganizationWebhook records and ensures
+  # that each record has an active and working webhook.
+  #
+  # all_organizations - Flag that specifies whether to iterate all OrganizationWebhooks,
+  #                     or just the ones with a blank github_id.
+  #
+  # Returns a result object with the schema:
+  #  {
+  #    success: #{array of ensured IDs},
+  #    failed: {
+  #      "#{error_class_name}": #{array of failed IDs}
+  #    }
+  #  }
+  def self.perform(all_organizations: false)
+    new(all_organizations: all_organizations).perform
+  end
+
+  # Public: Iterates through OrganizationWebhook records and ensures
+  # that each record has an active and working webhook. This method also
+  # prints the results in a human readable way.
+  #
+  # all_organizations - Flag that specifies whether to iterate all OrganizationWebhooks,
+  #                     or just the ones with a blank github_id.
+  #
+  # Returns a result object with the schema:
+  #  {
+  #    success: #{array of ensured IDs},
+  #    failed: {
+  #      "#{error_class_name}": #{array of failed IDs}
+  #    }
+  #  }
+  def self.perform_and_print(all_organizations: false)
+    results = perform(all_organizations: false)
+    print_results(results)
+    results
+  end
+
+  # Public: Iterates through OrganizationWebhook records and ensures
+  # that each record has an active and working webhook.
+  #
+  # Returns a result object with the schema:
+  #  {
+  #    success: #{array of ensured IDs},
+  #    failed: {
+  #      "#{error_class_name}": #{array of failed IDs}
+  #    }
+  #  }
+  def perform
+    success_org_webhooks = []
+    failed_org_webhooks = {}
+    (@all_organizations ? OrganizationWebhook : OrganizationWebhook.where(github_id: nil))
+      .find_in_batches(batch_size: 100) do |organization_webhooks|
+        organization_webhooks.each do |organization_webhook|
+          result = ensure_organization_webhook_exists!(organization_webhook)
+          if result.success?
+            success_org_webhooks << organization_webhook.id
+          else
+            failed_org_webhooks[result.error.class.to_s] ||= []
+            failed_org_webhooks[result.error.class.to_s] << organization_webhook.id
+          end
+        end
+      end
+
+    {
+      success: success_org_webhooks,
+      failed: failed_org_webhooks
+    }
+  end
+
+  private
+
+  # Internal: Creates a OrganizationWebhook if one does not exist yet,
+  # then creates an GitHubOrgHook if one does not exist, or activates the GitHubOrgHook if it is inactive.
+  #
+  # organization_webhook - an OrganizationWebhook to call #ensure_webhook_is_active! on.
+  #
+  # Returns a Result
+  def ensure_organization_webhook_exists!(organization_webhook)
+    organization_webhook.ensure_webhook_is_active!
+    Result.success
+  rescue ActiveRecord::RecordInvalid, GitHub::Error, OrganizationWebhook::NoValidTokenError => error
+    Result.failed(error)
+  end
+
+  # Internal: Prints results from #perform in a human readable way.
+  #
+  # results - the result object from #perform with the schema:
+  #           {
+  #             success: #{array of ensured IDs},
+  #             failed: {
+  #               "#{error_class_name}": #{array of failed IDs}
+  #             }
+  #           }
+  #
+  # Returns nothing.
+  private_class_method def self.print_results(results)
+    puts "\n"
+    puts "Organization Webhook Health Service Results"
+    puts "==========================================="
+    puts "Success count: #{results[success].count}"
+    puts "Errored organzations:"
+    pp results[failed]
+    puts "\n"
+  end
+end
