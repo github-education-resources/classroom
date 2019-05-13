54200c6acca109c4cfae17b518d76aca981f6bfc
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-05-13 12:22:21 -0500

    pull google classroom client id from secrets

--- config/initializers/google_classroom_client.rb
+++ config/initializers/google_classroom_client.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
-require 'googleauth'
-require 'googleauth/stores/redis_token_store'
-require 'google/api_client/client_secrets'
+require "googleauth"
+require "googleauth/stores/redis_token_store"
+require "google/api_client/client_secrets"
 
 module GitHubClassroom
-  def self.google_classroom_client(options = {})
-    scope = [Google::Apis::ClassroomV1::AUTH_CLASSROOM_COURSES_READONLY, Google::Apis::ClassroomV1::AUTH_CLASSROOM_ROSTERS_READONLY]
-    client_id = Google::Auth::ClientId.new("421659922438-rri2uuv71jj3aaeh2bkfj3906npmv5n2.apps.googleusercontent.com", "toXhuU9fg7-d2K45Xb_j3Jk5")
-    token_store = Google::Auth::Stores::RedisTokenStore.new(redis: GitHubClassroom.redis)
+  def self.google_classroom_client
+    scope = [
+      Google::Apis::ClassroomV1::AUTH_CLASSROOM_COURSES_READONLY,
+      Google::Apis::ClassroomV1::AUTH_CLASSROOM_ROSTERS_READONLY
+    ]
 
-    Google::Auth::WebUserAuthorizer.new(client_id, scope, token_store, '/google_classroom/oauth2_callback')
+    client_id = Rails.application.secrets.google_client_id
+    client_secret = Rails.application.secrets.google_client_secret
+    client = Google::Auth::ClientId.new(client_id, client_secret)
+
+    token_store = Google::Auth::Stores::RedisTokenStore.new(redis: GitHubClassroom.redis)
+    Google::Auth::WebUserAuthorizer.new(client, scope, token_store, "/google_classroom/oauth2_callback")
   end
 end
