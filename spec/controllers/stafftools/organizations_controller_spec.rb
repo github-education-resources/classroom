7a7e3b6e5d332a2c421476b49aab80f71d3b4d0a
BenEmdon <benemdon@github.com>
2019-06-04 11:44:26 -0400

    Update tests

--- spec/controllers/stafftools/organizations_controller_spec.rb
+++ spec/controllers/stafftools/organizations_controller_spec.rb
@@ -76,9 +76,9 @@ RSpec.describe Stafftools::OrganizationsController, type: :controller do
         end
       end
 
-      context "ensure_webhook_is_active returns false" do
+      context "ensure_webhook_is_active raises an error" do
         before do
-          expect_any_instance_of(OrganizationWebhook).to receive(:ensure_webhook_is_active!).and_return(false)
+          expect_any_instance_of(OrganizationWebhook).to receive(:ensure_webhook_is_active!).and_raise(OrganizationWebhook::NoValidTokenError)
           post :ensure_webhook_is_active, params: { id: organization.id }
         end
 
