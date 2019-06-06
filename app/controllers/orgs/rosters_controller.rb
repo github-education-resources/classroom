2d381fa4a5c224321dc6c1b0274bdb0f6907e49e
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-06-06 11:26:26 +0530

    move default identifier name downstream and address PR comments

--- app/controllers/orgs/rosters_controller.rb
+++ app/controllers/orgs/rosters_controller.rb
@@ -59,7 +59,6 @@ module Orgs
     def create
       result = Roster::Creator.perform(
         organization: current_organization,
-        identifier_name: "Identifiers",
         identifiers: params[:identifiers],
         google_user_ids: params[:google_user_ids]
       )
@@ -100,7 +99,7 @@ module Orgs
       user_id = params[:user_id].to_i
       raise ActiveRecord::ActiveRecordError unless unlinked_user_ids.include?(user_id)
 
-      current_roster_entry.update_attributes!(user_id: user_id)
+      current_roster_entry.update_attributes(user_id: user_id)
 
       flash[:success] = "Student and GitHub account linked!"
     rescue ActiveRecord::ActiveRecordError
@@ -200,7 +199,7 @@ module Orgs
         flash[:warning] = "No students were found in your Google Classroom. Please add students and try again."
         redirect_to roster_path(current_organization)
       else
-        current_organization.update_attributes!(google_course_id: params[:course_id])
+        current_organization.update_attributes(google_course_id: params[:course_id])
         add_google_classroom_students(students)
       end
     end
