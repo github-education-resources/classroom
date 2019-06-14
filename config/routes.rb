b481922c69e980e361436e4841a35182ca2b7d2f
Nathaniel Woodthorpe <d12@github.com>
2019-06-14 15:08:44 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -96,7 +96,7 @@ Rails.application.routes.draw do
         resources :assignment_repos, only: [:show], controller: "orgs/assignment_repos"
         get "/roster_entries/:roster_entry_id", to: "orgs/roster_entries#show", as: "roster_entry"
         get :assistant, on: :member
-        collection do
+        member do
           get :search
         end
       end
