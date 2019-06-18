f9e4af38e57b2ccbd24e162aca888ad84af1423e
Shaunak Pagnis <shaunakpp@github.com>
2019-06-18 16:05:11 -0700

    remove dependencies from group assignment repo, as they are now handled by creator

--- app/models/group_assignment_repo.rb
+++ app/models/group_assignment_repo.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class GroupAssignmentRepo < ApplicationRecord
-  include GitHubPlan
   include GitHubRepoable
-  include Nameable
   include AssignmentRepoable
 
   update_index("group_assignment_repo#group_assignment_repo") { self }
@@ -23,16 +21,6 @@ class GroupAssignmentRepo < ApplicationRecord
   validates :group, presence: true
   validates :group, uniqueness: { scope: :group_assignment }
 
-  before_validation(on: :create) do
-    if organization
-      create_github_repository
-      delete_github_repository_on_failure do
-        push_starter_code
-        add_team_to_github_repository
-      end
-    end
-  end
-
   before_destroy :silently_destroy_github_repository
 
   delegate :creator, :starter_code_repo_id, to: :group_assignment
@@ -43,15 +31,7 @@ class GroupAssignmentRepo < ApplicationRecord
     @github_team ||= group.github_team
   end
 
-  def repo_name
-    @repo_name ||= generate_github_repo_name
-  end
-
   private
 
   delegate :slug, to: :group_assignment
-
-  def name
-    @name ||= group.github_team.slug_no_cache
-  end
 end
