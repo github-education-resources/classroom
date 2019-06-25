3e76d7aeb409ec13001f4a53c73e70b2d3d71a99
stephaniegiang <stephaniegiang@github.com>
2019-06-25 16:51:32 -0700

    Refactor GroupAssignmentRepo sorting tests

--- spec/models/group_assignment_repo_spec.rb
+++ spec/models/group_assignment_repo_spec.rb
@@ -67,32 +67,22 @@ RSpec.describe GroupAssignmentRepo, type: :model do
       end
     end
 
-    describe "is sortable" do
-      before(:each) do
-        github_team_id_two = organization.github_organization.create_team(Faker::Team.name[0..39]).id
-        @group_two = create(:group, grouping: grouping, github_team_id: github_team_id_two)
-        @group_two.repo_accesses << repo_access
+    describe "is sortable", :vcr do
+      let(:github_team_id_two) { organization.github_organization.create_team(Faker::Team.name[0..39]).id }
+      let(:group_two) { create(:group, grouping: grouping, github_team_id: github_team_id_two, repo_accesses: [repo_access]) }
 
-        @group_assignment_repo_one = GroupAssignmentRepo.create(group_assignment: group_assignment, group: @group)
-        @group_assignment_repo_two = GroupAssignmentRepo.create(group_assignment: group_assignment, group: @group_two)
-      end
-
-      after(:each) do
-        @group_assignment_repo_one.destroy if @group_assignment_repo_one.present?
-        @group_assignment_repo_two.destroy if @group_assignment_repo_two.present?
-        organization.github_organization.delete_team(@group_two.github_team_id)
-        organization.github_organization.delete_team(@group.github_team_id)
-      end
+      let(:group_assignment_repo_one) { create(:group_assignment_repo, group_assignment: group_assignment, group: group, github_repo_id: 1) }
+      let(:group_assignment_repo_two) { create(:group_assignment_repo, group_assignment: group_assignment, group: group_two, github_repo_id: 2) }
 
       it "order_by_sort_mode sorts by 'Team name'" do
-        expected_ordering = [@group_assignment_repo_one, @group_assignment_repo_two].sort_by { |repo| repo.group.title }
+        expected_ordering = [group_assignment_repo_one, group_assignment_repo_two].sort_by { |repo| repo.group.title }
         actual_ordering = GroupAssignmentRepo.where(group_assignment: group_assignment).order_by_sort_mode("Team name")
 
         expect(actual_ordering).to eq(expected_ordering)
       end
 
       it "order_by_sort_mode sorts by 'Created at'" do
-        expected_ordering = [@group_assignment_repo_one, @group_assignment_repo_two].sort_by(&:created_at)
+        expected_ordering = [group_assignment_repo_one, group_assignment_repo_two].sort_by(&:created_at)
         actual_ordering = GroupAssignmentRepo.where(group_assignment: group_assignment).order_by_sort_mode("Created at")
 
         expect(actual_ordering).to eq(expected_ordering)
