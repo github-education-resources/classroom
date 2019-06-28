2db2afb50ebb3e6efd36c311e111242effd29ec1
Nathaniel Woodthorpe <d12@github.com>
2019-06-28 18:19:24 -0400

    AWIUDG

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -3,16 +3,6 @@
 require "typhoeus/adapters/faraday"
 
 Octokit.middleware = Faraday::RackBuilder.new do |builder|
-  options = {}.tap do |opts|
-    opts[:store]        = Rails.cache
-    opts[:shared_cache] = false
-    opts[:serializer]   = Marshal
-
-    opts[:logger] = Rails.logger unless Rails.env.production?
-  end
-
-  builder.use :http_cache, options
-
   builder.use Octokit::Middleware::FollowRedirects
   builder.use Octokit::Response::RaiseError
 
