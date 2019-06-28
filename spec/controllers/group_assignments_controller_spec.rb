26b981c862c2e284b9abb8448a9d25cc7ed27801
stephaniegiang <stephaniegiang@github.com>
2019-06-28 09:24:14 -0700

    Rubocop

--- spec/controllers/group_assignments_controller_spec.rb
+++ spec/controllers/group_assignments_controller_spec.rb
@@ -171,7 +171,6 @@ RSpec.describe GroupAssignmentsController, type: :controller do
       get :show, xhr: true, params: {
         organization_id: organization.slug, id: group_assignment.slug, query: "TEST"
       }
-
     end
   end
 
