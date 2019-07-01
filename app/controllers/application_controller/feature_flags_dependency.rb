7d6d346da3bfb30ea9e1b51a8f92f7743c98f441
stephaniegiang <stephaniegiang@github.com>
2019-07-01 10:34:57 -0700

    Add feature flag for LTI roster integration

--- app/controllers/application_controller/feature_flags_dependency.rb
+++ app/controllers/application_controller/feature_flags_dependency.rb
@@ -63,4 +63,9 @@ class ApplicationController
     logged_in? && current_user.feature_enabled?(:search_assignments)
   end
   helper_method :search_assignments_enabled?
+
+  def lti_roster_enabled?
+    logged_in? && current_user.feature_enabled?(:lti_roster)
+  end
+  helper_method :search_assignments_enabled?
 end
