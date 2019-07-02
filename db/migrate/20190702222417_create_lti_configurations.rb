93a83ed49cedb0794415f34fe44400b8d3fe1efe
stephaniegiang <stephaniegiang@github.com>
2019-07-02 15:57:17 -0700

    Add schema and make columns not null

--- db/migrate/20190702222417_create_lti_configurations.rb
+++ db/migrate/20190702222417_create_lti_configurations.rb
@@ -1,9 +1,9 @@
 class CreateLtiConfigurations < ActiveRecord::Migration[5.1]
   def change
     create_table :lti_configurations do |t|
-      t.text :consumer_key
-      t.text :shared_secret
-      t.text :lms_link
+      t.text :consumer_key, null: false
+      t.text :shared_secret, null: false
+      t.text :lms_link, null: false
       t.belongs_to :organization, index: true
 
       t.timestamps
