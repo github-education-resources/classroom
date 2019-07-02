e39ea840d529a349e0ca75cea12eccac9a2085a7
Stephanie Giang <stephaniegiang@users.noreply.github.com>
2019-07-02 16:28:02 -0700

    Change if statements
    Co-Authored-By: Nathaniel Woodthorpe <d12@github.com>

--- app/controllers/orgs/rosters_controller.rb
+++ app/controllers/orgs/rosters_controller.rb
@@ -314,7 +314,7 @@ module Orgs
 
     # Returns name of the linked google course to current organization (for syncing rosters)
     def current_organization_google_course_name
-      return nil if current_organization.google_course_id.nil?
+     return unless current_organization.google_course_id
       authorize_google_classroom
       course = @google_classroom_service.get_course(current_organization.google_course_id)
       return course.name unless course.nil?
