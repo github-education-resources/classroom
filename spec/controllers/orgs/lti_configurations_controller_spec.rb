f5be7d930a0f71edcb19453cc4c96e9e6da10b21
Bradley Bain <thebradbain@github.com>
2019-07-08 15:42:32 -0700

    Update tests

--- spec/controllers/orgs/lti_configurations_controller_spec.rb
+++ spec/controllers/orgs/lti_configurations_controller_spec.rb
@@ -8,36 +8,66 @@ RSpec.describe Orgs::LtiConfigurationsController, type: :controller do
 
   before(:each) do
     sign_in_as(user)
-    GitHubClassroom.flipper[:lti_launch].enable
   end
 
-  describe "GET #new" do
-    it "returns success status" do
-      get :new, params: { organization: organization }
-      expect(response).to have_http_status(:success)
+  describe "GET #new", :vcr do
+    context "with flipper disabled" do
+      before(:each) do
+        GitHubClassroom.flipper[:lti_launch].disable
+      end
+
+      it "returns not_found" do
+        get :new, params: { id: organization.slug }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context "with flipper enabled" do
+      before(:each) do
+        GitHubClassroom.flipper[:lti_launch].enable
+      end
+
+      it "returns success status" do
+        get :new, params: { id: organization.slug }
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns success status" do
-      get :show, params: { organization: organization }
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET #show", :vcr do
+    context "with flipper disabled" do
+      before(:each) do
+        GitHubClassroom.flipper[:lti_launch].disable
+      end
 
-  describe "flag is turned off" do
-    before(:each) do
-      GitHubClassroom.flipper[:lti_launch].disable
-    end
-
-    it "returns not found for #new" do
-      get :new, params: { organization: organization }
-      expect(response).to have_http_status(:not_found)
+      it "returns not_found" do
+        get :show, params: { id: organization.slug }
+        expect(response).to have_http_status(:not_found)
+      end
     end
 
-    it "returns not found for #show" do
-      get :show, params: { organization: organization }
-      expect(response).to have_http_status(:not_found)
+    context "with flipper enabled" do
+      before(:each) do
+        GitHubClassroom.flipper[:lti_launch].enable
+      end
+
+      context "with lti_configuration present" do
+        before(:each) do
+          create(:lti_configuration, organization: organization)
+        end
+
+        it "returns success status" do
+          get :show, params: { id: organization.slug }
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "with no existing lti_configuration" do
+        it "redirects to new" do
+          get :show, params: { id: organization.slug }
+          expect(response).to redirect_to(new_lti_configuration_path(organization))
+        end
+      end
     end
   end
 end
