a66990a8e05b1f7d86c9b44c203fa84f695ce7b2
Bradley Bain <thebradbain@github.com>
2019-07-08 12:07:18 -0700

    Ensure current lti configuration on controller action

--- app/controllers/orgs/lti_configurations_controller.rb
+++ app/controllers/orgs/lti_configurations_controller.rb
@@ -3,9 +3,22 @@
 module Orgs
   class LtiConfigurationsController < Orgs::Controller
     before_action :ensure_lti_launch_flipper_is_enabled
+    before_action :ensure_current_lti_configuration,             except: %i[new create]
 
-    def link_lms_classroom; end
+    def create
+      # Create a new lti configuration for the current organization here
+      # redirect_to lti_configuration_path(current_organization) after
+    end
 
-    def lms_configuration; end
+    private
+
+    def current_lti_configuration
+      @current_lti_configuration ||= LtiConfiguration.find_by(organization: current_organization)
+      # @current_lti_configuration ||= current_organization.lti_configuration
+    end
+
+    def ensure_current_lti_configuration
+      redirect_to new_lti_configuration_path(current_organization) if current_lti_configuration.nil?
+    end
   end
 end
