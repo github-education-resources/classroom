c5ff8c35700d923ccc0f29f153bc026f509a79ff
Nathaniel Woodthorpe <d12@github.com>
2019-07-08 14:28:33 -0400

    Add specs

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -8,20 +8,69 @@ describe GitHubUser do
     @client = oauth_client
   end
 
-  let(:github_user)       { GitHubUser.new(@client, @client.user.id, classroom_resource: classroom_teacher) }
+  let(:user)              { classroom_teacher }
+  let(:github_user)       { GitHubUser.new(@client, @client.user.id, classroom_resource: user) }
   let(:other_user)        { classroom_student }
   let(:other_github_user) { GitHubUser.new(@client, other_user.uid, classroom_resource: other_user) }
 
-  it "responds to all (GitHub) attributes", :vcr do
-    gh_user = github_user.client.user(github_user.id)
+  it "responds to all locally cached GitHub attributes", :vcr do
+    attributes = github_user.send(:local_cached_attributes)
 
-    github_user.attributes.each do |attribute, value|
-      next if %i[id_attributes client access_token].include?(attribute)
-      expect(gh_user).to respond_to(attribute)
-      expect(value).to eql(gh_user.send(attribute))
+    attributes.each do |attribute|
+      expect(github_user).to respond_to(attribute)
     end
 
-    expect(WebMock).to have_requested(:get, github_url("/user/#{gh_user.id}")).once
+    expect(WebMock).not_to have_requested(:get, github_url("/user/#{github_user.id}"))
+  end
+
+  context "locally cached attributes", :vcr do
+    context "when use_cache is true" do
+      context "when cache is not warm" do
+        before(:each) do
+          user.update_attributes(github_login: nil, github_name: nil)
+        end
+
+        it "makes an API request" do
+          expect(github_user.login).to_not be_nil
+          expect(WebMock).to have_requested(:get, github_url("/user/#{github_user.id}")).once
+        end
+
+        it "populates the cache" do
+          expect(user.github_login).to be_nil
+
+          github_user.login
+
+          expect(user.github_login).to_not be_nil
+        end
+
+        it "also populates other cached fields" do
+          expect(user.github_name).to be_nil
+
+          github_user.login
+
+          expect(user.github_name).to_not be_nil
+        end
+      end
+
+      context "when cache is warm" do
+        before do
+          user.update_attribute(:github_login, "login")
+        end
+
+        it "does not make an API request and returns the cached value" do
+          expect(github_user.login).to eq("login")
+          expect(WebMock).to_not have_requested(:get, github_url("/user/#{github_user.id}"))
+        end
+      end
+    end
+
+    context "when use_cache is false" do
+      it "makes an API request, even when cache is populated" do
+        user.update_attribute(:github_login, "login")
+        expect(github_user.login(use_cache: false)).to_not eq("login")
+        expect(WebMock).to have_requested(:get, github_url("/user/#{github_user.id}"))
+      end
+    end
   end
 
   it "responds to all *_no_cache methods", :vcr do
