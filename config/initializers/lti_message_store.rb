faff9de61c0ccfe39c0100bc2d1f0c90afe269bc
Bradley Bain <thebradbain@github.com>
2019-07-12 09:54:35 -0700

    Allow lti_configuration to initialize lti_message_store

--- config/initializers/lti_message_store.rb
+++ config/initializers/lti_message_store.rb
@@ -2,6 +2,11 @@
 
 module GitHubClassroom
   def self.lti_message_store(options = {})
+    if options[:lti_configuration].present?
+      options[:consumer_key] = options[:lti_configuration].consumer_key
+      options.delete(:lti_configuration)
+    end
+
     client_options = {
       consumer_key:  nil,
       redis_store: GitHubClassroom.redis
