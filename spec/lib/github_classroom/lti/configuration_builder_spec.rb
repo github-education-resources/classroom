96686eb1578133fbf7eba93ef105046a73820bcd
Bradley Bain <thebradbain@github.com>
2019-07-12 17:07:42 -0700

    Add specs for LTI::ConfigurationBuilder

--- spec/lib/github_classroom/lti/configuration_builder_spec.rb
+++ spec/lib/github_classroom/lti/configuration_builder_spec.rb
@@ -3,8 +3,6 @@
 require "rails_helper"
 
 describe GitHubClassroom::LTI::ConfigurationBuilder do
-  subject { described_class }
-
   let(:title) { Faker::App.name }
   let(:launch_url) { "www.example.com" }
   let(:options) do
@@ -20,18 +18,45 @@ describe GitHubClassroom::LTI::ConfigurationBuilder do
     }
   end
 
+  let(:ext_vendor) { Faker::Company.name }
+  let(:ext_options) do
+    {
+      param: Faker::Lorem.word,
+      nested: {
+        other_param: Faker::Lorem.sentence
+      }
+    }
+  end
+  subject { described_class.new(title, launch_url) }
+
   it "Generates a bare minimum xml configuration" do
     expected = IMS::LTI::Services::ToolConfig.new(title: title, launch_url: launch_url).to_xml
-    actual = subject.build_xml(title, launch_url)
+    actual = subject.to_xml
 
     expect(actual).to eq(expected)
   end
 
-  it "Generates an xml configuration with options" do
+  it "Generates an xml configuration with global options" do
     config_hash = options.merge(title: title, launch_url: launch_url)
     expected = IMS::LTI::Services::ToolConfig.new(config_hash).to_xml
 
-    actual = subject.build_xml(title, launch_url, options)
+    actual = subject.add_attributes(options).to_xml
+
+    expect(actual).to eq(expected)
+  end
+
+  it "Generates an xml configuration with vendor-specifc options" do
+    config_hash = options.merge(title: title, launch_url: launch_url)
+    config = IMS::LTI::Services::ToolConfig.new(config_hash)
+    ext_options.each_pair do |k, v|
+      config.set_ext_param(ext_vendor, k, v)
+    end
+
+    expected = config.to_xml
+    actual = subject
+      .add_attributes(options)
+      .add_vendor_attributes(ext_vendor, ext_options)
+      .to_xml
 
     expect(actual).to eq(expected)
   end
