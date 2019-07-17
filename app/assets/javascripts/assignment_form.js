4b8f4e85b332bf13879b65cf3dddc23e37f6bfac
Eric Pickup <ericpickup@github.com>
2019-07-17 09:38:12 -0700

    Remove blocking error box on input

--- app/assets/javascripts/assignment_form.js
+++ app/assets/javascripts/assignment_form.js
@@ -6,3 +6,12 @@ function importOptions(starterCodeFieldValue) {
     importOptions.style.display = 'none';
   }
 }
+
+function removeErrorBox() {
+  errorBoxes = document.getElementsByClassName("error");
+  for (let errorBox of errorBoxes) {
+    if (errorBox.innerText == "Starter code repository is not a template repository. Make it a template repository to use template cloning.") {
+      errorBox.style.display = 'none';
+    }
+  }
+}
