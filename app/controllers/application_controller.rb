e271847fd3ad56c572af4fd89cc7d52f24e2a03a
stephaniegiang <stephaniegiang@github.com>
2019-07-18 15:24:51 -0700

    Make changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  depends_on :authentication, :errors, :feature_flags
+  depends_on :authentication, :errors, :feature_flags, :google_authentication
 
   before_action :authenticate_user! # authentication_dependency
 
@@ -45,29 +45,7 @@ class ApplicationController < ActionController::Base
     }
   end
 
-  def authorize_google_classroom
-    google_classroom_client = GitHubClassroom.google_classroom_client
-    unless user_google_classroom_credentials
-      login_hint = current_user.github_user.login
-      redirect_to google_classroom_client.get_authorization_url(login_hint: login_hint, request: request)
-    end
-
-    @google_classroom_service = Google::Apis::ClassroomV1::ClassroomService.new
-    @google_classroom_service.client_options.application_name = "GitHub Classroom"
-    @google_classroom_service.authorization = user_google_classroom_credentials
-  end
-
   private
-
-  def user_google_classroom_credentials
-    google_classroom_client = GitHubClassroom.google_classroom_client
-    user_id = current_user.uid.to_s
-
-    google_classroom_client.get_credentials(user_id, request)
-  rescue Signet::AuthorizationError
-    # Will reauthorize upstream
-    nil
-  end
 
   def not_found
     raise ActionController::RoutingError, "Not Found"
