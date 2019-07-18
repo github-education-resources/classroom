7baa9d7577cc8ae527b207e9a0c236194e856d45
stephaniegiang <stephaniegiang@github.com>
2019-07-18 15:33:32 -0700

    Change column name

--- db/migrate/20190718211229_lms_type.rb
+++ db/migrate/20190718211229_lms_type.rb
@@ -1,5 +1,5 @@
 class LmsType < ActiveRecord::Migration[5.1]
   def change
-    add_column :lti_configurations, :type, :text, :null => false, :default => "other"
+    add_column :lti_configurations, :lms_type, :text, :null => false, :default => "other"
   end
 end
