0e3791742b0044fe43fa3ccf6fe2f89d6ca1f305
Eric Pickup <ericpickup@github.com>
2019-07-18 11:14:53 -0700

    Remove migrations done in separate PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190717205940) do
 
   # These are extensions that must be enabled in order to support this database
