b745d4f5bbe6a3fc43b3c63d66a1c38b69f89ff1
Bradley Bain <thebradbain@github.com>
2019-07-19 15:46:29 -0700

    Add lms_name helper method

--- app/models/lti_configuration.rb
+++ app/models/lti_configuration.rb
@@ -4,12 +4,17 @@ class LtiConfiguration < ApplicationRecord
   belongs_to :organization
 
   enum lms_type: {
-    canvas: "canvas",
-    blackboard: "blackboard",
-    brightspace: "brightspace",
-    moodle: "moodle",
+    canvas: "Canvas",
+    blackboard: "Blackboard",
+    brightspace: "Brightspace",
+    moodle: "Moodle",
     other: "other"
-  }
+  }, _prefix: true
+
+  def lms_name(default_name: "Other Learning Management System")
+    return default_name if lms_type_other?
+    LtiConfiguration.lms_types[lms_type]
+  end
 
   def self.find_by_auth_hash(hash)
     consumer_key = hash.credentials.token
--- spec/models/lti_configuration_spec.rb
+++ spec/models/lti_configuration_spec.rb
@@ -3,5 +3,25 @@
 require "rails_helper"
 
 RSpec.describe LtiConfiguration, type: :model do
+  let(:organization) { classroom_org }
+  let(:lti_configuration) { create(:lti_configuration, organization: organization) }
   it { should belong_to(:organization) }
+
+  describe "lms_name" do
+    it "returns the name corresponding an existing enum" do
+      lti_configuration.lms_type = :canvas
+      expect(lti_configuration.lms_name).to eq("Canvas")
+    end
+
+    it "returns the default name when unspecified type is :other" do
+      lti_configuration.lms_type = :other
+      expect(lti_configuration.lms_name).to eq("Other Learning Management System")
+    end
+
+    it "returns a custom default name when specified type is :other" do
+      custom_name = Faker::Company.name
+      lti_configuration.lms_type = :other
+      expect(lti_configuration.lms_name(default_name: custom_name)).to eq(custom_name)
+    end
+  end
 end
