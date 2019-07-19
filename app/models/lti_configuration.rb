2c0ddb40bd553241f4ef9aceebeda0ee862015e5
stephaniegiang <stephaniegiang@github.com>
2019-07-19 09:47:39 -0700

    Update type

--- app/models/lti_configuration.rb
+++ app/models/lti_configuration.rb
@@ -3,6 +3,14 @@
 class LtiConfiguration < ApplicationRecord
   belongs_to :organization
 
+  enum lms_type: [
+    :other
+    :blackboard,
+    :canvas,
+    :moodle,
+    :brightspace,
+  ]
+
   def self.find_by_auth_hash(hash)
     consumer_key = hash.credentials.token
     find_by(consumer_key: consumer_key)
