9da61da49f8683545f250f2f502b98a126ef5545
Shaunak Pagnis <shaunakpp@github.com>
2019-07-19 16:09:03 -0700

    changed Broadcaster to a module and added specs for it

--- app/services/create_github_repo_service/broadcaster.rb
+++ app/services/create_github_repo_service/broadcaster.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreateGitHubRepoService
-  class Broadcaster
+  module Broadcaster
     MESSAGES = {
       repository_creation_complete: "Your GitHub repository was created.",
       import_ongoing: "Your GitHub repository is importing starter code.",
@@ -9,38 +9,23 @@ class CreateGitHubRepoService
       importing_starter_code: "Importing starter code."
     }.freeze
 
-    attr_reader :exercise, :message, :message_type, :repo_url
-
     def self.call(exercise, message, message_type, repo_url = nil)
-      new(exercise, message, message_type, repo_url).call
-    end
-
-    def initialize(exercise, message, message_type, repo_url = nil)
-      @exercise = exercise
-      @message = message
-      @message_type = message_type
-      @repo_url = repo_url
-    end
-
-    def call
+      message_hash = build_message(exercise, message, message_type, repo_url)
+      channel = build_channel(exercise)
       ActionCable.server.broadcast(channel, message_hash)
     end
 
-    private
-
-    def channel
-      channel_class.channel(channel_hash)
+    def self.build_channel(exercise)
+      channel_class =
+        if exercise.user?
+          RepositoryCreationStatusChannel
+        else
+          GroupRepositoryCreationStatusChannel
+        end
+      channel_class.channel(channel_hash(exercise))
     end
 
-    def channel_class
-      if exercise.user?
-        RepositoryCreationStatusChannel
-      else
-        GroupRepositoryCreationStatusChannel
-      end
-    end
-
-    def message_hash
+    def self.build_message(exercise, message, message_type, repo_url = nil)
       {}.tap do |msg|
         msg[:status] = exercise.status
         msg[:repo_url] = repo_url if repo_url.present?
@@ -48,7 +33,7 @@ class CreateGitHubRepoService
       end
     end
 
-    def channel_hash
+    def self.channel_hash(exercise)
       {}.tap do |msg|
         msg["#{exercise.humanize}_id".to_sym] = exercise.collaborator.id
         msg["#{exercise.assignment_type}_id".to_sym] = exercise.assignment.id
