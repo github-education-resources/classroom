47a98a52564e393a0895fba8c919efac3f273d40
stephaniegiang <stephaniegiang@github.com>
2019-07-19 13:13:04 -0700

    Change to string

--- app/models/lti_configuration.rb
+++ app/models/lti_configuration.rb
@@ -4,11 +4,11 @@ class LtiConfiguration < ApplicationRecord
   belongs_to :organization
 
   enum lms_type: {
-    canvas: 1,
-    blackboard: 2,
-    brightspace: 3,
-    moodle: 4,
-    other: 5
+    canvas: "canvas",
+    blackboard: "blackboard",
+    brightspace: "brightspace",
+    moodle: "moodle",
+    other: "other"
   }
 
   def self.find_by_auth_hash(hash)
