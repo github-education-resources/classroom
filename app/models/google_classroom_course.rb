b20e51d30552e4d469876bf7e1dae479ad1f9b50
stephaniegiang <stephaniegiang@github.com>
2019-07-24 13:40:03 -0700

    test for model

--- app/models/google_classroom_course.rb
+++ app/models/google_classroom_course.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class GoogleClassroomCourse
+  attr_accessor :name, :students
   def initialize(google_classroom_service, course_id)
     @google_classroom_service = google_classroom_service
     @course_id = course_id
@@ -8,11 +9,13 @@ class GoogleClassroomCourse
 
   def name
     course = @google_classroom_service.get_course(@course_id)
+    binding.pry
     course&.name
   end
 
   def students
     response = @google_classroom_service.list_course_students(@course_id)
+    return [] if defined? response.students && respoonse.students.present? 
     response.students ||= []
     response.students
   end
---
+++ spec/models/google_classroom_course_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "google/apis/classroom_v1"
+
+RSpec.describe Roster, type: :model do
+  before do
+    GoogleAPI = Google::Apis::ClassroomV1
+  end
+  
+  describe "#initialize" do 
+    it "succeeds" do
+      google_classroom_service =  Google::Apis::ClassroomV1::ClassroomService.new
+      course = GoogleClassroomCourse.new(google_classroom_service, "5555")
+      expect(course).to_not be_nil
+    end
+  end
+
+  describe "#student", :vcr do
+    it "succeeds" do
+      client = Signet::OAuth2::Client.new
+        allow_any_instance_of(ApplicationController)
+          .to receive(:user_google_classroom_credentials)
+          .and_return(client)
+
+      response = GoogleAPI::ListCoursesResponse.new
+      allow_any_instance_of(GoogleAPI::ClassroomService)
+        .to receive(:list_course_students)
+        .and_return(response)
+
+      google_classroom_service =  GoogleAPI::ClassroomService.new
+      course = GoogleClassroomCourse.new(google_classroom_service, "5555")
+
+      expect(course.students).to eq([])
+    end
+  end
+
+  describe "#name", :vcr do
+    it "suceeds" do
+      client = Signet::OAuth2::Client.new
+      allow_any_instance_of(ApplicationController)
+        .to receive(:user_google_classroom_credentials)
+        .and_return(client)
+
+      response = GoogleAPI::Course.new
+      allow_any_instance_of(GoogleAPI::ClassroomService)
+        .to receive(:get_course)
+        .and_return(response)
+
+      google_classroom_service =  GoogleAPI::ClassroomService.new
+      course = GoogleClassroomCourse.new(google_classroom_service, "5555")
+
+      expect(course.name).to be_nil
+    end
+  end
+end
