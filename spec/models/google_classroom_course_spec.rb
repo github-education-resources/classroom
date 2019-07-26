15e1218f314220394d63aeb95ea59802881aa215
stephaniegiang <stephaniegiang@github.com>
2019-07-26 09:06:49 -0700

    Update tests

--- spec/models/google_classroom_course_spec.rb
+++ spec/models/google_classroom_course_spec.rb
@@ -17,20 +17,31 @@ RSpec.describe Roster, type: :model do
   end
 
   describe "#student", :vcr do
-    it "succeeds" do
+    before do
       client = Signet::OAuth2::Client.new
       allow_any_instance_of(ApplicationController)
         .to receive(:user_google_classroom_credentials)
         .and_return(client)
 
-      response = GoogleAPI::ListCoursesResponse.new
+      valid_response = GoogleAPI::ListStudentsResponse.new
+
+      student_names = ["Student 1", "Student 2"]
+      student_profiles = student_names.map do |name|
+        GoogleAPI::UserProfile.new(name: GoogleAPI::Name.new(full_name: name))
+      end
+      students = student_profiles.map { |prof| GoogleAPI::Student.new(profile: prof) }
+
+      valid_response.students = students
       allow_any_instance_of(GoogleAPI::ClassroomService)
         .to receive(:list_course_students)
-        .and_return(response)
+        .and_return(valid_response)
 
       google_classroom_service = GoogleAPI::ClassroomService.new
-      course = GoogleClassroomCourse.new(google_classroom_service, "5555")
-      expect(course.students).to eq([])
+      @course = GoogleClassroomCourse.new(google_classroom_service, "5555")
+    end
+
+    it "has the correct number of students" do
+      expect(@course.students.length).to eq(2)
     end
   end
 
