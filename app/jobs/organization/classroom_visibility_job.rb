e4eff5606a492a6439413c33cfc15d289171416c
Eric Pickup <ericpickup@github.com>
2019-07-30 12:40:22 -0700

    Linter fixes

--- app/jobs/organization/classroom_visibility_job.rb
+++ app/jobs/organization/classroom_visibility_job.rb
@@ -4,17 +4,24 @@ class Organization
   class ClassroomVisibilityJob < ApplicationJob
     queue_as :classroom_visibility
 
+    # rubocop:disable SkipsModelValidations
     def perform(organization, visibility)
       public_classroom = (visibility == "public" ? true : false)
       assignments = organization.assignments + organization.group_assignments
       assignments.each do |assignment|
         assignment.update_attribute("public_repo", public_classroom)
-        assignment.repos.each do |assignment_repo|
-          begin
-            assignment_repo.github_repository.public = public_classroom
-          rescue Octokit::InvalidRepository, GitHub::NotFound, GitHub::Forbidden, GitHub::Error
-            next
-          end
+        change_visibility_in_all_assignment_repos(assignment, public_classroom)
+      end
+    end
+
+    private
+
+    def change_visibility_in_all_assignment_repos(assignment, public_repo)
+      assignment.repos.each do |assignment_repo|
+        begin
+          assignment_repo.github_repository.public = public_repo
+        rescue Octokit::InvalidRepository, GitHub::NotFound, GitHub::Forbidden, GitHub::Error
+          next
         end
       end
     end
