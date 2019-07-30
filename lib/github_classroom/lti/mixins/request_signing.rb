df31b4d3c89ec5e6c7712c8cf30abfc56fdd7de2
Bradley Bain <thebradbain@github.com>
2019-07-30 10:42:45 -0700

    Modify LTI request signing for ContentItemSelectionRequest (probably not necessary)

--- lib/github_classroom/lti/mixins/request_signing.rb
+++ lib/github_classroom/lti/mixins/request_signing.rb
@@ -4,10 +4,10 @@ module GitHubClassroom
   module LTI
     module Mixins
       module RequestSigning
-        def signed_request(endpoint, consumer_key, secret, query: {}, headers: {})
+        def signed_request(endpoint, consumer_key, secret, query: {}, headers: {}, body: {})
           uri = URI.parse(endpoint)
           uri.query = URI.encode_www_form(query)
-          headers = { "Authorization": signed_auth_header(uri, consumer_key, secret) }.merge(headers)
+          headers = { "Authorization": signed_auth_header(uri, consumer_key, secret, body: body) }.merge(headers)
 
           opts = { url: uri, headers: headers }
           Faraday.new(opts) do |conn|
@@ -18,8 +18,13 @@ module GitHubClassroom
 
         private
 
-        def signed_auth_header(uri, consumer_key, secret)
-          req = Net::HTTP::Get.new(uri)
+        def signed_auth_header(uri, consumer_key, secret, method: :get, body: "")
+          if method == :get
+            req = Net::HTTP::Get.new(uri)
+          else
+            req = Net::HTTP::Post.new(uri)
+            req.body = body
+          end
 
           consumer = OAuth::Consumer.new(consumer_key, secret)
           auth_signer = OAuth::Client::Helper.new(req, request_uri: uri, consumer: consumer)
