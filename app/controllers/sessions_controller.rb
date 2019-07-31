ecde584eae7ffe58e29ef7554d5bc1a7640f2204
Bradley Bain <thebradbain@github.com>
2019-07-30 19:16:14 -0700

    Redirect depending on contexts of lti launch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -54,7 +54,10 @@ class SessionsController < ApplicationController
   # rubocop:disable MethodLength
   # rubocop:disable AbcSize
   def lti_launch
+    # TODO: There is so much tiny and important yet unrelated sutff happening in this method. Let's split it up.
     auth_hash = request.env["omniauth.auth"]
+
+    # PART ONE: validating the launch message
     message_store = GitHubClassroom.lti_message_store(
       consumer_key: auth_hash.credentials.token
     )
@@ -62,17 +65,35 @@ class SessionsController < ApplicationController
     message = GitHubClassroom::LTI::MessageStore.construct_message(auth_hash.extra.raw_info)
     raise("invalid lti launch message") unless message_store.message_valid?(message)
     puts message
+
+    # PART TWO: keying the launch message to the session
     nonce = message_store.save_message(message)
     session[:lti_nonce] = nonce
 
+    # PART 2.5, we should untangle the login logic from the redirect logic
+
+    # PART THREE: a spaghetti mess state machine determining how to interpret the launch message.
+    # This should definitely be cleaned up (as it doesn't exist pre-spike), but it's a spike, so :)
     linked_org = LtiConfiguration.find_by_auth_hash(auth_hash).organization
     if logged_in?
-      if(message.resource_link_id)
-        @post_launch_url = select_lms_assignment_organization_url(linked_org, resource_id: message.resource_link_id)
-      else
+      # If the organization has already been linked, determine whether the instructor is trying to
+      # link an LMS assignment to an assignment on classroom or if the assignment is being launched
+      # by a student to start their homework
+      if(message.resource_link_id && linked_org.lti_configuration.lms_connection_status_linked?)
+        linked_assignment = linked_org.assignments.find_by(lti_resource_link_id: message.resource_link_id)
+        if(linked_assignment)
+          @post_launch_url = assignment_invitation_path(id: linked_assignment.invitation.key) #organization_assignment_url(linked_org, linked_assignment)
+        else
+          @post_launch_url = select_lms_assignment_organization_url(linked_org, resource_id: message.resource_link_id)
+        end
+      else  # Organization is unliked, so finish linking the lti configuration
+        linked_org.lti_configuration.lms_connection_status_linked!
+        linked_org.lti_configuration.save!
         @post_launch_url = complete_lti_configuration_url(linked_org)
       end
     else
+      # ... all of that mess of logic, without refactor, would also have to be re-put here, but because
+      # it's a spike, let's pretend a user is always authenticated with GitHub pre-launch :)
       @post_launch_url = login_url
       session[:pre_login_destination] = complete_lti_configuration_url(linked_org)
     end
