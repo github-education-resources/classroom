68b17dc6c2c79451495ea0f6de81c318c6ea3b59
Shaunak Pagnis <shaunakpp@github.com>
2019-07-31 17:24:49 -0700

    rescues Octokit::InvalidRepository error in DeadlineJob

--- app/jobs/deadline_job.rb
+++ app/jobs/deadline_job.rb
@@ -20,8 +20,20 @@ class DeadlineJob < ApplicationJob
 
     repo.submission_sha = latest_commit[:sha]
     repo.save!
+
+  # we explicitly rescue Octokit::InvalidRepository because
+  # it is not inherited from Octokit::Error so it is not
+  # handled by our GitHub::Error.with_error_handling block
+  # for more info see:
+  # https://github.com/octokit/octokit.rb/blob/master/lib/octokit/error.rb#L310
+  rescue Octokit::InvalidRepository => e
+    log_error(repo, e)
   rescue ActiveRecord::ActiveRecordError => e
+    log_error(repo, e)
+  end
+
+  def log_error(repo, error)
     logger.error(repo)
-    logger.error(e)
+    logger.error(error)
   end
 end
