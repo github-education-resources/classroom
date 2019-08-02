c32a7dec69e9f15332fbb9ceb838270749e45eb7
Bradley Bain <thebradbain@github.com>
2019-08-02 13:34:35 -0700

    Faraday response

--- lib/github_classroom/lti/mixins/request_signing.rb
+++ lib/github_classroom/lti/mixins/request_signing.rb
@@ -5,42 +5,56 @@ module GitHubClassroom
     module Mixins
       module RequestSigning
         # given an endpoint, build a Faraday connection which satisfies the LTI standard
-        def signed_request(endpoint, method: :post, headers: {}, query: {}, body: nil)
-          req = build_net_req(endpoint, method, headers, query, body)
+        def signed_request(endpoint, lti_version: 1.1, method: :post, headers: {}, query: {}, body: nil)
+          req = build_http_req(endpoint, method, headers, query, body)
+          sign_request!(req, @consumer_key, @secret, lti_version: lti_version)
 
-          sign_request!(req, @consumer_key, @secret)
+          request_headers = {}
+          req.each_header { |header, value| request_headers[header] = value }
 
-          signed_headers = {}
-          req.each_header { |header, value| signed_headers[header] = value }
-
-          Faraday.new(url: req.uri, headers: signed_headers) do |conn|
+          connection = Faraday.new(url: req.uri, headers: request_headers) do |conn|
             conn.response :raise_error
             conn.adapter Faraday.default_adapter
           end
+
+          body.present? ? connection.send(method, nil, req.body) : connection.send(method)
         end
+
+        #@@default_lti_request_options = {
+        #  lti_version: 1.1,
+        #  method: :post,
+        #  scheme: :header,
+        #  headers: {},
+        #  query: {},
+        #  body: nil
+        #}
 
         private
 
         # builds a Net::HTTP request from endpoint and options
-        def build_net_req(endpoint, method = :post, headers = {}, query = {}, body = nil)
+        def build_http_req(endpoint, method = :post, headers = {}, query = nil, body = nil)
           uri = URI.parse(endpoint)
-          uri.query = URI.encode_www_form(query)
+          uri.query = URI.encode_www_form(query) if query
 
           klass = "Net::HTTP::#{method.to_s.capitalize}".constantize
-          req = klass.new(uri)
+          req = klass.new(uri, headers.stringify_keys)
 
-          headers.each_pair { |header, value| req[header] = value }
-          req.body = body
+          if(body.is_a?(Hash))
+            req.body = OAuth::Helper.normalize(body)
+            req.content_type = 'application/x-www-form-urlencoded'
+          else
+            req.body = body.to_s
+            req["Content-Length"] = req.body.length.to_s
+          end
 
           req
         end
 
         def sign_request!(req, consumer_key, secret, lti_version: 1.1)
-          consumer = OAuth::Consumer.new(consumer_key, secret)
-
           http = Net::HTTP.new(req.uri.host, req.uri.port)
+          consumer = OAuth::Consumer.new(consumer_key, secret)
           if lti_version == 1.1
-            # necessary to override because LTI 1.1 expects a body hash
+            # Necessary to override because LTI 1.1 expects a body hash
             # even when the body is empty, as it is in GET requests
             req.instance_variable_set(:@request_has_body, true)
             req.oauth!(http, consumer, nil, scheme: "header")
