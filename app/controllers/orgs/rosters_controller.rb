1890076c606fdb2c2606883cf20b5574c498cb2c
stephaniegiang <stephaniegiang@github.com>
2019-08-05 23:00:30 -0700

    Rubocop

--- app/controllers/orgs/rosters_controller.rb
+++ app/controllers/orgs/rosters_controller.rb
@@ -211,10 +211,9 @@ module Orgs
     end
 
     def check_for_duplicate_entry
-      if RosterEntry.where(roster: current_roster, identifier: params[:roster_entry_identifier]).any?
-        flash[:error] = "There is already a roster entry named #{params[:roster_entry_identifier]}."
-        redirect_to roster_url(current_organization)
-      end
+      return if RosterEntry.where(roster: current_roster, identifier: params[:roster_entry_identifier]).any?
+      flash[:error] = "There is already a roster entry named #{params[:roster_entry_identifier]}."
+      redirect_to roster_url(current_organization)
     end
 
     # An unlinked user is a user who:
