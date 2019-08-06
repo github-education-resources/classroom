e2d6aa82aeed721d55ea1ecd11b6a8d738561349
Bradley Bain <thebradbain@github.com>
2019-08-05 18:35:03 -0700

    Add Membership LtiModels (serialization comes free!)

---
+++ lib/github_classroom/lti/models/membership_extension/member.rb
@@ -0,0 +1,7 @@
+module GitHubClassroom::LTI::Models::MembershipExtension
+  class Member < IMS::LTI::Models::LTIModel
+    add_attributes :person_name_family, :person_name_given, :person_sourcedid, :user_id
+    add_attribute :name,  json_key: "person_name_full"
+    add_attribute :email, json_key: "person_contact_email_primary"
+  end
+end
---
+++ lib/github_classroom/lti/models/membership_service/membership_subject.rb
@@ -0,0 +1,13 @@
+module GitHubClassroom::LTI::Models::MembershipService
+  class MembershipSubject < IMS::LTI::Models::LTIModel
+    add_attribute :membership, relation: "GitHubClassroom::LTI::Models::MembershipService::Membership"
+
+    add_attribute :context, json_key: "contextId"
+    add_attribute :id, json_key: "@id"
+    add_attribute :type, json_key: "@type"
+
+    def memberships
+      [*@membership]
+    end
+  end
+end
