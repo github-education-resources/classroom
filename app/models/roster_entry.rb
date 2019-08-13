7ce6481e10122667f3e86a962b958ca7944f2bd0
Shaunak Pagnis <shaunakpp@github.com>
2019-08-12 22:46:39 -0700

    improve  query in RosterEntry

--- app/models/roster_entry.rb
+++ app/models/roster_entry.rb
@@ -69,18 +69,25 @@ class RosterEntry < ApplicationRecord
   # last:   Unlinked student
   #
   # with a secondary sort on ID to ensure ties are always handled in the same way
+  # rubocop:disable Metrics/MethodLength
   def self.order_for_view(assignment)
-    users_with_repo = assignment.repos.pluck(:user_id)
-    sql_formatted_users = users_with_repo.empty? ? "(NULL)" : "(#{users_with_repo.join(',')})"
+    join_sql = <<~SQL
+      LEFT JOIN assignment_repos
+      ON roster_entries.user_id = assignment_repos.user_id
+      AND assignment_repos.assignment_id = #{assignment.id}
+    SQL
 
-    order <<~SQL
+    order_sql = <<~SQL
       CASE
-        WHEN roster_entries.user_id IS NULL THEN 2                   /* Not linked */
-        WHEN roster_entries.user_id IN #{sql_formatted_users} THEN 0 /* Accepted */
-        ELSE 1                                                       /* Linked but not accepted */
+        WHEN roster_entries.user_id IS NULL THEN 2      /* Not linked */
+        WHEN roster_entries.user_id IS NOT NULL THEN 0  /* Linked but not accepted */
+        ELSE 1                                          /* Unlinked */
       END
     SQL
+
+    joins(join_sql).order(order_sql)
   end
+  # rubocop:enable Metrics/MethodLength
 
   # Restrict relation to only entries that have not joined a team
   def self.students_not_on_team(group_assignment)
