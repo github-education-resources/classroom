89ecdd057d8a279d1b4b8543e45e8de94d9dc20d
Jess Rudder <thejessicarudder@gmail.com>
2019-09-09 15:08:29 -0700

    Update the feature flag name

--- app/controllers/application_controller/feature_flags_dependency.rb
+++ app/controllers/application_controller/feature_flags_dependency.rb
@@ -38,8 +38,8 @@ class ApplicationController
   end
   helper_method :classroom_visibility_enabled?
 
-  def redesign_enabled?
-    GitHubClassroom.flipper[:redesign].enabled? || (logged_in? && current_user.feature_enabled?(:redesign))
+  def onboarding_redesign_enabled?
+    GitHubClassroom.flipper[:onboarding_redesign].enabled? || (logged_in? && current_user.feature_enabled?(:onboarding_redesign))
   end
-  helper_method :redesign_enabled?
+  helper_method :onboarding_redesign_enabled?
 end
