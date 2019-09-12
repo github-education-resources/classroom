eeb4906f566b9a3a8d9a11890b575ab03f488180
Jess Rudder <thejessicarudder@gmail.com>
2019-09-11 14:56:55 -1000

    Remove archive_classroom feature flag code

--- app/controllers/application_controller/feature_flags_dependency.rb
+++ app/controllers/application_controller/feature_flags_dependency.rb
@@ -23,11 +23,6 @@ class ApplicationController
   end
   helper_method :team_management_enabled?
 
-  def archive_classrooms_enabled?
-    logged_in? && current_user.feature_enabled?(:archive_classrooms)
-  end
-  helper_method :archive_classrooms_enabled?
-
   def lti_launch_enabled?
     GitHubClassroom.flipper[:lti_launch].enabled? || (logged_in? && current_user.feature_enabled?(:lti_launch))
   end
