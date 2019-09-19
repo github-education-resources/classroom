fa60bc4a1be4b657cddf38a4670c5627be53b96a
Nathaniel Woodthorpe <d12@github.com>
2019-09-19 09:53:17 -0400

    Rename user to student

--- db/schema.rb
+++ db/schema.rb
@@ -258,7 +258,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_192858) do
     t.string "github_avatar_url"
     t.string "github_html_url"
     t.boolean "teacher"
-    t.boolean "user"
+    t.boolean "student"
     t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
