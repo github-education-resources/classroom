9f972e70c43b4c06ae8e20af19e54357e9405b7e
Terian Koscik <spinecone@github.com>
2019-09-27 14:18:31 -0500

    switch single quotes to double quotes 4

--- spec/controllers/stafftools/groups_controller_spec.rb
+++ spec/controllers/stafftools/groups_controller_spec.rb
@@ -42,32 +42,32 @@ RSpec.describe Stafftools::GroupsController, type: :controller do
     end
   end
 
-  describe 'DELETE #destroy', :vcr do
-    context 'as an unauthorized user' do
+  describe "DELETE #destroy", :vcr do
+    context "as an unauthorized user" do
       before do
         delete :destroy, params: { id: group.id }
       end
 
-      it' returns a 404' do
+      it "returns a 404" do
         expect(response.status).to eq(404)
       end
     end
 
-    context 'as an authorized user' do
+    context "as an authorized user" do
       before do
         user.update_attributes(site_admin: true)
         delete :destroy, params: { id: group.id }
       end
 
-      it 'deletes the group' do
+      it "deletes the group" do
         expect(Group.find_by(id: group.id)).to be_nil
       end
 
-      it 'shows an informative message' do
-        expect(flash[:success]).to eq('Group was destroyed')
+      it "shows an informative message" do
+        expect(flash[:success]).to eq("Group was destroyed")
       end
 
-      it 'redirects to grouping page' do
+      it "redirects to grouping page" do
         expect(response).to redirect_to(stafftools_grouping_path(group.grouping.id))
       end
     end
