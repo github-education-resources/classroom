a26b28ffcd3a1cf8b0b22507121b02ac8e2f6ceb
Jess Rudder <thejessicarudder@gmail.com>
2019-10-21 10:22:19 -1000

    Replace custom dropdown menu operations with details menu javascript. TODO: Get working on Edge

--- app/assets/javascripts/organization-filtering.js
+++ app/assets/javascripts/organization-filtering.js
@@ -16,17 +16,6 @@ $(document).ready(function() {
     history.replaceState(null, '', '?' + formData);
     debounce(() => submitSearchAndFilters(searchForm), 150)
   });
-
-  $("#js-filtering-form .SelectMenu-item").on("change", function(e) {
-    const clickedItem = e.target.closest(".SelectMenu-item");
-    const currentMenu = clickedItem.closest("details");
-    const currentActiveItem = currentMenu.querySelector("[aria-checked=true]");
-
-    currentActiveItem.setAttribute("aria-checked", false);
-    clickedItem.setAttribute("aria-checked", true);
-    currentMenu.querySelector("[data-menu-button]").innerText = clickedItem.innerText;
-    currentMenu.removeAttribute('open');
-  });
 });
 
 function submitSearchAndFilters(form) {
