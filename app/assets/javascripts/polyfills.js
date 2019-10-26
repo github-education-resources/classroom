dbae12ca4d685936acfa6a986e8d2fa091d180fc
Jess Rudder <thejessicarudder@gmail.com>
2019-10-25 16:01:24 -1000

    Move polyfills to separate file and include them

---
+++ app/assets/javascripts/polyfills.js
@@ -0,0 +1,11 @@
+// Polyfills required by browsers without Web Components
+// This addresses issues we had using Primer components on
+// some versions of Edge and IE
+//
+//= require @webcomponents/custom-elements/custom-elements.min
+//= require mdn-polyfills/Node.prototype.append
+//= require mdn-polyfills/Node.prototype.prepend
+//= require mdn-polyfills/Node.prototype.before
+//= require mdn-polyfills/Node.prototype.after
+//= require mdn-polyfills/Node.prototype.replaceWith
+//= require details-element-polyfill/dist/details-element-polyfill
