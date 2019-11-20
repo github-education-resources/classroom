4c4cffac39b5eeccdf92f8eddff042184db6359d
Jess Rudder <thejessicarudder@gmail.com>
2019-11-20 09:28:47 -1000

    Fix test issue related to bullet gem upgrade for rails 6.0.1

--- spec/support/bullet.rb
+++ spec/support/bullet.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 if defined?(Bullet) && Bullet.enable?
+  # There's an issue with the gem that results in false positives in tests. The
+  # recommendation is to disable this until it's fixed https://github.com/flyerhzm/bullet/issues/481
+  Bullet.unused_eager_loading_enable = false
+
   RSpec.configure do |config|
     config.before(:each) do
       Bullet.start_request
-    end
-
-    config.around(:each, type: :controller) do |example|
-      Bullet.unused_eager_loading_enable = false
-      example.run
-      Bullet.unused_eager_loading_enable = true
     end
 
     config.after(:each) do
