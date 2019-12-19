8db751f36915d1b5b55a418d0255f3b266c35000
Terian Koscik <spinecone@github.com>
2019-12-19 10:50:10 -0600

    remove classroom visibility feature

--- app/controllers/application_controller/feature_flags_dependency.rb
+++ app/controllers/application_controller/feature_flags_dependency.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController
-  def classroom_visibility_enabled?
-    logged_in? && current_user.feature_enabled?(:classroom_visibility)
-  end
-  helper_method :classroom_visibility_enabled?
-
   def onboarding_redesign_enabled?
     GitHubClassroom.flipper[:onboarding_redesign].enabled? || (logged_in? && current_user.feature_enabled?(:onboarding_redesign))
   end
--- lib/tasks/enable_features.rake
+++ lib/tasks/enable_features.rake
@@ -2,5 +2,4 @@
 
 task enable_features: :environment do
   GitHubClassroom.flipper[:archive_classrooms].enable_group :staff
-  GitHubClassroom.flipper[:classroom_visibility].enable_group :staff
 end
